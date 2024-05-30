a54cc3313df247259a4949301ca3be7d28191b46
alexander-mann <alexandercmann@gmail.com>
2024-05-30 17:55:24 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@331978dd2098fc09f226a3e5e701f6acf3b50ae7 🚀

---
+++ assets/DropdownMenu-CAWHcM67.js
@@ -0,0 +1,29 @@
+import{j as o}from"./jsx-runtime-qGIIFXMu.js";import{useMDXComponents as r}from"./index-CqcSDpoT.js";import"./Avatar-lC8ay4Bw.js";import"./index-DI7FrtIC.js";import{B as i}from"./Button-Hml3oSe8.js";import"./Popover-CieifkIx.js";import"./Tooltip-sA3mfm35.js";import"./Text-C1Eb4vO9.js";import"./Resizable-DHQBnRfD.js";import"./Icon-C_fIsRl8.js";import"./SidebarLayoutNavAvatar-vvPiAYw7.js";import"./Command-OoteAhfS.js";import{M as p,T as m,d as u,C as s}from"./index-CebPbu-g.js";import{S as a,B as d}from"./DropdownMenu.stories-BgBQzgMw.js";import"./index-CDs2tPxN.js";import"./extends-CCbyfPlC.js";import"./index-CCD8SudB.js";import"./index-B-yFm4aN.js";import"./index-CBeK71W_.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./createLucideIcon-BucF5Q8I.js";import"./index-CcyUcsxs.js";import"./index-CKUEfieN.js";import"./index-rwdFSQ0g.js";import"./index-BQ87-lmA.js";import"./iframe-DEbsL6-B.js";import"../sb-preview/runtime.js";import"./index-DXimoRZY.js";import"./index-BH_9z_ur.js";import"./index-DrFu-skq.js";function e(n){const t={h4:"h4",...r(),...n.components};return o.jsxs(o.Fragment,{children:[o.jsx(p,{of:a}),`
+`,o.jsx(m,{}),`
+`,o.jsx(t.h4,{id:"displays-a-menu-to-the-user--such-as-a-set-of-actions-or-functions--triggered-by-a-button",children:"Displays a menu to the user — such as a set of actions or functions — triggered by a button."}),`
+`,o.jsx(i,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/dropdown-menu#api-reference","_blank"),children:"API Reference"}),`
+`,o.jsx(u,{dark:!0,language:"tsx",code:`
+import { 
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger, 
+} from '@0xintuition/1ui'
+
+<DropdownMenu>
+  <DropdownMenuTrigger>
+    <Button size="lg">Open menu</Button>
+  </DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>Profile</DropdownMenuItem>
+    <DropdownMenuItem>Billing</DropdownMenuItem>
+    <DropdownMenuItem>Team</DropdownMenuItem>
+    <DropdownMenuItem>Subscription</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+`}),`
+`,o.jsx(s,{of:d,sourceState:"none"})]})}function G(n={}){const{wrapper:t}={...r(),...n.components};return t?o.jsx(t,{...n,children:o.jsx(e,{...n})}):e(n)}export{G as default};
