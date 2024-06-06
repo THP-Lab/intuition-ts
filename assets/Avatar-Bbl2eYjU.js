c09b51e23774df650d267203743369189f88a933
alexander-mann <alexandercmann@gmail.com>
2024-06-06 17:14:35 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@4afa86fc01ed311c5b9158605fddb97215bffd73 🚀

---
+++ assets/Avatar-Bbl2eYjU.js
@@ -0,0 +1,24 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as i}from"./index-DSkyVWTJ.js";import{M as s,T as m,d as e,C as n,e as p}from"./index-BKWMfzsS.js";import"./Avatar-4zGhZtDZ.js";import"./Badge-DdnwakjS.js";import{B as c}from"./HoverCard-Uxsve1dp.js";import"./Input-BOF_Ax6K.js";import"./Separator-C2dyLaF-.js";import"./Text-Ut-2NFPv.js";import"./Resizable-D6r50leV.js";import"./Icon-Cl8ckYc1.js";import"./Textarea-BcMsqAZI.js";import"./Label-CGvHXEnN.js";import{S as l,B as h,F as x}from"./Avatar.stories-yqSQqvRZ.js";import"./index-CTjT7uj6.js";import"./iframe-CbUSlLJL.js";import"../sb-preview/runtime.js";import"./index-B7lOVQLg.js";import"./index-DdAkmXN0.js";import"./extends-CCbyfPlC.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-CjhMTvA2.js";import"./index-bugisSBR.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function o(r){const a={h2:"h2",h3:"h3",h4:"h4",...i(),...r.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:l}),`
+`,t.jsx(m,{}),`
+`,t.jsx(a.h4,{id:"an-image-element-with-a-fallback-for-representing-the-user",children:"An image element with a fallback for representing the user."}),`
+`,t.jsx(c,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/avatar#api-reference","_blank"),children:"API Reference"}),`
+`,t.jsx(e,{dark:!0,language:"tsx",code:`
+import { Avatar, AvatarFallback, AvatarImage } from '@0xintuition/1ui'
+
+<Avatar>
+  <AvatarImage src="https://myimage.jpeg" alt="intuition" />
+  <AvatarFallback>IN</AvatarFallback>
+</Avatar>
+`}),`
+`,t.jsx(n,{of:h,sourceState:"none"}),`
+`,t.jsx(a.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(p,{}),`
+`,t.jsx(a.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(a.h3,{id:"fallback",children:"Fallback"}),`
+`,t.jsx(e,{dark:!0,language:"tsx",code:`
+<Avatar>
+  <AvatarImage src="broken-link" alt="broken-link" />
+  <AvatarFallback>IN</AvatarFallback>
+</Avatar>
+`}),`
+`,t.jsx(n,{of:x,sourceState:"none"})]})}function V(r={}){const{wrapper:a}={...i(),...r.components};return a?t.jsx(a,{...r,children:t.jsx(o,{...r})}):o(r)}export{V as default};
