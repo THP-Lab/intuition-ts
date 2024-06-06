6891c4cf10b10fbece61cd4ad4cd38b614d258e3
alexander-mann <alexandercmann@gmail.com>
2024-06-06 15:53:05 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@76d6a0007824fc2387d17b630520c472569a1df6 🚀

---
+++ assets/Switch-DBgkKuT1.js
@@ -0,0 +1,13 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as i}from"./index-DSkyVWTJ.js";import{M as n,T as m,d as a,C as s}from"./index-gm_XPJJF.js";import"./Avatar-4zGhZtDZ.js";import"./Badge-DdnwakjS.js";import{B as p}from"./Select-CVck_Aun.js";import"./Input-BOF_Ax6K.js";import"./Separator-C2dyLaF-.js";import"./Text-Ut-2NFPv.js";import"./Resizable-D6r50leV.js";import"./Icon-Cl8ckYc1.js";import"./Textarea-BcMsqAZI.js";import"./Label-CGvHXEnN.js";import{S as c,B as d}from"./Switch.stories-CFea0u8U.js";import"./index-CTjT7uj6.js";import"./iframe-YRPZ4UzD.js";import"../sb-preview/runtime.js";import"./index-B7lOVQLg.js";import"./index-DdAkmXN0.js";import"./extends-CCbyfPlC.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-CjhMTvA2.js";import"./index-bugisSBR.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function r(o){const e={h4:"h4",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:c}),`
+`,t.jsx(m,{}),`
+`,t.jsx(e.h4,{id:"a-control-that-allows-the-user-to-toggle-between-checked-and-not-checked",children:"A control that allows the user to toggle between checked and not checked."}),`
+`,t.jsx(p,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/switch#api-reference","_blank"),children:"API Reference"}),`
+`,t.jsx(a,{dark:!0,language:"tsx",code:`
+import { Switch } from '@0xintuition/1ui'
+
+<div>
+  <Switch id="airplane-mode" />
+  <Label htmlFor="airplane-mode">Airplane Mode</Label>
+</div>
+`}),`
+`,t.jsx(s,{of:d,sourceState:"none"})]})}function U(o={}){const{wrapper:e}={...i(),...o.components};return e?t.jsx(e,{...o,children:t.jsx(r,{...o})}):r(o)}export{U as default};
