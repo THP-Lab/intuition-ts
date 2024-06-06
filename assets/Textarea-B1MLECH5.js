55177d5346a937b770e94d6fe80e1c5214cbe494
alexander-mann <alexandercmann@gmail.com>
2024-06-06 18:23:27 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@7e77493ec9a627b1e2a6d0c51f9d324e15eb2a92 🚀

---
+++ assets/Textarea-B1MLECH5.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as i}from"./index-DSkyVWTJ.js";import{M as a,T as m,d as n,C as s,e as p}from"./index-DL-lpZaJ.js";import"./Avatar-4zGhZtDZ.js";import"./Badge-DdnwakjS.js";import"./HoverCard-DfiUVqdc.js";import"./Input-BOF_Ax6K.js";import"./Separator-C2dyLaF-.js";import"./Text-Ut-2NFPv.js";import"./Resizable-D6r50leV.js";import"./Icon-Cl8ckYc1.js";import"./Textarea-BcMsqAZI.js";import"./Label-CGvHXEnN.js";import{S as x,B as c}from"./Textarea.stories-CCYi0Cem.js";import"./index-CTjT7uj6.js";import"./iframe-D5qEJ7N4.js";import"../sb-preview/runtime.js";import"./index-B7lOVQLg.js";import"./index-DdAkmXN0.js";import"./extends-CCbyfPlC.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-CjhMTvA2.js";import"./index-bugisSBR.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function e(o){const r={h2:"h2",h4:"h4",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{of:x}),`
+`,t.jsx(m,{}),`
+`,t.jsx(r.h4,{id:"displays-a-form-textarea-or-a-component-that-looks-like-a-textarea",children:"Displays a form textarea or a component that looks like a textarea."}),`
+`,t.jsx(n,{dark:!0,language:"tsx",code:`
+import { Textarea } from '@0xintuition/1ui'
+
+<Textarea />
+`}),`
+`,t.jsx(s,{of:c,sourceState:"none"}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(p,{})]})}function z(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(e,{...o})}):e(o)}export{z as default};
