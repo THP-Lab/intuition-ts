2c23b8ef0d1b1ff563d2e2961eb53f18fc252a89
alexander-mann <alexandercmann@gmail.com>
2024-06-06 18:01:06 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@98f0354c5080b5723e943adf8efadcde2eb95a5b 🚀

---
+++ assets/Switch.stories-C92206PL.js
@@ -0,0 +1,7 @@
+import{j as r}from"./jsx-runtime-Cw0GR0a5.js";import{a8 as s}from"./HoverCard-DfiUVqdc.js";import"./Avatar-4zGhZtDZ.js";import"./Badge-DdnwakjS.js";import"./Input-BOF_Ax6K.js";import"./Separator-C2dyLaF-.js";import"./Text-Ut-2NFPv.js";import"./Resizable-D6r50leV.js";import"./Icon-Cl8ckYc1.js";import"./Textarea-BcMsqAZI.js";import{L as m}from"./Label-CGvHXEnN.js";const n={title:"Components/Switch",component:s},e={args:{},render:i=>r.jsxs("div",{className:"flex items-center space-x-3",children:[r.jsx(s,{id:"airplane-mode",...i}),r.jsx(m,{htmlFor:"airplane-mode",children:"Airplane Mode"})]})};var a,o,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {},
+  render: args => <div className="flex items-center space-x-3">
+      <Switch id="airplane-mode" {...args} />
+      <Label htmlFor="airplane-mode">Airplane Mode</Label>
+    </div>
+}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const p=["BasicUsage"],_=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:e,__namedExportsOrder:p,default:n},Symbol.toStringTag,{value:"Module"}));export{e as B,_ as S};
