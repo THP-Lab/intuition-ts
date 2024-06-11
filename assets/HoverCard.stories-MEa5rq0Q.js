43cef8b4381dde68653b04501aee178c14827674
Vitalsine85 <rob@creative-element.net>
2024-06-11 15:13:48 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@16ad6c42b552930d063f9396db78d709dab9297f 🚀

---
+++ assets/HoverCard.stories-MEa5rq0Q.js
@@ -0,0 +1,41 @@
+import{j as a}from"./jsx-runtime-Cw0GR0a5.js";import{A as d,a as l,b as c}from"./Avatar-BtHJmJsO.js";import"./Badge-DdnwakjS.js";import{H as i,u as v,B as m,v as p}from"./HoverCard-B70Z4XjK.js";import"./Input-BOF_Ax6K.js";import"./Separator-CDCDkwLb.js";import{T as e}from"./Text-Ut-2NFPv.js";import"./Resizable-D6r50leV.js";import"./Icon-Cl8ckYc1.js";import"./Textarea-BcMsqAZI.js";import"./Label-Cn83oreb.js";import"./Table-DhRwqTN4.js";import"./ValueDisplay-D0Lul068.js";const x={title:"Components/HoverCard",component:i},r={args:{},render:o=>a.jsx("div",{className:"h-[200px]",children:a.jsxs(i,{...o,children:[a.jsx(v,{asChild:!0,children:a.jsx(m,{variant:"text",children:"@super_dave"})}),a.jsxs(p,{className:"w-60",children:[a.jsxs("div",{className:"flex gap-4",children:[a.jsxs(d,{children:[a.jsx(l,{src:"https://m.media-amazon.com/images/M/MV5BNDhiMWYzMjgtNTRiYi00ZTA3LThlODctNDRkMDk0NzFkMWI3L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTg0MTkzMzA@._V1_.jpg"}),a.jsx(c,{children:"SD"})]}),a.jsxs("div",{children:[a.jsx(e,{variant:"body",children:"Super Dave"}),a.jsx(e,{variant:"caption",className:"text-primary/50",children:"@super_dave"})]})]}),a.jsx(e,{variant:"caption",children:"I am Super Dave, the most super Dave you'll ever meet."}),a.jsxs("div",{className:"flex gap-10",children:[a.jsxs("div",{children:[a.jsx(e,{variant:"body",children:"9122"}),a.jsx(e,{variant:"caption",className:"text-primary/50",children:"followers"})]}),a.jsxs("div",{children:[a.jsx(e,{variant:"body",children:"1239"}),a.jsx(e,{variant:"caption",className:"text-primary/50",children:"following"})]})]})]})]})})};var t,n,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {},
+  render: args => <div className="h-[200px]">
+      <HoverCard {...args}>
+        <HoverCardTrigger asChild>
+          <Button variant="text">@super_dave</Button>
+        </HoverCardTrigger>
+        <HoverCardContent className="w-60">
+          <div className="flex gap-4">
+            <Avatar>
+              <AvatarImage src="https://m.media-amazon.com/images/M/MV5BNDhiMWYzMjgtNTRiYi00ZTA3LThlODctNDRkMDk0NzFkMWI3L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTg0MTkzMzA@._V1_.jpg" />
+              <AvatarFallback>SD</AvatarFallback>
+            </Avatar>
+            <div>
+              <Text variant="body">Super Dave</Text>
+              <Text variant="caption" className="text-primary/50">
+                @super_dave
+              </Text>
+            </div>
+          </div>
+          <Text variant="caption">
+            I am Super Dave, the most super Dave you&apos;ll ever meet.
+          </Text>
+          <div className="flex gap-10">
+            <div>
+              <Text variant="body">9122</Text>
+              <Text variant="caption" className="text-primary/50">
+                followers
+              </Text>
+            </div>
+            <div>
+              <Text variant="body">1239</Text>
+              <Text variant="caption" className="text-primary/50">
+                following
+              </Text>
+            </div>
+          </div>
+        </HoverCardContent>
+      </HoverCard>
+    </div>
+}`,...(s=(n=r.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const g=["BasicUsage"],A=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:r,__namedExportsOrder:g,default:x},Symbol.toStringTag,{value:"Module"}));export{r as B,A as S};
