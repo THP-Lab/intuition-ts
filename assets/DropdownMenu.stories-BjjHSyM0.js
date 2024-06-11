6f3de00c8aae7c116bbeefe69677e718d0e0e8c3
alexander-mann <alexandercmann@gmail.com>
2024-06-11 19:45:00 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@0a2f56a14ca1f93a2bba1b5b5d214a62e43a9c44 🚀

---
+++ assets/DropdownMenu.stories-BjjHSyM0.js
@@ -0,0 +1,16 @@
+import{j as n}from"./jsx-runtime-Cw0GR0a5.js";import"./Avatar-BtHJmJsO.js";import"./Badge-DdnwakjS.js";import{o as s,p as d,B as a,q as u,r as m,s as c,t as e}from"./Identity-Ci4Ffj2_.js";import"./Input-BOF_Ax6K.js";import"./Separator-CDCDkwLb.js";import"./Text-Ut-2NFPv.js";import"./Resizable-D6r50leV.js";import"./Icon-B74lSK-X.js";import"./Textarea-BcMsqAZI.js";import"./Label-Cn83oreb.js";import"./Table-DhRwqTN4.js";import"./ValueDisplay-D0Lul068.js";const l={title:"Components/DropdownMenu",component:s},o={args:{},render:i=>n.jsxs(s,{...i,children:[n.jsx(d,{children:n.jsx(a,{size:"lg",children:"Open menu"})}),n.jsxs(u,{children:[n.jsx(m,{children:"My Account"}),n.jsx(c,{}),n.jsx(e,{children:"Profile"}),n.jsx(e,{children:"Billing"}),n.jsx(e,{children:"Team"}),n.jsx(e,{children:"Subscription"})]})]})};var r,t,p;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {},
+  render: args => <DropdownMenu {...args}>
+      <DropdownMenuTrigger>
+        <Button size="lg">Open menu</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+}`,...(p=(t=o.parameters)==null?void 0:t.docs)==null?void 0:p.source}}};const M=["BasicUsage"],O=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:o,__namedExportsOrder:M,default:l},Symbol.toStringTag,{value:"Module"}));export{o as B,O as S};
---
+++ assets/Tooltip-Cr-NEcnl.js
@@ -0,0 +1,26 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as r}from"./index-DSkyVWTJ.js";import{M as n,T as p,d as m,C as s}from"./index-DoZAaXoK.js";import"./Avatar-BtHJmJsO.js";import"./Badge-DdnwakjS.js";import{B as a}from"./Identity-Ci4Ffj2_.js";import"./Input-BOF_Ax6K.js";import"./Separator-CDCDkwLb.js";import"./Text-Ut-2NFPv.js";import"./Resizable-D6r50leV.js";import"./Icon-B74lSK-X.js";import"./Textarea-BcMsqAZI.js";import"./Label-Cn83oreb.js";import"./Table-DhRwqTN4.js";import"./ValueDisplay-D0Lul068.js";import{S as l,B as c}from"./Tooltip.stories-Dxx6UkX4.js";import"./index-CTjT7uj6.js";import"./iframe-DIRCjE3b.js";import"../sb-preview/runtime.js";import"./index-D62jTv_I.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-BPrHwcGF.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-bugisSBR.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function i(t){const e={h4:"h4",...r(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{of:l}),`
+`,o.jsx(p,{}),`
+`,o.jsx(e.h4,{id:"a-popup-that-displays-information-related-to-an-element-when-the-element-receives-keyboard-focus-or-the-mouse-hovers-over-it",children:"A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it."}),`
+`,o.jsx(a,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/tooltip#api-reference","_blank"),children:"API Reference"}),`
+`,o.jsx(m,{dark:!0,language:"tsx",code:`
+import { 
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger, 
+} from '@0xintuition/1ui'
+
+<TooltipProvider>
+  <Tooltip>
+    <TooltipTrigger asChild>
+      <Button variant="ghost" size="lg">
+        Hover
+      </Button>
+    </TooltipTrigger>
+    <TooltipContent>
+      <Text variant="body">Some text</Text>
+    </TooltipContent>
+  </Tooltip>
+</TooltipProvider>
+`}),`
+`,o.jsx(s,{of:c,sourceState:"none"})]})}function U(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(i,{...t})}):i(t)}export{U as default};
