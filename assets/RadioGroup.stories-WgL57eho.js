1d7f0d9dae88adcb6c092203fd6f54047df40137
Vitalsine85 <rob@creative-element.net>
2024-06-11 14:13:51 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@d84dfc69e65216fd12c36f4d347f472c5513a1ca 🚀

---
+++ assets/RadioGroup.stories-WgL57eho.js
@@ -0,0 +1,13 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import"./Avatar-BtHJmJsO.js";import"./Badge-DdnwakjS.js";import{R as s,Q as t}from"./HoverCard-BtE0Zi4W.js";import"./Input-BOF_Ax6K.js";import"./Separator-CDCDkwLb.js";import"./Text-Ut-2NFPv.js";import"./Resizable-D6r50leV.js";import"./Icon-Cl8ckYc1.js";import"./Textarea-BcMsqAZI.js";import{L as r}from"./Label-Cn83oreb.js";import"./Table-DhRwqTN4.js";import"./ValueDisplay-D0Lul068.js";const m={title:"Components/RadioGroup",component:s},e={args:{},render:p=>o.jsxs(s,{defaultValue:"option-one",...p,children:[o.jsxs("div",{className:"flex items-center space-x-2",children:[o.jsx(t,{value:"option-one",id:"option-one"}),o.jsx(r,{htmlFor:"option-one",children:"Option One"})]}),o.jsxs("div",{className:"flex items-center space-x-2",children:[o.jsx(t,{value:"option-two",id:"option-two"}),o.jsx(r,{htmlFor:"option-two",children:"Option Two"})]})]})};var i,n,a;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {},
+  render: args => <RadioGroup defaultValue="option-one" {...args}>
+      <div className="flex items-center space-x-2">
+        <RadioGroupItem value="option-one" id="option-one" />
+        <Label htmlFor="option-one">Option One</Label>
+      </div>
+      <div className="flex items-center space-x-2">
+        <RadioGroupItem value="option-two" id="option-two" />
+        <Label htmlFor="option-two">Option Two</Label>
+      </div>
+    </RadioGroup>
+}`,...(a=(n=e.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};const l=["BasicUsage"],G=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:e,__namedExportsOrder:l,default:m},Symbol.toStringTag,{value:"Module"}));export{e as B,G as S};
---
+++ assets/Textarea-Yot4eQhm.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as i}from"./index-DSkyVWTJ.js";import{M as a,T as m,d as n,C as s,e as p}from"./index-J2WzYzYa.js";import"./Avatar-BtHJmJsO.js";import"./Badge-DdnwakjS.js";import"./HoverCard-BtE0Zi4W.js";import"./Input-BOF_Ax6K.js";import"./Separator-CDCDkwLb.js";import"./Text-Ut-2NFPv.js";import"./Resizable-D6r50leV.js";import"./Icon-Cl8ckYc1.js";import"./Textarea-BcMsqAZI.js";import"./Label-Cn83oreb.js";import"./Table-DhRwqTN4.js";import"./ValueDisplay-D0Lul068.js";import{S as x,B as c}from"./Textarea.stories-CCYi0Cem.js";import"./index-CTjT7uj6.js";import"./iframe-DWbv2EV2.js";import"../sb-preview/runtime.js";import"./index-D62jTv_I.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-BPrHwcGF.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-bugisSBR.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function e(o){const r={h2:"h2",h4:"h4",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{of:x}),`
+`,t.jsx(m,{}),`
+`,t.jsx(r.h4,{id:"displays-a-form-textarea-or-a-component-that-looks-like-a-textarea",children:"Displays a form textarea or a component that looks like a textarea."}),`
+`,t.jsx(n,{dark:!0,language:"tsx",code:`
+import { Textarea } from '@0xintuition/1ui'
+
+<Textarea />
+`}),`
+`,t.jsx(s,{of:c,sourceState:"none"}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(p,{})]})}function G(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(e,{...o})}):e(o)}export{G as default};
