9c38cae362d487d57f2518f15b82647aa828e8fa
alexander-mann <alexandercmann@gmail.com>
2024-06-13 15:30:56 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@2a4014359069d645849ef96f3be3e06f36ba4b79 🚀

---
+++ assets/Popover.stories-rs61FwgO.js
@@ -0,0 +1,14 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import"./Avatar-BqFJ6ENc.js";import"./Badge-BlZKMVE_.js";import{N as i,O as a,B as m,Q as l}from"./Identity-B5T8pjat.js";import"./Input-CFrL2YAq.js";import"./Separator-ho1Nl6GX.js";import{T as t}from"./Text-BJFUDWxl.js";import"./Resizable-DNd95Y6K.js";import"./Icon-Ba3_jQfB.js";import"./Textarea-DpSkS59a.js";import"./Label-FZHk6kRz.js";import"./Table-Cj0PeqN2.js";import"./ValueDisplay-CeuIe0pc.js";import"./SegmentedControl-DewWK8dB.js";const c={title:"Components/Popover",component:i,argTypes:{side:{describe:"Side the content will open on",table:{type:{summary:"string"},defaultValue:{summary:"bottom"}},options:["top","right","bottom","left"],control:"select"},align:{describe:"Where to align content to trigger",table:{type:{summary:"string"},defaultValue:{summary:"center"}},options:["start","center","end"],control:"select"}}},e={render:p=>o.jsxs(i,{children:[o.jsx(a,{children:o.jsx(m,{size:"lg",children:"Open popover"})}),o.jsxs(l,{...p,children:[o.jsx(t,{variant:"bodyLarge",children:"Popover Content"}),o.jsx(t,{variant:"body",children:"Here is some very long content to test out this popover. It is good to see how it looks with lots of text!"})]})]})};var r,n,s;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: args => <Popover>
+      <PopoverTrigger>
+        <Button size="lg">Open popover</Button>
+      </PopoverTrigger>
+      <PopoverContent {...args}>
+        <Text variant="bodyLarge">Popover Content</Text>
+        <Text variant="body">
+          Here is some very long content to test out this popover. It is good to
+          see how it looks with lots of text!
+        </Text>
+      </PopoverContent>
+    </Popover>
+}`,...(s=(n=e.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const d=["BasicUsage"],S=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:e,__namedExportsOrder:d,default:c},Symbol.toStringTag,{value:"Module"}));export{e as B,S};
--- assets/Switch.stories-Q-EO-5i8.js
+++
@@ -1,7 +0,0 @@
-import{j as r}from"./jsx-runtime-Cw0GR0a5.js";import{a9 as s}from"./Identity-DFlxxIp7.js";import"./Avatar-BtHJmJsO.js";import"./Badge-DdnwakjS.js";import"./Input-BOF_Ax6K.js";import"./Separator-CDCDkwLb.js";import"./Text-Ut-2NFPv.js";import"./Resizable-D6r50leV.js";import"./Icon-B74lSK-X.js";import"./Textarea-BcMsqAZI.js";import{L as m}from"./Label-Cn83oreb.js";import"./Table-DhRwqTN4.js";import"./ValueDisplay-D0Lul068.js";import"./SegmentedControl-DECnSklO.js";const p={title:"Components/Switch",component:s},e={args:{},render:i=>r.jsxs("div",{className:"flex items-center space-x-3",children:[r.jsx(s,{id:"airplane-mode",...i}),r.jsx(m,{htmlFor:"airplane-mode",children:"Airplane Mode"})]})};var o,t,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {},
-  render: args => <div className="flex items-center space-x-3">
-      <Switch id="airplane-mode" {...args} />
-      <Label htmlFor="airplane-mode">Airplane Mode</Label>
-    </div>
-}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const n=["BasicUsage"],w=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:e,__namedExportsOrder:n,default:p},Symbol.toStringTag,{value:"Module"}));export{e as B,w as S};
