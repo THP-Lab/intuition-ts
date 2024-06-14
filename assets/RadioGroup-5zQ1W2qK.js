c2cef7c689ab25a73d263818d7c9e43e711a8d8e
Vitalsine85 <rob@creative-element.net>
2024-06-14 14:57:29 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@4869816be2e7bcb524cfc9865bddc4d85258969e 🚀

---
+++ assets/RadioGroup-5zQ1W2qK.js
@@ -0,0 +1,19 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as n}from"./index-DSkyVWTJ.js";import{M as r,T as a,d as m,C as p}from"./index-CAjH1--A.js";import"./Avatar-BqFJ6ENc.js";import"./Badge-BlZKMVE_.js";import{B as s}from"./Identity-BmlzQzyN.js";import"./Input-CFrL2YAq.js";import"./Separator-ho1Nl6GX.js";import"./Text-BJFUDWxl.js";import"./Resizable-DNd95Y6K.js";import"./Icon-Ba3_jQfB.js";import"./Textarea-DpSkS59a.js";import"./Label-FZHk6kRz.js";import"./Table-Cj0PeqN2.js";import"./ValueDisplay-CeuIe0pc.js";import"./SegmentedControl-DewWK8dB.js";import{S as c,B as u}from"./RadioGroup.stories-CCMzDdf2.js";import"./index-CTjT7uj6.js";import"./iframe-DleLBgbQ.js";import"../sb-preview/runtime.js";import"./index-D62jTv_I.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-BPrHwcGF.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-B7U7eni1.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function i(t){const e={h4:"h4",...n(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(r,{of:c}),`
+`,o.jsx(a,{}),`
+`,o.jsx(e.h4,{id:"a-set-of-checkable-buttonsknown-as-radio-buttonswhere-no-more-than-one-of-the-buttons-can-be-checked-at-a-time",children:"A set of checkable buttons—known as radio buttons—where no more than one of the buttons can be checked at a time."}),`
+`,o.jsx(s,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/radio-group#api-reference","_blank"),children:"API Reference"}),`
+`,o.jsx(m,{dark:!0,language:"tsx",code:`
+import { RadioGroup } from '@0xintuition/1ui'
+
+<RadioGroup defaultValue="option-one">
+  <div className="flex items-center space-x-2">
+    <RadioGroupItem value="option-one" id="option-one" />
+    <Label htmlFor="option-one">Option One</Label>
+  </div>
+  <div className="flex items-center space-x-2">
+    <RadioGroupItem value="option-two" id="option-two" />
+    <Label htmlFor="option-two">Option Two</Label>
+  </div>
+</RadioGroup>
+`}),`
+`,o.jsx(p,{of:u,sourceState:"none"})]})}function U(t={}){const{wrapper:e}={...n(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(i,{...t})}):i(t)}export{U as default};
