e89eb39eac95d7a00b8d5e345fc4dc905d6827e2
alexander-mann <alexandercmann@gmail.com>
2024-06-14 15:00:43 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@6514e0cdf35c940caaad0d3abac9c9dc285112a0 🚀

---
+++ assets/Accordion-CLTOoJ6Z.js
@@ -0,0 +1,37 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as r}from"./index-DSkyVWTJ.js";import{M as n,T as c,d as s,C as a,e as m}from"./index-CwzE_U0-.js";import"./Avatar-BqFJ6ENc.js";import"./Badge-BlZKMVE_.js";import{B as d}from"./Identity-BmlzQzyN.js";import"./Input-CFrL2YAq.js";import"./Separator-ho1Nl6GX.js";import"./Text-BJFUDWxl.js";import"./Resizable-DNd95Y6K.js";import"./Icon-Ba3_jQfB.js";import"./Textarea-DpSkS59a.js";import"./Label-FZHk6kRz.js";import"./Table-Cj0PeqN2.js";import"./ValueDisplay-CeuIe0pc.js";import"./SegmentedControl-DewWK8dB.js";import{S as p,B as l}from"./Accordion.stories-CNXMGxU0.js";import"./index-CTjT7uj6.js";import"./iframe-B3kDBMWy.js";import"../sb-preview/runtime.js";import"./index-D62jTv_I.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-BPrHwcGF.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-B7U7eni1.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function i(t){const e={h2:"h2",h4:"h4",...r(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{of:p}),`
+`,o.jsx(c,{}),`
+`,o.jsx(e.h4,{id:"a-vertically-stacked-set-of-interactive-headings-that-each-reveal-a-section-of-content",children:"A vertically stacked set of interactive headings that each reveal a section of content."}),`
+`,o.jsx(d,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/accordion#api-reference","_blank"),children:"API Reference"}),`
+`,o.jsx(s,{dark:!0,language:"tsx",code:`
+import { 
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger, 
+} from '@0xintuition/1ui'
+
+<Accordion type="single" collapsible className="w-full">
+  <AccordionItem value="item-1">
+    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+    <AccordionContent>
+      Yes. It adheres to the WAI-ARIA design pattern.
+    </AccordionContent>
+  </AccordionItem>
+  <AccordionItem value="item-2">
+    <AccordionTrigger>Is it styled?</AccordionTrigger>
+    <AccordionContent>
+      Yes. It comes with default styles that matches the other components&apos;
+      aesthetic.
+    </AccordionContent>
+  </AccordionItem>
+  <AccordionItem value="item-3">
+    <AccordionTrigger>Is it animated?</AccordionTrigger>
+    <AccordionContent>
+      Yes. It&apos;s animated by default, but you can disable it if you prefer.
+    </AccordionContent>
+  </AccordionItem>
+</Accordion>
+`}),`
+`,o.jsx(a,{of:l,sourceState:"none"}),`
+`,o.jsx(e.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(m,{})]})}function q(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(i,{...t})}):i(t)}export{q as default};
---
+++ assets/Identity-BcaM_5yp.js
@@ -0,0 +1,19 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as s}from"./index-DSkyVWTJ.js";import{M as m,T as p,d as n,C as o,e as a}from"./index-CwzE_U0-.js";import"./Avatar-BqFJ6ENc.js";import"./Badge-BlZKMVE_.js";import"./Identity-BmlzQzyN.js";import"./Input-CFrL2YAq.js";import"./Separator-ho1Nl6GX.js";import"./Text-BJFUDWxl.js";import"./Resizable-DNd95Y6K.js";import"./Icon-Ba3_jQfB.js";import"./Textarea-DpSkS59a.js";import"./Label-FZHk6kRz.js";import"./Table-Cj0PeqN2.js";import"./ValueDisplay-CeuIe0pc.js";import"./SegmentedControl-DewWK8dB.js";import{S as d,B as c,U as u}from"./Identity.stories-CR0f1cnD.js";import"./index-CTjT7uj6.js";import"./iframe-B3kDBMWy.js";import"../sb-preview/runtime.js";import"./index-D62jTv_I.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-BPrHwcGF.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-B7U7eni1.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function e(r){const i={h2:"h2",h3:"h3",h4:"h4",...s(),...r.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{of:d}),`
+`,t.jsx(p,{}),`
+`,t.jsx(i.h4,{id:"a-clickable-component-representing-an-indentity",children:"A clickable component representing an indentity."}),`
+`,t.jsx(n,{dark:!0,language:"tsx",code:`
+import { Identity } from '@0xintuition/1ui'
+
+<Identity>identity name</Identity>
+`}),`
+`,t.jsx(o,{of:c,sourceState:"none"}),`
+`,t.jsx(i.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(a,{}),`
+`,t.jsx(i.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(i.h3,{id:"user",children:"User"}),`
+`,t.jsx(n,{dark:!0,language:"tsx",code:`
+import { Identity } from '@0xintuition/1ui'
+
+<Identity variant="user">super dave</Identity>
+`}),`
+`,t.jsx(o,{of:u,sourceState:"none"})]})}function G(r={}){const{wrapper:i}={...s(),...r.components};return i?t.jsx(i,{...r,children:t.jsx(e,{...r})}):e(r)}export{G as default};
