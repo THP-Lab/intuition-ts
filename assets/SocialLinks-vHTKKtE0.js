7cfd380316c9573721d10c1fe3a5f0e5e2287f9e
alexander-mann <alexandercmann@gmail.com>
2024-06-17 19:39:54 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@0b8c59784a6f714201922c65d876af49f9e0c21f 🚀

---
+++ assets/SocialLinks-vHTKKtE0.js
@@ -0,0 +1,25 @@
+import{j as i}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as r}from"./index-DSkyVWTJ.js";import{M as s,T as t,d as n,C as m}from"./index-DIRRHfOj.js";import{S as p,B as c}from"./SocialLinks.stories-CyKt7Jyl.js";import"./index-CTjT7uj6.js";import"./iframe-BKMQc0KA.js";import"../sb-preview/runtime.js";import"./index-CDJ69J4A.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-BPrHwcGF.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./Tags-BY9hcarK.js";import"./Avatar-BqFJ6ENc.js";import"./index-B7U7eni1.js";import"./palette-saq09hvB.js";import"./Badge-BlZKMVE_.js";import"./index-Bb4qSo10.js";import"./Resizable-DNd95Y6K.js";import"./Icon-BTTLXkGj.js";import"./Input-CFrL2YAq.js";import"./Label-FZHk6kRz.js";import"./SegmentedControl-DewWK8dB.js";import"./Separator-ho1Nl6GX.js";import"./Table-Cj0PeqN2.js";import"./Text-BJFUDWxl.js";import"./Textarea-DpSkS59a.js";import"./ValueDisplay-CeuIe0pc.js";function a(o){const e={h4:"h4",...r(),...o.components};return i.jsxs(i.Fragment,{children:[i.jsx(s,{of:p}),`
+`,i.jsx(t,{}),`
+`,i.jsx(e.h4,{id:"displays-a-users-social-media-connections",children:"Displays a user's social media connections."}),`
+`,i.jsx(n,{dark:!0,language:"tsx",code:`
+import { 
+  SocialLinks
+  SocialLinksBadge,
+  SocialLinksBadges,
+  SocialLinksButton, 
+} from '@0xintuition/1ui'
+
+<SocialLinks>
+  <SocialLinksBadges>
+    <SocialLinksBadge isVerified platform="discord" username="@superdave" />
+    <SocialLinksBadge isVerified platform="x" username="@superdave" />
+    <SocialLinksBadge platform="farcaster" username="@superdave" />
+    <SocialLinksBadge platform="lens" username="@superdave" />
+    <SocialLinksBadge isVerified platform="calendly" username="@superdave" />
+    <SocialLinksBadge platform="github" username="@superdave" />
+    <SocialLinksBadge platform="medium" username="@superdave" />
+  </SocialLinksBadges>
+  <SocialLinksButton onClick={() => doSomething()} />
+</SocialLinks>
+`}),`
+`,i.jsx(m,{of:c,sourceState:"none"})]})}function G(o={}){const{wrapper:e}={...r(),...o.components};return e?i.jsx(e,{...o,children:i.jsx(a,{...o})}):a(o)}export{G as default};
---
+++ assets/preview-DQUJiYuD.js
@@ -0,0 +1,7 @@
+import{_ as a}from"./iframe-BKMQc0KA.js";import"../sb-preview/runtime.js";const{global:s}=__STORYBOOK_MODULE_GLOBAL__;var _=Object.entries(s.TAGS_OPTIONS??{}).reduce((e,r)=>{let[t,o]=r;return o.excludeFromDocsStories&&(e[t]=!0),e},{}),d={docs:{renderer:async()=>{let{DocsRenderer:e}=await a(()=>import("./DocsRenderer-K4EAMTCU-CbwvBYzR.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11]),import.meta.url);return new e},stories:{filter:e=>{var r;return(e.tags||[]).filter(t=>_[t]).length===0&&!((r=e.parameters.docs)!=null&&r.disable)}}}};export{d as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-K4EAMTCU-CbwvBYzR.js","./iframe-BKMQc0KA.js","./index-CTjT7uj6.js","./react-18-I1chspnn.js","./index-BhgyLgKK.js","./index-DIRRHfOj.js","./index-CDJ69J4A.js","./index-BI02G6dj.js","./index-BPrHwcGF.js","./index-DXimoRZY.js","./index-Bx4XDAbk.js","./index-DrFu-skq.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
