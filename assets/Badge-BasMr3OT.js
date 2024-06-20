ead8765f7909110d631f0ca695f2c882a3de1f85
alexander-mann <alexandercmann@gmail.com>
2024-06-20 17:23:57 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@dac5ca3c439320072a45332eeed5b1b4137ed27d 🚀

---
+++ assets/Badge-BasMr3OT.js
@@ -0,0 +1,27 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as a}from"./index-DSkyVWTJ.js";import{M as s,T as m,d as e,C as i,e as p}from"./index-BitBFC-o.js";import"./QuestHeaderCard-CeV-yLEm.js";import"./Avatar-BUaynhXT.js";import"./Badge-D2xxBGMY.js";import"./Button-CzEHE8a5.js";import"./Icon.types-DkS0gbR0.js";import"./Input-DFWTvv6T.js";import"./Label-ByAlek89.js";import"./PieChart-DH4cDbJQ.js";import"./PositionCardStaked-BOl4kzrD.js";import"./ProfileCard-CY9m7nXW.js";import"./Resizable-Dk6Cp6at.js";import"./SegmentedControl-DbX5wOQO.js";import"./Separator-Ct33SRfh.js";import"./Table-D9p853bW.js";import"./Text-ANaTnboW.js";import"./Textarea-Ds9ZbOCk.js";import"./ValueDisplay-2lt2w_5b.js";import{S as d,B as c,a as u,D as x,O as l}from"./Badge.stories-1HjAutAG.js";import"./index-CTjT7uj6.js";import"./iframe-DPN_0Csi.js";import"../sb-preview/runtime.js";import"./index-D3WfGLRt.js";import"./index-DBez60WA.js";import"./index-BPrHwcGF.js";import"./index-BSh8g_S6.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-CNBET2ao.js";import"./palette-saq09hvB.js";import"./createLucideIcon-BfM7wYCo.js";function n(r){const o={h2:"h2",h3:"h3",h4:"h4",...a(),...r.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:d}),`
+`,t.jsx(m,{}),`
+`,t.jsx(o.h4,{id:"displays-a-badge-or-a-component-that-looks-like-a-badge",children:"Displays a badge or a component that looks like a badge."}),`
+`,t.jsx(e,{dark:!0,language:"tsx",code:`
+import { Badge } from '@0xintuition/1ui'
+
+<Badge>Badge</Badge>
+`}),`
+`,t.jsx(i,{of:c,sourceState:"none"}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(p,{}),`
+`,t.jsx(o.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(o.h3,{id:"secondary",children:"Secondary"}),`
+`,t.jsx(e,{dark:!0,language:"tsx",code:`
+<Badge variant="secondary">
+`}),`
+`,t.jsx(i,{of:u,sourceState:"none"}),`
+`,t.jsx(o.h3,{id:"destructive",children:"Destructive"}),`
+`,t.jsx(e,{dark:!0,language:"tsx",code:`
+<Badge variant="destructive">
+`}),`
+`,t.jsx(i,{of:x,sourceState:"none"}),`
+`,t.jsx(o.h3,{id:"outline",children:"Outline"}),`
+`,t.jsx(e,{dark:!0,language:"tsx",code:`
+<Badge variant="outline">
+`}),`
+`,t.jsx(i,{of:l,sourceState:"none"})]})}function Y(r={}){const{wrapper:o}={...a(),...r.components};return o?t.jsx(o,{...r,children:t.jsx(n,{...r})}):n(r)}export{Y as default};
