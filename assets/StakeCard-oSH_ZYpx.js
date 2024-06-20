41517239784ca2e8189a0a73e2309971fff55d56
alexander-mann <alexandercmann@gmail.com>
2024-06-20 20:32:51 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@962e74aeb997d0f5cefd05d5102d3ea0860bffda 🚀

---
+++ assets/StakeCard-oSH_ZYpx.js
@@ -0,0 +1,16 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as m}from"./index-DSkyVWTJ.js";import{M as s,T as e,d as p,C as n,e as a}from"./index-BjalGFmG.js";import{S as c,B as d}from"./StakeCard.stories-C1G0aNH3.js";import"./index-CTjT7uj6.js";import"./iframe-Dpk_j2dA.js";import"../sb-preview/runtime.js";import"./index-D3WfGLRt.js";import"./index-DBez60WA.js";import"./index-BPrHwcGF.js";import"./index-BSh8g_S6.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./InfoCard-7pwmEdEs.js";import"./Avatar-BUaynhXT.js";import"./index-CNBET2ao.js";import"./palette-saq09hvB.js";import"./Badge-D2xxBGMY.js";import"./index-Bb4qSo10.js";import"./Button-CzEHE8a5.js";import"./createLucideIcon-BfM7wYCo.js";import"./Icon.types-DkS0gbR0.js";import"./Input-DFWTvv6T.js";import"./Label-ByAlek89.js";import"./PieChart-DH4cDbJQ.js";import"./PositionCardStaked-BOl4kzrD.js";import"./Text-ANaTnboW.js";import"./ProfileCard--cviTZvn.js";import"./Resizable-Dk6Cp6at.js";import"./SegmentedControl-DbX5wOQO.js";import"./Separator-Ct33SRfh.js";import"./Table-D9p853bW.js";import"./Textarea-Ds9ZbOCk.js";import"./ValueDisplay-2lt2w_5b.js";function i(t){const r={h2:"h2",h4:"h4",...m(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(s,{of:c}),`
+`,o.jsx(e,{}),`
+`,o.jsx(r.h4,{id:"a-display-card-to-show-a-users-stake-position",children:"A display card to show a user's stake position."}),`
+`,o.jsx(p,{dark:!0,language:"tsx",code:`
+import { StakeCard } from '@0xintuition/1ui'
+
+<StakeCard
+  tvl="4.928 ETH"
+  holders={69}
+  onBuyClick={() => doSomething()}
+  onViewAllClick={() => doSomething()}
+/>
+`}),`
+`,o.jsx(n,{of:d,sourceState:"none"}),`
+`,o.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(a,{})]})}function O(t={}){const{wrapper:r}={...m(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(i,{...t})}):i(t)}export{O as default};
