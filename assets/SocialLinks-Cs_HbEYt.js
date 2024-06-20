4bc84029394e997977c3bb96dc1c239016501e08
alexander-mann <alexandercmann@gmail.com>
2024-06-20 14:46:06 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@c321dc9b7ce3ac11e1d9f139eb948827206d8aae 🚀

---
+++ assets/SocialLinks-Cs_HbEYt.js
@@ -0,0 +1,25 @@
+import{j as i}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as t}from"./index-DSkyVWTJ.js";import{M as a,T as s,d as n,C as m}from"./index-CbCKvk67.js";import{S as p,B as c}from"./SocialLinks.stories-DYCBK6nZ.js";import"./index-CTjT7uj6.js";import"./iframe-BuG3qZgK.js";import"../sb-preview/runtime.js";import"./index-D3WfGLRt.js";import"./index-DBez60WA.js";import"./index-BPrHwcGF.js";import"./index-BSh8g_S6.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./Tabs-DVbk2V3D.js";import"./Avatar-BUaynhXT.js";import"./index-CNBET2ao.js";import"./palette-saq09hvB.js";import"./Badge-D2xxBGMY.js";import"./index-Bb4qSo10.js";import"./Button-DtBxy_Zq.js";import"./createLucideIcon-BfM7wYCo.js";import"./Icon.types-DkS0gbR0.js";import"./Input-DFWTvv6T.js";import"./Label-ByAlek89.js";import"./PieChart-DH4cDbJQ.js";import"./PositionCardStaked-oeI8zpAm.js";import"./Text-ANaTnboW.js";import"./ProfileCard-CY9m7nXW.js";import"./Resizable-Dk6Cp6at.js";import"./SegmentedControl-Bb5mVLPJ.js";import"./Separator-Ct33SRfh.js";import"./Table-D9p853bW.js";import"./Textarea-Ds9ZbOCk.js";import"./ValueDisplay-2lt2w_5b.js";function r(o){const e={h4:"h4",...t(),...o.components};return i.jsxs(i.Fragment,{children:[i.jsx(a,{of:p}),`
+`,i.jsx(s,{}),`
+`,i.jsx(e.h4,{id:"displays-a-users-social-media-connections",children:"Displays a user's social media connections."}),`
+`,i.jsx(n,{dark:!0,language:"tsx",code:`
+import { 
+  SocialLinks
+  SocialLinksBadge,
+  SocialLinksBadges,
+  SocialLinksButton, 
+} from '@0xintuition/1ui'
+
+<SocialLinks>
+  <SocialLinksBadges>
+    <SocialLinksBadge isVerified platform="discord" username="@superdave" />
+    <SocialLinksBadge isVerified platform="x" username="@superdave" />
+    <SocialLinksBadge platform="farcaster" username="@superdave" />
+    <SocialLinksBadge platform="lens" username="@superdave" />
+    <SocialLinksBadge isVerified platform="calendly" username="@superdave" />
+    <SocialLinksBadge platform="github" username="@superdave" />
+    <SocialLinksBadge platform="medium" username="@superdave" />
+  </SocialLinksBadges>
+  <SocialLinksButton onClick={() => doSomething()} />
+</SocialLinks>
+`}),`
+`,i.jsx(m,{of:c,sourceState:"none"})]})}function O(o={}){const{wrapper:e}={...t(),...o.components};return e?i.jsx(e,{...o,children:i.jsx(r,{...o})}):r(o)}export{O as default};
