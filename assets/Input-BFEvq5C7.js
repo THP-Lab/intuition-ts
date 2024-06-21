50578d52bb9f8ebd3d3c7591401963022300f5a0
rahulr8 <37151383+rahulr8@users.noreply.github.com>
2024-06-21 15:57:10 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@e9d442297f3f54de59bc3854df9308c3b91a2b5d 🚀

---
+++ assets/Input-BFEvq5C7.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as m}from"./index-DSkyVWTJ.js";import{M as p,T as n,d as e,C as s,e as a}from"./index-Cl-ITZ4t.js";import"./Tooltip-vTKMDEW3.js";import"./Avatar-BUaynhXT.js";import"./Badge-D2xxBGMY.js";import"./Button-CzEHE8a5.js";import"./Icon.types-DkS0gbR0.js";import"./Input-DFWTvv6T.js";import"./Label-ByAlek89.js";import"./PieChart-B_dtFskq.js";import"./PositionCardStaked-CSnDpiat.js";import"./ProfileCard-Nm5GFBZ4.js";import"./Resizable-Dk6Cp6at.js";import"./SegmentedControl-DbX5wOQO.js";import"./Separator-Ct33SRfh.js";import"./Table-D9p853bW.js";import"./Text-ANaTnboW.js";import"./Textarea-Ds9ZbOCk.js";import"./ValueDisplay-2lt2w_5b.js";import{S as l,B as u}from"./Input.stories-B562hsJ8.js";import"./index-CTjT7uj6.js";import"./iframe-ONcrSGGw.js";import"../sb-preview/runtime.js";import"./index-D3WfGLRt.js";import"./index-DBez60WA.js";import"./index-BPrHwcGF.js";import"./index-BSh8g_S6.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-CNBET2ao.js";import"./palette-saq09hvB.js";import"./createLucideIcon-BfM7wYCo.js";function r(o){const i={h2:"h2",h4:"h4",...m(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(p,{of:l}),`
+`,t.jsx(n,{}),`
+`,t.jsx(i.h4,{id:"displays-a-form-input-field-or-a-component-that-looks-like-an-input-field",children:"Displays a form input field or a component that looks like an input field."}),`
+`,t.jsx(e,{dark:!0,language:"tsx",code:`
+import { Input } from '@0xintuition/1ui'
+
+<Input type="email" placeholder="Email" />
+`}),`
+`,t.jsx(s,{of:u,sourceState:"none"}),`
+`,t.jsx(i.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(a,{})]})}function Q(o={}){const{wrapper:i}={...m(),...o.components};return i?t.jsx(i,{...o,children:t.jsx(r,{...o})}):r(o)}export{Q as default};
---
+++ assets/QuestHeaderCard-DxI-whEU.js
@@ -0,0 +1,17 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as e}from"./index-DSkyVWTJ.js";import{M as m,T as s,d as n,C as p,e as a}from"./index-Cl-ITZ4t.js";import{S as u,B as d}from"./QuestHeaderCard.stories-ClwmWiwh.js";import"./index-CTjT7uj6.js";import"./iframe-ONcrSGGw.js";import"../sb-preview/runtime.js";import"./index-D3WfGLRt.js";import"./index-DBez60WA.js";import"./index-BPrHwcGF.js";import"./index-BSh8g_S6.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./Tooltip-vTKMDEW3.js";import"./Avatar-BUaynhXT.js";import"./index-CNBET2ao.js";import"./palette-saq09hvB.js";import"./Badge-D2xxBGMY.js";import"./index-Bb4qSo10.js";import"./Button-CzEHE8a5.js";import"./createLucideIcon-BfM7wYCo.js";import"./Icon.types-DkS0gbR0.js";import"./ProfileCard-Nm5GFBZ4.js";import"./Text-ANaTnboW.js";import"./Input-DFWTvv6T.js";import"./Label-ByAlek89.js";import"./PieChart-B_dtFskq.js";import"./PositionCardStaked-CSnDpiat.js";import"./Resizable-Dk6Cp6at.js";import"./SegmentedControl-DbX5wOQO.js";import"./Separator-Ct33SRfh.js";import"./Table-D9p853bW.js";import"./Textarea-Ds9ZbOCk.js";import"./ValueDisplay-2lt2w_5b.js";function i(o){const r={h2:"h2",h4:"h4",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{of:u}),`
+`,t.jsx(s,{}),`
+`,t.jsx(r.h4,{id:"a-card-to-show-quest-data-to-a-user",children:"A card to show quest data to a user."}),`
+`,t.jsx(n,{dark:!0,language:"tsx",code:`
+import { QuestHeaderCard } from '@0xintuition/1ui'
+
+<QuestHeaderCard
+  title="Primitive Island"
+  subtitle="Continue your journey."
+  numberOfCompletedQuests={1}
+  totalNumberOfQuests={10}
+  onButtonClick={() => doSomething}
+/>
+`}),`
+`,t.jsx(p,{of:d,sourceState:"none"}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(a,{})]})}function K(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{K as default};
