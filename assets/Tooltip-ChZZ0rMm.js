456e58709c57b1cec1e0a8158d1196ba6fdad223
alexander-mann <alexandercmann@gmail.com>
2024-06-27 18:09:56 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@3ccbde96109c9384f5a00558c557009ceb97221e 🚀

---
+++ assets/Tooltip-ChZZ0rMm.js
@@ -0,0 +1,26 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as r}from"./index-DSkyVWTJ.js";import{M as n,T as p,d as m,C as s}from"./index-CO5cVRTK.js";import{B as a}from"./Tooltip-CuQnIpQU.js";import"./Icon.types-CdnF8hRC.js";import"./Input-ChUPLhCP.js";import"./Label-BE9934Bx.js";import"./PieChart-B_dtFskq.js";import"./Resizable-CYN5O2qj.js";import"./SegmentedControl-C8563B2q.js";import"./Separator-upiNYcQT.js";import"./Table-BRTqj0wF.js";import"./Text-dMYK5Tk0.js";import"./Textarea-D_YQ6kc-.js";import"./ValueDisplay-BU89B4rt.js";import{S as l,B as c}from"./Tooltip.stories-Do8yu7yF.js";import"./index-CTjT7uj6.js";import"./iframe-Ca-1eEfq.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-BIDtd1lf.js";import"./palette-saq09hvB.js";function i(t){const e={h4:"h4",...r(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{of:l}),`
+`,o.jsx(p,{}),`
+`,o.jsx(e.h4,{id:"a-popup-that-displays-information-related-to-an-element-when-the-element-receives-keyboard-focus-or-the-mouse-hovers-over-it",children:"A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it."}),`
+`,o.jsx(a,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/tooltip#api-reference","_blank"),children:"API Reference"}),`
+`,o.jsx(m,{dark:!0,language:"tsx",code:`
+import { 
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger, 
+} from '@0xintuition/1ui'
+
+<TooltipProvider>
+  <Tooltip>
+    <TooltipTrigger asChild>
+      <Button variant="ghost" size="lg">
+        Hover
+      </Button>
+    </TooltipTrigger>
+    <TooltipContent>
+      <Text variant="body">Some text</Text>
+    </TooltipContent>
+  </Tooltip>
+</TooltipProvider>
+`}),`
+`,o.jsx(s,{of:c,sourceState:"none"})]})}function L(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(i,{...t})}):i(t)}export{L as default};
---
+++ assets/Tooltip.stories-Do8yu7yF.js
@@ -0,0 +1,14 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{aQ as n,aR as a,aS as p,B as l,aT as m}from"./Tooltip-CuQnIpQU.js";import"./Icon.types-CdnF8hRC.js";import"./Input-ChUPLhCP.js";import"./Label-BE9934Bx.js";import"./PieChart-B_dtFskq.js";import"./Resizable-CYN5O2qj.js";import"./SegmentedControl-C8563B2q.js";import"./Separator-upiNYcQT.js";import"./Table-BRTqj0wF.js";import{T as d}from"./Text-dMYK5Tk0.js";import"./Textarea-D_YQ6kc-.js";import"./ValueDisplay-BU89B4rt.js";const T={title:"Components/Tooltip",component:n},t={render:s=>o.jsx(a,{...s,children:o.jsxs(n,{children:[o.jsx(p,{asChild:!0,children:o.jsx(l,{variant:"ghost",size:"lg",children:"Hover"})}),o.jsx(m,{children:o.jsx(d,{variant:"body",children:"Some text"})})]})})};var r,e,i;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: args => <TooltipProvider {...args}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button variant="ghost" size="lg">
+            Hover
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <Text variant="body">Some text</Text>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+}`,...(i=(e=t.parameters)==null?void 0:e.docs)==null?void 0:i.source}}};const c=["BasicUsage"],P=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:t,__namedExportsOrder:c,default:T},Symbol.toStringTag,{value:"Module"}));export{t as B,P as S};
