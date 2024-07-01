9bd3f1b3654994fdafbd75e6425cf4dda675a677
Vitalsine85 <rob@creative-element.net>
2024-07-01 19:22:16 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@85bf3b0debeb8a4f7aa0d6af9137d957ba00c45d 🚀

---
+++ assets/Claim.stories-DbVc7H9x.js
@@ -0,0 +1,35 @@
+import{j as n}from"./jsx-runtime-Cw0GR0a5.js";import{C as s,i as d}from"./IdentitySearchCombobox-MHfrFTIe.js";const p={title:"Components/Claim",component:s,argTypes:{subject:{description:"Subject of the claim",table:{type:{summary:"string"}}},predicate:{description:"Predicate of the claim",table:{type:{summary:"string"}}},object:{description:"Object of the claim",table:{type:{summary:"string"}}},size:{description:"Size of component",options:Object.values(d),table:{type:{summary:"string"},defaultValue:{summary:"default"}},control:"select"}}},e={args:{size:"md",disabled:!1,subject:{variant:"non-user",label:"0xintution"},predicate:{variant:"non-user",label:"is really"},object:{variant:"non-user",label:"cool"}},render:m=>n.jsx("div",{className:"w-[500px]",children:n.jsx(s,{...m})})},a={render:()=>n.jsx("div",{className:"w-[500px]",children:n.jsx(s,{size:"md",subject:{variant:"user",label:"Alice",imgSrc:"https://m.media-amazon.com/images/M/MV5BNDhiMWYzMjgtNTRiYi00ZTA3LThlODctNDRkMDk0NzFkMWI3L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTg0MTkzMzA@._V1_.jpg"},predicate:{variant:"non-user",label:"likes"},object:{variant:"non-user",label:"pizza"}})})};var t,r,i;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    size: 'md',
+    disabled: false,
+    subject: {
+      variant: 'non-user',
+      label: '0xintution'
+    },
+    predicate: {
+      variant: 'non-user',
+      label: 'is really'
+    },
+    object: {
+      variant: 'non-user',
+      label: 'cool'
+    }
+  },
+  render: args => <div className="w-[500px]">
+      <Claim {...args} />
+    </div>
+}`,...(i=(r=e.parameters)==null?void 0:r.docs)==null?void 0:i.source}}};var l,o,c;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: () => <div className="w-[500px]">
+      <Claim size="md" subject={{
+      variant: 'user',
+      label: 'Alice',
+      imgSrc: 'https://m.media-amazon.com/images/M/MV5BNDhiMWYzMjgtNTRiYi00ZTA3LThlODctNDRkMDk0NzFkMWI3L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTg0MTkzMzA@._V1_.jpg'
+    }} predicate={{
+      variant: 'non-user',
+      label: 'likes'
+    }} object={{
+      variant: 'non-user',
+      label: 'pizza'
+    }} />
+    </div>
+}`,...(c=(o=a.parameters)==null?void 0:o.docs)==null?void 0:c.source}}};const u=["BasicUsage","User"],j=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:e,User:a,__namedExportsOrder:u,default:p},Symbol.toStringTag,{value:"Module"}));export{e as B,j as S,a as U};
---
+++ assets/QuestHeaderCard-LIjvLTeC.js
@@ -0,0 +1,17 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as i}from"./index-DSkyVWTJ.js";import{M as s,T as n,d as m,C as a,e as p}from"./index-BhKtC9bs.js";import{S as u,B as d}from"./QuestHeaderCard.stories-D7oWQfp6.js";import"./index-CTjT7uj6.js";import"./iframe-BAfZRdWu.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./IdentitySearchCombobox-MHfrFTIe.js";import"./index-Bb4qSo10.js";import"./index-BIDtd1lf.js";import"./palette-saq09hvB.js";import"./Resizable-CYN5O2qj.js";import"./Icon.types-pRyTQzB2.js";import"./Text-dMYK5Tk0.js";import"./Input-ChUPLhCP.js";import"./Label-BE9934Bx.js";import"./PieChart-B_dtFskq.js";import"./SegmentedControl-C8563B2q.js";import"./Separator-upiNYcQT.js";import"./Table-BRTqj0wF.js";import"./Textarea-D_YQ6kc-.js";import"./ValueDisplay-BU89B4rt.js";function e(o){const r={h2:"h2",h4:"h4",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:u}),`
+`,t.jsx(n,{}),`
+`,t.jsx(r.h4,{id:"a-card-to-show-quest-data-to-a-user",children:"A card to show quest data to a user."}),`
+`,t.jsx(m,{dark:!0,language:"tsx",code:`
+import { QuestHeaderCard } from '@0xintuition/1ui'
+
+<QuestHeaderCard
+  title="Primitive Island"
+  subtitle="Continue your journey."
+  numberOfCompletedQuests={1}
+  totalNumberOfQuests={10}
+  onButtonClick={() => doSomething}
+/>
+`}),`
+`,t.jsx(a,{of:d,sourceState:"none"}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(p,{})]})}function I(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(e,{...o})}):e(o)}export{I as default};
