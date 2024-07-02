ad910b427b54a0e57d87e6d6b84964bb8d0e53bb
alexander-mann <alexandercmann@gmail.com>
2024-07-02 16:23:37 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@a6e6e20fe7b62621229b6cdd83b712f7da8cd976 🚀

---
+++ assets/Tabs-DaSQw1DT.js
@@ -0,0 +1,24 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as n}from"./index-DSkyVWTJ.js";import{M as r,T as i,d as s,C as m}from"./index-B1nenY3m.js";import{B as p}from"./IdentityInput-mw73YtJF.js";import"./Icon.types-pRyTQzB2.js";import"./Input-ChUPLhCP.js";import"./Label-BE9934Bx.js";import"./PieChart-B_dtFskq.js";import"./Resizable-CYN5O2qj.js";import"./SegmentedControl-C8563B2q.js";import"./Separator-upiNYcQT.js";import"./Table-BRTqj0wF.js";import"./Text-dMYK5Tk0.js";import"./Textarea-D_YQ6kc-.js";import"./ValueDisplay-BU89B4rt.js";import{S as l,B as c}from"./Tabs.stories-DmFYDIb7.js";import"./index-CTjT7uj6.js";import"./iframe-CauN1-9R.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-BIDtd1lf.js";import"./palette-saq09hvB.js";function a(o){const e={h4:"h4",...n(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:l}),`
+`,t.jsx(i,{}),`
+`,t.jsx(e.h4,{id:"a-set-of-layered-sections-of-contentknown-as-tab-panelsthat-are-displayed-one-at-a-time",children:"A set of layered sections of content—known as tab panels—that are displayed one at a time."}),`
+`,t.jsx(p,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/tabs#api-reference","_blank"),children:"API Reference"}),`
+`,t.jsx(s,{dark:!0,language:"tsx",code:`
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@0xintuition/1ui'
+
+<Tabs defaultValue="one">
+  <TabsList>
+    <TabsTrigger value="one" label="One " totalCount={69} />
+    <TabsTrigger value="two" label="Two" totalCount={69} />
+    <TabsTrigger disabled value="three" label="Three" totalCount={69} />
+  </TabsList>
+  <div className="bg-primary/10 rounded-lg p-4">
+    <TabsContent value="one">
+      <p>One</p>
+    </TabsContent>
+    <TabsContent value="two">
+      <p>Two</p>
+    </TabsContent>
+  </div>
+</Tabs>
+`}),`
+`,t.jsx(m,{of:c,sourceState:"none"})]})}function P(o={}){const{wrapper:e}={...n(),...o.components};return e?t.jsx(e,{...o,children:t.jsx(a,{...o})}):a(o)}export{P as default};
---
+++ assets/Textarea-Be0xVKyl.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as i}from"./index-DSkyVWTJ.js";import{M as a,T as m,d as n,C as s,e as p}from"./index-B1nenY3m.js";import"./IdentityInput-mw73YtJF.js";import"./Icon.types-pRyTQzB2.js";import"./Input-ChUPLhCP.js";import"./Label-BE9934Bx.js";import"./PieChart-B_dtFskq.js";import"./Resizable-CYN5O2qj.js";import"./SegmentedControl-C8563B2q.js";import"./Separator-upiNYcQT.js";import"./Table-BRTqj0wF.js";import"./Text-dMYK5Tk0.js";import"./Textarea-D_YQ6kc-.js";import"./ValueDisplay-BU89B4rt.js";import{S as x,B as c}from"./Textarea.stories-BBJ8vris.js";import"./index-CTjT7uj6.js";import"./iframe-CauN1-9R.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-BIDtd1lf.js";import"./palette-saq09hvB.js";function e(o){const r={h2:"h2",h4:"h4",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{of:x}),`
+`,t.jsx(m,{}),`
+`,t.jsx(r.h4,{id:"displays-a-form-textarea-or-a-component-that-looks-like-a-textarea",children:"Displays a form textarea or a component that looks like a textarea."}),`
+`,t.jsx(n,{dark:!0,language:"tsx",code:`
+import { Textarea } from '@0xintuition/1ui'
+
+<Textarea />
+`}),`
+`,t.jsx(s,{of:c,sourceState:"none"}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(p,{})]})}function A(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(e,{...o})}):e(o)}export{A as default};
