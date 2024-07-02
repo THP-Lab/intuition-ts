b6e4cf29768dd1e0aa0121bc2615bac572b91c2d
jonathanprozzi <jonathanprozzi@gmail.com>
2024-07-02 02:31:11 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@3efd3c212797a212033be90e6dcf785794c46349 🚀

---
+++ assets/Popover.stories-Clwq77bY.js
@@ -0,0 +1,14 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{a6 as i,a7 as p,B as m,a8 as l}from"./IdentitySearchCombobox-k02URgUF.js";import"./Icon.types-pRyTQzB2.js";import"./Input-ChUPLhCP.js";import"./Label-BE9934Bx.js";import"./PieChart-B_dtFskq.js";import"./Resizable-CYN5O2qj.js";import"./SegmentedControl-C8563B2q.js";import"./Separator-upiNYcQT.js";import"./Table-BRTqj0wF.js";import{T as t}from"./Text-dMYK5Tk0.js";import"./Textarea-D_YQ6kc-.js";import"./ValueDisplay-BU89B4rt.js";const c={title:"Components/Popover",component:i,argTypes:{side:{describe:"Side the content will open on",table:{type:{summary:"string"},defaultValue:{summary:"bottom"}},options:["top","right","bottom","left"],control:"select"},align:{describe:"Where to align content to trigger",table:{type:{summary:"string"},defaultValue:{summary:"center"}},options:["start","center","end"],control:"select"}}},e={render:a=>o.jsxs(i,{children:[o.jsx(p,{children:o.jsx(m,{size:"lg",children:"Open popover"})}),o.jsxs(l,{...a,children:[o.jsx(t,{variant:"bodyLarge",children:"Popover Content"}),o.jsx(t,{variant:"body",children:"Here is some very long content to test out this popover. It is good to see how it looks with lots of text!"})]})]})};var r,n,s;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: args => <Popover>
+      <PopoverTrigger>
+        <Button size="lg">Open popover</Button>
+      </PopoverTrigger>
+      <PopoverContent {...args}>
+        <Text variant="bodyLarge">Popover Content</Text>
+        <Text variant="body">
+          Here is some very long content to test out this popover. It is good to
+          see how it looks with lots of text!
+        </Text>
+      </PopoverContent>
+    </Popover>
+}`,...(s=(n=e.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const d=["BasicUsage"],S=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:e,__namedExportsOrder:d,default:c},Symbol.toStringTag,{value:"Module"}));export{e as B,S};
---
+++ assets/ProfileCard.stories-Do-ChEQb.js
@@ -0,0 +1,36 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{ae as o,I as m,B as u}from"./IdentitySearchCombobox-k02URgUF.js";const p={title:"Components/ProfileCard",component:o,argTypes:{variant:{description:"Variant of avatar",options:Object.values(m),table:{type:{summary:"string"},defaultValue:{summary:"user"}},control:"select"},avatarSrc:{description:"URL of the avatar image",table:{type:{summary:"string"}}},name:{description:"Name of the identity",table:{type:{summary:"string"}}},walletAddress:{description:"Wallet address of the identity",table:{type:{summary:"string"}}},stats:{description:"Statistics related to the identity",table:{type:{summary:"{ numberOfFollowers: number, numberOfFollowing?: number, points?: number }"}}},link:{description:"Link related to the entity (optional)",table:{type:{summary:"string"},defaultValue:{summary:"undefined"}}},bio:{description:"Bio or description of the identity",table:{type:{summary:"string"},defaultValue:{summary:"undefined"}}}}},e={args:{variant:"user",avatarSrc:'https://avatars.githubusercontent.com/u/94311139?s=200&v=4"',name:"John Doe",walletAddress:"0x1234567890abcdef1234567890abcdef12345678",stats:{numberOfFollowers:123,numberOfFollowing:45,points:671234},bio:"John Doe is a blockchain enthusiast. He loves to learn new things and share his knowledge with others. He is also a contributor to various open-source projects."},render:r=>t.jsx("div",{className:"w-[500px]",children:t.jsx(o,{...r,children:t.jsx(u,{variant:"secondary",size:"lg",className:"w-full",onClick:()=>window.open("https://example.com","_blank"),children:"Follow"})})})},a={args:{variant:"non-user",avatarSrc:'https://avatars.githubusercontent.com/u/94311139?s=200&v=4"',name:"Blockchain Corp",walletAddress:"0x1234567890abcdef1234567890abcdef12345678",stats:{numberOfFollowers:300},link:"https://blockchaincorp.com",bio:"Blockchain Corp is a leading company in blockchain technology. Visit our website for more information about how you can benefit from our services."},render:r=>t.jsx("div",{className:"w-[500px]",children:t.jsx(o,{...r})})};var n,s,i;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    variant: 'user',
+    avatarSrc: 'https://avatars.githubusercontent.com/u/94311139?s=200&v=4"',
+    name: 'John Doe',
+    walletAddress: '0x1234567890abcdef1234567890abcdef12345678',
+    stats: {
+      numberOfFollowers: 123,
+      numberOfFollowing: 45,
+      points: 671234
+    },
+    bio: 'John Doe is a blockchain enthusiast. He loves to learn new things and share his knowledge with others. He is also a contributor to various open-source projects.'
+  },
+  render: (args: ProfileCardProps) => <div className="w-[500px]">
+      <ProfileCard {...args}>
+        <Button variant="secondary" size="lg" className="w-full" onClick={() => window.open('https://example.com', '_blank')}>
+          Follow
+        </Button>
+      </ProfileCard>
+    </div>
+}`,...(i=(s=e.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};var l,c,d;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    variant: 'non-user',
+    avatarSrc: 'https://avatars.githubusercontent.com/u/94311139?s=200&v=4"',
+    name: 'Blockchain Corp',
+    walletAddress: '0x1234567890abcdef1234567890abcdef12345678',
+    stats: {
+      numberOfFollowers: 300
+    },
+    link: 'https://blockchaincorp.com',
+    bio: 'Blockchain Corp is a leading company in blockchain technology. Visit our website for more information about how you can benefit from our services.'
+  },
+  render: (args: ProfileCardProps) => <div className="w-[500px]">
+      <ProfileCard {...args} />
+    </div>
+}`,...(d=(c=a.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};const b=["BasicUsage","EntityProfile"],g=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:e,EntityProfile:a,__namedExportsOrder:b,default:p},Symbol.toStringTag,{value:"Module"}));export{e as B,a as E,g as S};
