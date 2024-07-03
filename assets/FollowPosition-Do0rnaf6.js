d958c87d06c9d12f4bdab250e15553c7e14e2b3a
alexander-mann <alexandercmann@gmail.com>
2024-07-03 14:52:19 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@7e6b2e2d2635f39af66d16f211c56d928aedfc84 🚀

---
+++ assets/FollowPosition-Do0rnaf6.js
@@ -0,0 +1,19 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as s}from"./index-DSkyVWTJ.js";import{M as n,T as e,d as m,C as p,e as a}from"./index-BP0nnktB.js";import"./ActivePositionCard-DXrOyMt3.js";import"./Icon.types-pRyTQzB2.js";import"./Input-ChUPLhCP.js";import"./Label-BE9934Bx.js";import"./PieChart-B_dtFskq.js";import"./Resizable-CYN5O2qj.js";import"./SegmentedControl-C8563B2q.js";import"./Separator-upiNYcQT.js";import"./Table-BRTqj0wF.js";import"./Text-dMYK5Tk0.js";import"./Textarea-D_YQ6kc-.js";import"./ValueDisplay-BU89B4rt.js";import{S as l,B as c}from"./FollowPosition.stories-BdtP4TDM.js";import"./index-CTjT7uj6.js";import"./iframe-BUxY-szZ.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-BIDtd1lf.js";import"./palette-saq09hvB.js";function r(t){const i={h2:"h2",h4:"h4",...s(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{of:l}),`
+`,o.jsx(e,{}),`
+`,o.jsx(i.h4,{id:"a-follow-position-component-is-used-to-display-a-position-or-a-list-of-positions-relating-to-follows",children:"A Follow Position component is used to display a position or a list of positions relating to follows"}),`
+`,o.jsx(m,{dark:!0,language:"tsx",code:`
+import { FollowPosition } from '@0xintuition/1ui'
+
+<FollowPosition
+  position="claimFor"
+  avatarSrc="https://avatars.githubusercontent.com/u/94311139?s=200&v=4"
+  name="John Doe"
+  walletAddress="0x1234567890abcdef1234567890abcdef12345678"
+  amount={1.21}
+  feesAccrued={0.005}
+  updatedAt="2021-10-01T16:00:00Z"
+/>
+`}),`
+`,o.jsx(p,{of:c,sourceState:"none"}),`
+`,o.jsx(i.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(a,{})]})}function Z(t={}){const{wrapper:i}={...s(),...t.components};return i?o.jsx(i,{...t,children:o.jsx(r,{...t})}):r(t)}export{Z as default};
---
+++ assets/Popover-DNvRb58E.js
@@ -0,0 +1,22 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as n}from"./index-DSkyVWTJ.js";import{M as i,T as p,d as s,C as m,e as a}from"./index-BP0nnktB.js";import{S as c,B as x}from"./Popover.stories-BV-FlqqN.js";import"./index-CTjT7uj6.js";import"./iframe-BUxY-szZ.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./ActivePositionCard-DXrOyMt3.js";import"./index-Bb4qSo10.js";import"./index-BIDtd1lf.js";import"./palette-saq09hvB.js";import"./Resizable-CYN5O2qj.js";import"./Icon.types-pRyTQzB2.js";import"./Text-dMYK5Tk0.js";import"./Input-ChUPLhCP.js";import"./Label-BE9934Bx.js";import"./PieChart-B_dtFskq.js";import"./SegmentedControl-C8563B2q.js";import"./Separator-upiNYcQT.js";import"./Table-BRTqj0wF.js";import"./Textarea-D_YQ6kc-.js";import"./ValueDisplay-BU89B4rt.js";function e(t){const r={h2:"h2",h4:"h4",...n(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{of:c}),`
+`,o.jsx(p,{}),`
+`,o.jsx(r.h4,{id:"displays-rich-content-in-a-portal-triggered-by-a-button",children:"Displays rich content in a portal, triggered by a button."}),`
+`,o.jsx(s,{dark:!0,language:"tsx",code:`
+import { Popover, PopoverContent, PopoverTrigger } from '@0xintuition/1ui'
+
+<Popover {...args}>
+  <PopoverTrigger>
+    <Button>Open popover</Button>
+  </PopoverTrigger>
+  <PopoverContent>
+    <Text variant="bodyLarge">Popover Content</Text>
+    <Text variant="body">
+      Here is some very long content to test out this popover. It is good to see
+      how it looks with lots of text!
+    </Text>
+  </PopoverContent>
+</Popover>
+`}),`
+`,o.jsx(m,{of:x,sourceState:"none"}),`
+`,o.jsx(r.h2,{id:"popovercontent-properties",children:"PopoverContent Properties"}),`
+`,o.jsx(a,{})]})}function U(t={}){const{wrapper:r}={...n(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(e,{...t})}):e(t)}export{U as default};
