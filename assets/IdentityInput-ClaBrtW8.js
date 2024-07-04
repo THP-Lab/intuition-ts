d5e5b004ac967f13a28f0860b90603a71b347802
alexander-mann <alexandercmann@gmail.com>
2024-07-04 18:33:42 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@b7da00d85b31018b65caf8647b8a663b2e610dd9 🚀

---
+++ assets/IdentityInput-ClaBrtW8.js
@@ -0,0 +1,47 @@
+import{j as e}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as a}from"./index-DSkyVWTJ.js";import{M as s,T as m,d as n,C as i,e as c}from"./index-CuPG11g_.js";import{S as l,B as p,O as d}from"./IdentityInput.stories-D15NY2dC.js";import"./index-CTjT7uj6.js";import"./iframe-BlWsGh6o.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./TransactionStatusIndicator-C2digs4e.js";import"./index-BIDtd1lf.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Resizable-CYN5O2qj.js";import"./Icon.types-DKKjwDUO.js";import"./Text-dMYK5Tk0.js";import"./Input-ChUPLhCP.js";import"./Label-BE9934Bx.js";import"./PieChart-B_dtFskq.js";import"./SegmentedControl-C8563B2q.js";import"./Separator-upiNYcQT.js";import"./Table-BRTqj0wF.js";import"./Textarea-D_YQ6kc-.js";import"./ValueDisplay-BU89B4rt.js";function r(o){const t={h2:"h2",h3:"h3",h4:"h4",...a(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:l}),`
+`,e.jsx(m,{}),`
+`,e.jsx(t.h4,{id:"allows-users-to-create-a-claim",children:"Allows users to create a claim."}),`
+`,e.jsx(n,{dark:!0,language:"tsx",code:`
+import { IdentityInput } from '@0xintuition/1ui'
+
+<IdentityInput
+  showLabels
+  subject={{
+    selectedValue: { name: 'Super Dave' },
+    onClick: () => doSomething,
+  }}
+  predicate={{
+    selectedValue: { name: 'is a', variant: 'non-user' },
+    onClick: () => doSomething,
+  }}
+  object={{
+    selectedValue: { name: 'daredevil', variant: 'non-user' },
+    onClick: () => doSomething,
+  }}
+/>
+`}),`
+`,e.jsx(i,{of:p,sourceState:"none"}),`
+`,e.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(c,{}),`
+`,e.jsx(t.h2,{id:"variants",children:"Variants"}),`
+`,e.jsx(t.h3,{id:"other",children:"Other"}),`
+`,e.jsx(n,{dark:!0,language:"tsx",code:`
+<IdentityInput
+  subject={{
+    placeholder: 'Select an identity',
+    selectedValue: {},
+    onClick: () => doSomething,
+  }}
+  predicate={{
+    placeholder: 'Select an identity',
+    selectedValue: {},
+    onClick: () => doSomething,
+  }}
+  object={{
+    placeholder: 'Select an identity',
+    selectedValue: {},
+    onClick: () => doSomething,
+  }}
+/>
+`}),`
+`,e.jsx(i,{of:d,sourceState:"none"})]})}function U(o={}){const{wrapper:t}={...a(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(r,{...o})}):r(o)}export{U as default};
--- assets/preview-CSEmoyn8.js
+++
@@ -1,7 +0,0 @@
-import{_ as a}from"./iframe-Bbynkv0N.js";import"../sb-preview/runtime.js";const{global:s}=__STORYBOOK_MODULE_GLOBAL__;var _=Object.entries(s.TAGS_OPTIONS??{}).reduce((e,r)=>{let[t,o]=r;return o.excludeFromDocsStories&&(e[t]=!0),e},{}),d={docs:{renderer:async()=>{let{DocsRenderer:e}=await a(()=>import("./DocsRenderer-K4EAMTCU-DGKkoq4D.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new e},stories:{filter:e=>{var r;return(e.tags||[]).filter(t=>_[t]).length===0&&!((r=e.parameters.docs)!=null&&r.disable)}}}};export{d as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-K4EAMTCU-DGKkoq4D.js","./iframe-Bbynkv0N.js","./index-CTjT7uj6.js","./react-18-I1chspnn.js","./index-BhgyLgKK.js","./index-B9Zz9L1M.js","./index-DP8FJ8Qs.js","./index-BI02G6dj.js","./index-DXimoRZY.js","./index-Bx4XDAbk.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
