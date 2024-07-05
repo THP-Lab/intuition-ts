14e3f980665d1c15f5e21fef93072fa87bb36bd1
Vitalsine85 <rob@creative-element.net>
2024-07-05 13:32:23 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@915d87149c1fc6c003a8f9d1877a9cccd654c9f3 🚀

---
+++ assets/ClaimPosition.stories-CmFj8d-j.js
@@ -0,0 +1,38 @@
+import{j as e}from"./jsx-runtime-Cw0GR0a5.js";import{m as t,k as d}from"./TransactionStatusIndicator-BT-UtpL1.js";import"./Icon.types-DKKjwDUO.js";import"./Input-ChUPLhCP.js";import"./Label-BE9934Bx.js";import"./PieChart-B_dtFskq.js";import"./Resizable-CYN5O2qj.js";import"./SegmentedControl-C8563B2q.js";import"./Separator-upiNYcQT.js";import"./Table-BRTqj0wF.js";import"./Text-dMYK5Tk0.js";import"./Textarea-D_YQ6kc-.js";import"./ValueDisplay-BU89B4rt.js";const p={title:"Components/ClaimPosition",component:t},a={args:{variant:"user",position:"claimFor",name:"John Doe",walletAddress:"0x1234567890abcdef1234567890abcdef12345678",avatarSrc:'https://avatars.githubusercontent.com/u/94311139?s=200&v=4"',amount:1.21,feesAccrued:.005,updatedAt:"2021-10-01T16:00:00Z"},render:r=>e.jsx("div",{className:"w-[800px]",children:e.jsx(t,{...r})})},n={args:{variant:"claim",position:"claimAgainst",claimsFor:30,claimsAgainst:70,amount:1.21,feesAccrued:.005},render:r=>e.jsx("div",{className:"w-[800px]",children:e.jsx(t,{...r,children:e.jsx(d,{subject:{variant:"non-user",label:"0xintuition"},predicate:{variant:"non-user",label:"is really"},object:{variant:"non-user",label:"cool"}})})})};var s,i,o;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    variant: 'user',
+    position: 'claimFor',
+    name: 'John Doe',
+    walletAddress: '0x1234567890abcdef1234567890abcdef12345678',
+    avatarSrc: 'https://avatars.githubusercontent.com/u/94311139?s=200&v=4"',
+    amount: 1.21,
+    feesAccrued: 0.005,
+    updatedAt: '2021-10-01T16:00:00Z'
+  },
+  render: args => <div className="w-[800px]">
+      <ClaimPosition {...args}></ClaimPosition>
+    </div>
+}`,...(o=(i=a.parameters)==null?void 0:i.docs)==null?void 0:o.source}}};var c,m,l;n.parameters={...n.parameters,docs:{...(c=n.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    variant: 'claim',
+    position: 'claimAgainst',
+    claimsFor: 30,
+    claimsAgainst: 70,
+    amount: 1.21,
+    feesAccrued: 0.005
+  },
+  render: args => <div className="w-[800px]">
+      <ClaimPosition {...args}>
+        <Claim subject={{
+        variant: 'non-user',
+        label: '0xintuition'
+      }} predicate={{
+        variant: 'non-user',
+        label: 'is really'
+      }} object={{
+        variant: 'non-user',
+        label: 'cool'
+      }} />
+      </ClaimPosition>
+    </div>
+}`,...(l=(m=n.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};const u=["UserVariant","IdentityVariant"],_=Object.freeze(Object.defineProperty({__proto__:null,IdentityVariant:n,UserVariant:a,__namedExportsOrder:u,default:p},Symbol.toStringTag,{value:"Module"}));export{n as I,_ as S,a as U};
