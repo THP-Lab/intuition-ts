cb8266db7cb8ee4913e92fc18009467f03e45fed
rahulr8 <37151383+rahulr8@users.noreply.github.com>
2024-07-09 17:57:02 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@c463d092d226d3fe90dfa5925094331b560b9e78 🚀

---
+++ assets/DropdownMenu.stories-BEbTOXQ9.js
@@ -0,0 +1,16 @@
+import{j as n}from"./jsx-runtime-Cw0GR0a5.js";import{G as s,H as d,B as a,J as u,K as m,L as c,M as e}from"./Trunctacular-Ze9FFfLi.js";import"./Icon.types-bVvTOTyL.js";import"./Label-Ca4F4AkB.js";import"./PieChart-B_dtFskq.js";import"./Resizable-B4D6NlOM.js";import"./SegmentedControl-CpGzRY7X.js";import"./Separator-BPzIOX_U.js";import"./Table-DoQDVlQC.js";import"./Text-DRgAEttF.js";import"./Textarea-w5FyYqF_.js";import"./ValueDisplay-BGyvBqcS.js";const l={title:"Components/DropdownMenu",component:s},o={args:{},render:i=>n.jsxs(s,{...i,children:[n.jsx(d,{children:n.jsx(a,{size:"lg",children:"Open menu"})}),n.jsxs(u,{children:[n.jsx(m,{children:"My Account"}),n.jsx(c,{}),n.jsx(e,{children:"Profile"}),n.jsx(e,{children:"Billing"}),n.jsx(e,{children:"Team"}),n.jsx(e,{children:"Subscription"})]})]})};var r,t,p;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {},
+  render: args => <DropdownMenu {...args}>
+      <DropdownMenuTrigger>
+        <Button size="lg">Open menu</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+}`,...(p=(t=o.parameters)==null?void 0:t.docs)==null?void 0:p.source}}};const M=["BasicUsage"],_=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:o,__namedExportsOrder:M,default:l},Symbol.toStringTag,{value:"Module"}));export{o as B,_ as S};
---
+++ assets/Textarea-WQmfMlma.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as i}from"./index-DSkyVWTJ.js";import{M as a,T as m,d as n,C as s,e as p}from"./index-DPVSPeqD.js";import"./Trunctacular-Ze9FFfLi.js";import"./Icon.types-bVvTOTyL.js";import"./Label-Ca4F4AkB.js";import"./PieChart-B_dtFskq.js";import"./Resizable-B4D6NlOM.js";import"./SegmentedControl-CpGzRY7X.js";import"./Separator-BPzIOX_U.js";import"./Table-DoQDVlQC.js";import"./Text-DRgAEttF.js";import"./Textarea-w5FyYqF_.js";import"./ValueDisplay-BGyvBqcS.js";import{S as x,B as c}from"./Textarea.stories-B47KHf1z.js";import"./index-CTjT7uj6.js";import"./iframe-DPt28wnH.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-DeHqaMYO.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";function e(o){const r={h2:"h2",h4:"h4",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{of:x}),`
+`,t.jsx(m,{}),`
+`,t.jsx(r.h4,{id:"displays-a-form-textarea-or-a-component-that-looks-like-a-textarea",children:"Displays a form textarea or a component that looks like a textarea."}),`
+`,t.jsx(n,{dark:!0,language:"tsx",code:`
+import { Textarea } from '@0xintuition/1ui'
+
+<Textarea />
+`}),`
+`,t.jsx(s,{of:c,sourceState:"none"}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(p,{})]})}function A(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(e,{...o})}):e(o)}export{A as default};
