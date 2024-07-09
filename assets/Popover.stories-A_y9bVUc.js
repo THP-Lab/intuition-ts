4e71e4b620392d1af7653470a91ff8ef557d4300
Vitalsine85 <rob@creative-element.net>
2024-07-09 19:26:02 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@2efc978a69a34f596c4e86b6fca94e7bc95b326f 🚀

---
+++ assets/Popover.stories-A_y9bVUc.js
@@ -0,0 +1,14 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{aa as i,ab as p,B as l,ac as m}from"./Trunctacular-C4_B4lLx.js";import"./Icon.types-bVvTOTyL.js";import"./Label-Ca4F4AkB.js";import"./PieChart-B_dtFskq.js";import"./Resizable-B4D6NlOM.js";import"./SegmentedControl-CpGzRY7X.js";import"./Separator-BPzIOX_U.js";import"./Table-DoQDVlQC.js";import{T as t}from"./Text-DRgAEttF.js";import"./Textarea-w5FyYqF_.js";import"./ValueDisplay-BGyvBqcS.js";const c={title:"Components/Popover",component:i,argTypes:{side:{describe:"Side the content will open on",table:{type:{summary:"string"},defaultValue:{summary:"bottom"}},options:["top","right","bottom","left"],control:"select"},align:{describe:"Where to align content to trigger",table:{type:{summary:"string"},defaultValue:{summary:"center"}},options:["start","center","end"],control:"select"}}},e={render:a=>o.jsxs(i,{children:[o.jsx(p,{children:o.jsx(l,{size:"lg",children:"Open popover"})}),o.jsxs(m,{...a,children:[o.jsx(t,{variant:"bodyLarge",children:"Popover Content"}),o.jsx(t,{variant:"body",children:"Here is some very long content to test out this popover. It is good to see how it looks with lots of text!"})]})]})};var r,n,s;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: args => <Popover>
+      <PopoverTrigger>
+        <Button size="lg">Open popover</Button>
+      </PopoverTrigger>
+      <PopoverContent {...args}>
+        <Text variant="bodyLarge">Popover Content</Text>
+        <Text variant="body">
+          Here is some very long content to test out this popover. It is good to
+          see how it looks with lots of text!
+        </Text>
+      </PopoverContent>
+    </Popover>
+}`,...(s=(n=e.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const d=["BasicUsage"],C=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:e,__namedExportsOrder:d,default:c},Symbol.toStringTag,{value:"Module"}));export{e as B,C as S};
