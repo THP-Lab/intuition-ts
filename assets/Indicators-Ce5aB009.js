f49dbe6a78633e36bfbd705ee669a37fea8dc7bb
jonathanprozzi <jonathanprozzi@gmail.com>
2024-07-10 22:52:13 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@20e0d754741680e08928bf2fe9b286e1883a9a6a 🚀

---
+++ assets/Indicators-Ce5aB009.js
@@ -0,0 +1,19 @@
+import{j as i}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as r}from"./index-DSkyVWTJ.js";import{M as s,T as a,d as n,C as m}from"./index-e-QMy8pX.js";import"./Trunctacular-D7w2W2WH.js";import"./Icon.types-bVvTOTyL.js";import"./Label-Ca4F4AkB.js";import"./PieChart-B_dtFskq.js";import"./Resizable-B4D6NlOM.js";import"./SegmentedControl-CpGzRY7X.js";import"./Separator-BPzIOX_U.js";import"./Table-DoQDVlQC.js";import"./Text-DRgAEttF.js";import"./Textarea-w5FyYqF_.js";import"./ValueDisplay-BGyvBqcS.js";import{S as l,I as p}from"./Indicators.stories-Co8zfqwg.js";import"./index-CTjT7uj6.js";import"./iframe-BZPjGakx.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-DeHqaMYO.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";function e(o){const t={h2:"h2",...r(),...o.components};return i.jsxs(i.Fragment,{children:[i.jsx(s,{of:l}),`
+`,i.jsx(a,{}),`
+`,i.jsx(t.h2,{id:"a-list-of-indicators-for-identities-claims-and-positions",children:"A list of indicators for Identities, Claims, and Positions"}),`
+`,i.jsx(n,{dark:!0,language:"tsx",code:`
+import { MonetaryValue, FeesAccrued, PositionValueDisplay, ClaimValueDisplay, IdentityValueDisplay } from '@0xintuition/1ui'
+
+<MonetaryValue value={0.345} currency="ETH" />
+<FeesAccrued value={0.005} currency="ETH" />
+<PositionValueDisplay value={0.345} position="claimFor" feesAccrued={0.005} />
+<PositionValueDisplay
+  value={0.345}
+  position="claimAgainst"
+  feesAccrued={0.005}
+/>
+<PositionValueDisplay value={0.345} position="identity" feesAccrued={0.005} />
+<ClaimValueDisplay value={0.345} claimsFor={230} claimsAgainst={125} />
+<IdentityValueDisplay value={0.345} currency="ETH" followers={230} />
+`}),`
+`,i.jsx(m,{of:p,sourceState:"none"})]})}function U(o={}){const{wrapper:t}={...r(),...o.components};return t?i.jsx(t,{...o,children:i.jsx(e,{...o})}):e(o)}export{U as default};
