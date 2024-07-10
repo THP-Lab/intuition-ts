23a5dbfd722a26a15e6541414f7753d5bea75775
alexander-mann <alexandercmann@gmail.com>
2024-07-10 15:03:49 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@d4262760c8a30db114cde0d401338e3c6db3bc19 🚀

---
+++ assets/Select-DwPtdlVT.js
@@ -0,0 +1,10 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as e}from"./index-DSkyVWTJ.js";import{M as n,T as m,d as s,C as p}from"./index-CfFtdBYX.js";import{B as a}from"./Trunctacular-CInRYgri.js";import"./Icon.types-bVvTOTyL.js";import"./Label-Ca4F4AkB.js";import"./PieChart-B_dtFskq.js";import"./Resizable-B4D6NlOM.js";import"./SegmentedControl-CpGzRY7X.js";import"./Separator-BPzIOX_U.js";import"./Table-DoQDVlQC.js";import"./Text-DRgAEttF.js";import"./Textarea-w5FyYqF_.js";import"./ValueDisplay-BGyvBqcS.js";import{S as c,B as u}from"./Select.stories-Df4cW-3w.js";import"./index-CTjT7uj6.js";import"./iframe-DwNPRnfA.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-DeHqaMYO.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";function i(o){const r={h4:"h4",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:c}),`
+`,t.jsx(m,{}),`
+`,t.jsx(r.h4,{id:"displays-a-list-of-options-for-the-user-to-pick-fromtriggered-by-a-button",children:"Displays a list of options for the user to pick from—triggered by a button."}),`
+`,t.jsx(a,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/select#api-reference","_blank"),children:"API Reference"}),`
+`,t.jsx(s,{dark:!0,language:"tsx",code:`
+import { Select } from '@0xintuition/1ui'
+
+<Select />
+`}),`
+`,t.jsx(p,{of:u,sourceState:"none"})]})}function q(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{q as default};
