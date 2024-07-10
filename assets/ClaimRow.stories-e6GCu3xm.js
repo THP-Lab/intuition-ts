a6d26a2370b3546cfdc9067fd0e86f12c2506d3c
Vitalsine85 <rob@creative-element.net>
2024-07-10 20:44:42 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@0c26fa875a230f0a8db0ff0e3d0b027dffb941d1 🚀

---
+++ assets/ClaimRow.stories-e6GCu3xm.js
@@ -0,0 +1,21 @@
+import{j as n}from"./jsx-runtime-Cw0GR0a5.js";import{n as t,k as s}from"./Trunctacular-Dy8wPhzO.js";import"./Icon.types-bVvTOTyL.js";import"./Label-Ca4F4AkB.js";import"./PieChart-B_dtFskq.js";import"./Resizable-B4D6NlOM.js";import"./SegmentedControl-CpGzRY7X.js";import"./Separator-BPzIOX_U.js";import"./Table-DoQDVlQC.js";import"./Text-DRgAEttF.js";import"./Textarea-w5FyYqF_.js";import"./ValueDisplay-BGyvBqcS.js";const l={title:"Components/Claim/ClaimRow",component:t},a={args:{claimsFor:736,claimsAgainst:234,amount:.383},render:i=>n.jsx("div",{className:"w-[600px]",children:n.jsx(t,{...i,children:n.jsx(s,{subject:{variant:"non-user",label:"0xintuition"},predicate:{variant:"non-user",label:"is really"},object:{variant:"non-user",label:"cool"}})})})};var r,e,o;a.parameters={...a.parameters,docs:{...(r=a.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    claimsFor: 736,
+    claimsAgainst: 234,
+    amount: 0.383
+  },
+  render: args => <div className="w-[600px]">
+      <ClaimRow {...args}>
+        <Claim subject={{
+        variant: 'non-user',
+        label: '0xintuition'
+      }} predicate={{
+        variant: 'non-user',
+        label: 'is really'
+      }} object={{
+        variant: 'non-user',
+        label: 'cool'
+      }} />
+      </ClaimRow>
+    </div>
+}`,...(o=(e=a.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const m=["BasicUsage"],f=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:a,__namedExportsOrder:m,default:l},Symbol.toStringTag,{value:"Module"}));export{a as B,f as S};
---
+++ assets/Tooltip.stories-pYXUVxcm.js
@@ -0,0 +1,14 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{a$ as n,b0 as a,b1 as p,B as l,b2 as m}from"./Trunctacular-Dy8wPhzO.js";import"./Icon.types-bVvTOTyL.js";import"./Label-Ca4F4AkB.js";import"./PieChart-B_dtFskq.js";import"./Resizable-B4D6NlOM.js";import"./SegmentedControl-CpGzRY7X.js";import"./Separator-BPzIOX_U.js";import"./Table-DoQDVlQC.js";import{T as d}from"./Text-DRgAEttF.js";import"./Textarea-w5FyYqF_.js";import"./ValueDisplay-BGyvBqcS.js";const c={title:"Components/Tooltip",component:n},t={render:s=>o.jsx(a,{...s,children:o.jsxs(n,{children:[o.jsx(p,{asChild:!0,children:o.jsx(l,{variant:"ghost",size:"lg",children:"Hover"})}),o.jsx(m,{children:o.jsx(d,{variant:"body",children:"Some text"})})]})})};var r,e,i;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: args => <TooltipProvider {...args}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button variant="ghost" size="lg">
+            Hover
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <Text variant="body">Some text</Text>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+}`,...(i=(e=t.parameters)==null?void 0:e.docs)==null?void 0:i.source}}};const T=["BasicUsage"],y=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:t,__namedExportsOrder:T,default:c},Symbol.toStringTag,{value:"Module"}));export{t as B,y as S};
