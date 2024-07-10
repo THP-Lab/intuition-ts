3770b7897bb37a53b15f2acecb0433b98c5679ae
jonathanprozzi <jonathanprozzi@gmail.com>
2024-07-10 15:48:03 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@222209ea881a2ffcca245d17e7db4fa420e18dcc 🚀

---
+++ assets/IdentityCard-D-yBzCJ_.js
@@ -0,0 +1,31 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as a}from"./index-DSkyVWTJ.js";import{M as s,T as m,d as n,C as i,e as p}from"./index-CeHtRIFE.js";import{S as d,B as c,E as u}from"./IdentityCard.stories-Bue4Xq2X.js";import"./index-CTjT7uj6.js";import"./iframe-DvcvNLSh.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./Trunctacular-ZJ52VLFC.js";import"./index-Bb4qSo10.js";import"./index-DeHqaMYO.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./Resizable-B4D6NlOM.js";import"./Icon.types-bVvTOTyL.js";import"./Text-DRgAEttF.js";import"./Label-Ca4F4AkB.js";import"./PieChart-B_dtFskq.js";import"./SegmentedControl-CpGzRY7X.js";import"./Separator-BPzIOX_U.js";import"./Table-DoQDVlQC.js";import"./Textarea-w5FyYqF_.js";import"./ValueDisplay-BGyvBqcS.js";function o(e){const r={h2:"h2",h3:"h3",h4:"h4",...a(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:d}),`
+`,t.jsx(m,{}),`
+`,t.jsx(r.h4,{id:"displays-an-identity-or-entity-and-its-key-data",children:"Displays an identity or entity and its key data."}),`
+`,t.jsx(n,{dark:!0,language:"tsx",code:`
+import { IdentityCard } from '@0xintuition/1ui'
+
+<IdentityCard
+  variant="user"
+  avatarSrc="image.jpg"
+  name="Super Dave"
+  value={4.123}
+  currency="ETH"
+  walletAddress="0x1234567890abcdef1234567890abcdef12345678"
+/>
+`}),`
+`,t.jsx(i,{of:c,sourceState:"none"}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(p,{}),`
+`,t.jsx(r.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(r.h3,{id:"other",children:"Other"}),`
+`,t.jsx(n,{dark:!0,language:"tsx",code:`
+<IdentityCard
+  variant="non-user"
+  avatarSrc="https://avatars.githubusercontent.com/u/94311139?s=200&v=4"
+  name="Intuition"
+  value={7.892}
+  currency="ETH"
+  walletAddress="0x1234567890abcdef1234567890abcdef12345678"
+/>
+`}),`
+`,t.jsx(i,{of:u,sourceState:"none"})]})}function U(e={}){const{wrapper:r}={...a(),...e.components};return r?t.jsx(r,{...e,children:t.jsx(o,{...e})}):o(e)}export{U as default};
---
+++ assets/Label-D0Hb6UES.js
@@ -0,0 +1,10 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as i}from"./index-DSkyVWTJ.js";import{M as n,T as s,d as m,C as a}from"./index-CeHtRIFE.js";import{B as p}from"./Trunctacular-ZJ52VLFC.js";import"./Icon.types-bVvTOTyL.js";import"./Label-Ca4F4AkB.js";import"./PieChart-B_dtFskq.js";import"./Resizable-B4D6NlOM.js";import"./SegmentedControl-CpGzRY7X.js";import"./Separator-BPzIOX_U.js";import"./Table-DoQDVlQC.js";import"./Text-DRgAEttF.js";import"./Textarea-w5FyYqF_.js";import"./ValueDisplay-BGyvBqcS.js";import{S as c,B as l}from"./Label.stories-9uce9I0S.js";import"./index-CTjT7uj6.js";import"./iframe-DvcvNLSh.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-DeHqaMYO.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";function e(o){const r={h4:"h4",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:c}),`
+`,t.jsx(s,{}),`
+`,t.jsx(r.h4,{id:"renders-an-accessible-label-associated-with-controls",children:"Renders an accessible label associated with controls."}),`
+`,t.jsx(p,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/label#api-reference","_blank"),children:"API Reference"}),`
+`,t.jsx(m,{dark:!0,language:"tsx",code:`
+import { Label } from '@0xintuition/1ui'
+
+<Label htmlFor="email">Your email address</Label>
+`}),`
+`,t.jsx(a,{of:l,sourceState:"none"})]})}function Y(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(e,{...o})}):e(o)}export{Y as default};
