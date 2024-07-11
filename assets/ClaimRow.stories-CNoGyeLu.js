9e3b75724c54d5990f4ef07c9aa5f3a10e29cc0d
0xjojikun <110207608+0xjojikun@users.noreply.github.com>
2024-07-11 03:59:55 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@3b9fc5e248b24e1b3bec5ed1a716f1e913466615 🚀

---
+++ assets/ClaimRow.stories-CNoGyeLu.js
@@ -0,0 +1,21 @@
+import{j as r}from"./jsx-runtime-Cw0GR0a5.js";import{k as t,h as s}from"./QuestStatusCard-Cc4Hx_RH.js";import"./QuestSetProgressCard-lZkOsrNz.js";import"./Icon.types-bVvTOTyL.js";import"./Label-hoJUpLlw.js";import"./PieChart-B_dtFskq.js";import"./Resizable-vl710Htm.js";import"./SegmentedControl-CpGzRY7X.js";import"./Separator-ClB74Fps.js";import"./Table-DoQDVlQC.js";import"./Text-DRgAEttF.js";import"./Textarea-w5FyYqF_.js";import"./ValueDisplay-BGyvBqcS.js";import"./QuestSetCard-DtapH5js.js";import"./ProgressBar-DJT993xg.js";const m={title:"Components/Claim/ClaimRow",component:t},a={args:{claimsFor:736,claimsAgainst:234,amount:.383},render:i=>r.jsx("div",{className:"w-[600px]",children:r.jsx(t,{...i,children:r.jsx(s,{subject:{variant:"non-user",label:"0xintuition"},predicate:{variant:"non-user",label:"is really"},object:{variant:"non-user",label:"cool"}})})})};var n,o,e;a.parameters={...a.parameters,docs:{...(n=a.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    claimsFor: 736,
+    claimsAgainst: 234,
+    amount: 0.383
+  },
+  render: args => <div className="w-[600px]">
+      <ClaimRow {...args}>
+        <Claim subject={{
+        variant: 'non-user',
+        label: '0xintuition'
+      }} predicate={{
+        variant: 'non-user',
+        label: 'is really'
+      }} object={{
+        variant: 'non-user',
+        label: 'cool'
+      }} />
+      </ClaimRow>
+    </div>
+}`,...(e=(o=a.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const l=["BasicUsage"],y=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:a,__namedExportsOrder:l,default:m},Symbol.toStringTag,{value:"Module"}));export{a as B,y as S};
---
+++ assets/QuestStatusCard.stories-CH-CJtgN.js
@@ -0,0 +1,7 @@
+import{j as u}from"./jsx-runtime-Cw0GR0a5.js";import{ah as r,ai as o}from"./QuestStatusCard-Cc4Hx_RH.js";const p={title:"Components/Quest/QuestStatusCard",component:r,argTypes:{status:{description:"Status of quest",options:Object.values(o),table:{type:{summary:"string"}},control:"select"},tooltip:{description:"Tooltip content",table:{type:{summary:"string"}}}}},t={args:{status:o.notStarted,tooltip:"Example text"},render:n=>u.jsx(r,{...n})};var e,s,a;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    status: QuestStatus.notStarted,
+    tooltip: 'Example text'
+  },
+  render: args => <QuestStatusCard {...args} />
+}`,...(a=(s=t.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const i=["BasicUsage"],l=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:t,__namedExportsOrder:i,default:p},Symbol.toStringTag,{value:"Module"}));export{t as B,l as S};
