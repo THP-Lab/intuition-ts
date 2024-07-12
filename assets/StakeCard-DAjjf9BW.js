87dd18df9524e312ed736678cf0bb45be66209d1
jonathanprozzi <jonathanprozzi@gmail.com>
2024-07-12 04:47:41 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@3e3fd4204f614d516ddb847560b42f5b9cae2733 🚀

---
+++ assets/StakeCard-DAjjf9BW.js
@@ -0,0 +1,16 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as m}from"./index-DSkyVWTJ.js";import{M as s,T as e,d as n,C as p,e as a}from"./index-GwhrMMT8.js";import{S as c,B as d}from"./StakeCard.stories-Bg0_xScp.js";import"./index-CTjT7uj6.js";import"./iframe-CAqc9L96.js";import"../sb-preview/runtime.js";import"./index-Ba1MzMRK.js";import"./index-DBez60WA.js";import"./index-BSh8g_S6.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./QuestStatusCard-C-xnORxh.js";import"./index-DgtlF2hQ.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./QuestStatusIndicator-DHmvCJek.js";import"./Icon.types-D4lfunSt.js";import"./index-Bb4qSo10.js";import"./QuestSetProgressCard-BNr9QVR-.js";import"./createLucideIcon-BfM7wYCo.js";import"./ProgressBar-D75vLLzs.js";import"./Text-DsVeH9qI.js";import"./Label-4gKwHNvc.js";import"./PieChart-B_dtFskq.js";import"./Resizable-B7XdxUdz.js";import"./SegmentedControl-C43o2uqF.js";import"./Separator-EZUw1-4L.js";import"./Table-C0qIXiUM.js";import"./Textarea-CUiVvNKI.js";import"./ValueDisplay-D2U_pdiu.js";import"./QuestSetCard-BHXW0rRZ.js";function i(t){const r={h2:"h2",h4:"h4",...m(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(s,{of:c}),`
+`,o.jsx(e,{}),`
+`,o.jsx(r.h4,{id:"a-display-card-to-show-a-users-stake-position",children:"A display card to show a user's stake position."}),`
+`,o.jsx(n,{dark:!0,language:"tsx",code:`
+import { StakeCard } from '@0xintuition/1ui'
+
+<StakeCard
+  tvl={4.928}
+  holders={69}
+  onBuyClick={() => doSomething()}
+  onViewAllClick={() => doSomething()}
+/>
+`}),`
+`,o.jsx(p,{of:d,sourceState:"none"}),`
+`,o.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(a,{})]})}function K(t={}){const{wrapper:r}={...m(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(i,{...t})}):i(t)}export{K as default};
--- assets/introduction-BL3pmV66.js
+++
@@ -1,7 +0,0 @@
-import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as r}from"./index-DSkyVWTJ.js";import{M as a,d as i}from"./index-B6U34Qgx.js";import"./index-CTjT7uj6.js";import"./iframe-C-UtSshj.js";import"../sb-preview/runtime.js";import"./index-Ba1MzMRK.js";import"./index-DBez60WA.js";import"./index-BSh8g_S6.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";function e(o){const n={a:"a",h1:"h1",h2:"h2",p:"p",...r(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{title:"Documentation/Introduction"}),`
-`,t.jsx(n.h1,{id:"introduction",children:"Introduction"}),`
-`,t.jsxs(n.p,{children:["1ui is a web3-enabled ",t.jsx(n.a,{href:"https://react.dev/",rel:"nofollow",children:"React"})," component library by Intuition Systems."]}),`
-`,t.jsx(n.h2,{id:"usage",children:"Usage"}),`
-`,t.jsx(i,{dark:!0,language:"bash",code:"pnpm install @0xintuition/1ui"}),`
-`,t.jsx(i,{dark:!0,language:"bash",code:"npm install @0xintuition/1ui"}),`
-`,t.jsx(i,{dark:!0,language:"bash",code:"yarn add @0xintuition/1ui"})]})}function M(o={}){const{wrapper:n}={...r(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(e,{...o})}):e(o)}export{M as default};
