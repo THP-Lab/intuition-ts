2a1499950254c8255b27169548612fe7e4f0f6ed
Vitalsine85 <rob@creative-element.net>
2024-07-12 04:24:51 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@82fd58a6691a3d9a441a012b6a30cb5ec46d2021 🚀

---
+++ assets/ClaimStakeCard-BOo2Dl5S.js
@@ -0,0 +1,20 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as n}from"./index-DSkyVWTJ.js";import{M as m,T as a,d as s,C as e,e as p}from"./index-B6U34Qgx.js";import{S as c,B as d}from"./ClaimStakeCard.stories-CLw60WO3.js";import"./index-CTjT7uj6.js";import"./iframe-C-UtSshj.js";import"../sb-preview/runtime.js";import"./index-Ba1MzMRK.js";import"./index-DBez60WA.js";import"./index-BSh8g_S6.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./QuestStatusCard-CJAcnthM.js";import"./index-DgtlF2hQ.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./QuestStatusIndicator-DHmvCJek.js";import"./Icon.types-D4lfunSt.js";import"./index-Bb4qSo10.js";import"./QuestSetProgressCard-BNr9QVR-.js";import"./createLucideIcon-BfM7wYCo.js";import"./ProgressBar-D75vLLzs.js";import"./Text-DsVeH9qI.js";import"./Label-4gKwHNvc.js";import"./PieChart-B_dtFskq.js";import"./Resizable-B7XdxUdz.js";import"./SegmentedControl-C43o2uqF.js";import"./Separator-EZUw1-4L.js";import"./Table-C0qIXiUM.js";import"./Textarea-CUiVvNKI.js";import"./ValueDisplay-D2U_pdiu.js";import"./QuestSetCard-BHXW0rRZ.js";function r(o){const i={h2:"h2",h4:"h4",...n(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{of:c}),`
+`,t.jsx(a,{}),`
+`,t.jsx(i.h4,{id:"card-displaying-claim-stake-information-and-actions",children:"Card displaying claim stake information and actions."}),`
+`,t.jsx(s,{dark:!0,language:"tsx",code:`
+import { ClaimStakeCard } from '@0xintuition/1ui'
+
+<ClaimStakeCard
+  currency="ETH"
+  totalTVL={4.928}
+  tvlAgainst={0.567}
+  tvlFor={3.643}
+  amountAgainst={39}
+  amountFor={124}
+  onAgainstBtnClick={() => doSomething}
+  onForBtnClick={() => doSomething}
+/>
+`}),`
+`,t.jsx(e,{of:d,sourceState:"none"}),`
+`,t.jsx(i.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(p,{})]})}function K(o={}){const{wrapper:i}={...n(),...o.components};return i?t.jsx(i,{...o,children:t.jsx(r,{...o})}):r(o)}export{K as default};
