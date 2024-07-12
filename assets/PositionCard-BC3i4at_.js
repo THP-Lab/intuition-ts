89c3594b98b1df4d43a84e509af3ebb86729234f
alexander-mann <alexandercmann@gmail.com>
2024-07-12 14:25:38 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@e324e6f72c270d5885b138cd5ffbdcb833fd22b9 🚀

---
+++ assets/PositionCard-BC3i4at_.js
@@ -0,0 +1,26 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as n}from"./index-DSkyVWTJ.js";import{M as e,T as s,d as a,C as p,e as m}from"./index-HVR8A0fn.js";import"./QuestSetProgressCard-DbXmtDWB.js";import{S as d,B as c}from"./PositionCard.stories-F6yKjYVL.js";import"./index-CTjT7uj6.js";import"./iframe-CLwQAj7-.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-DgtlF2hQ.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./Icon.types-DvvrcWKg.js";import"./Text-DsVeH9qI.js";import"./Label-BSnWqvXc.js";import"./PieChart-B_dtFskq.js";import"./SegmentedControl-C43o2uqF.js";import"./Separator-8RGezXAr.js";import"./Table-C0qIXiUM.js";import"./Textarea-CUiVvNKI.js";import"./ValueDisplay-D2U_pdiu.js";import"./QuestStatusIndicator-B_BWZovd.js";import"./ProgressBar-D75vLLzs.js";import"./QuestSetCard-BHXW0rRZ.js";function r(t){const i={h2:"h2",h4:"h4",...n(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(e,{of:d}),`
+`,o.jsx(s,{}),`
+`,o.jsx(i.h4,{id:"positioncard---a-card-to-display-a-position",children:"PositionCard - A card to display a position"}),`
+`,o.jsx(a,{dark:!0,language:"tsx",code:`
+import { PositionCard, PositionCardStaked, PositionCardOwnership, PositionCardFeesAccrued, PositionCardLastUpdated } from '@0xintuition/1ui'
+
+const ExampleUsage = () => {
+const handleSell = () => {
+console.log('Sell button clicked')
+}
+
+return (
+
+<PositionCard onButtonClick={handleSell}>
+  <PositionCardStaked amount={0.512} />
+  <PositionCardOwnership percentOwnership={24} />
+  <PositionCardFeesAccrued amount={0.005} />
+  <PositionCardLastUpdated timestamp="2024-05-10T00:00:00Z" />
+</PositionCard>
+) }
+
+export default ExampleUsage
+`}),`
+`,o.jsx(p,{of:c,sourceState:"none"}),`
+`,o.jsx(i.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(m,{})]})}function z(t={}){const{wrapper:i}={...n(),...t.components};return i?o.jsx(i,{...t,children:o.jsx(r,{...t})}):r(t)}export{z as default};
---
+++ assets/TagsListInput-LFm_kGl6.js
@@ -0,0 +1,55 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as e}from"./index-DSkyVWTJ.js";import{M as m,T as g,C as o,d as s}from"./index-HVR8A0fn.js";import{S as d,T as r,E as p,a as c}from"./TagsListInput.stories-C9JdDYED.js";import"./index-CTjT7uj6.js";import"./iframe-CLwQAj7-.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./QuestSetProgressCard-DbXmtDWB.js";import"./index-Bb4qSo10.js";import"./index-DgtlF2hQ.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./Icon.types-DvvrcWKg.js";import"./Text-DsVeH9qI.js";import"./Label-BSnWqvXc.js";import"./PieChart-B_dtFskq.js";import"./SegmentedControl-C43o2uqF.js";import"./Separator-8RGezXAr.js";import"./Table-C0qIXiUM.js";import"./Textarea-CUiVvNKI.js";import"./ValueDisplay-D2U_pdiu.js";import"./QuestStatusIndicator-B_BWZovd.js";import"./ProgressBar-D75vLLzs.js";import"./QuestSetCard-BHXW0rRZ.js";function n(i){const a={h3:"h3",h4:"h4",...e(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{of:d}),`
+`,t.jsx(g,{}),`
+`,t.jsx(a.h4,{id:"displays-list-of-tags-or-trust-circles-with-the-ability-to-add-more",children:"Displays list of Tags or Trust circles with the ability to add more"}),`
+`,t.jsx(o,{of:r,sourceState:"none"}),`
+`,t.jsx(a.h3,{id:"empty-state-variant",children:"Empty State Variant"}),`
+`,t.jsx(o,{of:p}),`
+`,t.jsx(s,{dark:!0,language:"tsx",code:`
+import { TagsListInput } from '@0xintuition/1ui'
+
+<TagsListInput
+  variant="trustCircle"
+  tags={[]}
+  maxTags={5}
+  onAddTag={() => console.log('Add Tag')}
+  onRemoveTag={(id: string) => console.log('Remove Tag', id)}
+/>
+`}),`
+`,t.jsx(a.h3,{id:"trust-circles-variant",children:"Trust Circles Variant"}),`
+`,t.jsx(o,{of:r}),`
+`,t.jsx(s,{dark:!0,language:"tsx",code:`
+import { TagsListInput } from '@0xintuition/1ui'
+
+const trustCirclesTags = [
+{ name: 'Tag Name 1', id: '1' },
+{ name: 'Tag Name 2', id: '2' },
+{ name: 'Tag Name 3', id: '3' },
+];
+
+<TagsListInput
+  variant="trustCircle"
+  tags={trustCirclesTags}
+  maxTags={5}
+  onAddTag={() => console.log('Add Tag')}
+  onRemoveTag={(id: string) => console.log('Remove Tag', id)}
+/>
+`}),`
+`,t.jsx(a.h3,{id:"tags-variant",children:"Tags Variant"}),`
+`,t.jsx(o,{of:c}),`
+`,t.jsx(s,{dark:!0,language:"tsx",code:`
+import { TagsListInput } from '@0xintuition/1ui'
+
+const tags = [
+{ name: 'Tag Name 1', id: '1' },
+{ name: 'Tag Name 2', id: '2' },
+{ name: 'Tag Name 3', id: '3' },
+];
+
+<TagsListInput
+  variant="tag"
+  tags={tags}
+  maxTags={5}
+  onAddTag={() => console.log('Add Tag')}
+  onRemoveTag={(id: string) => console.log('Remove Tag', id)}
+/>
+`})]})}function H(i={}){const{wrapper:a}={...e(),...i.components};return a?t.jsx(a,{...i,children:t.jsx(n,{...i})}):n(i)}export{H as default};
