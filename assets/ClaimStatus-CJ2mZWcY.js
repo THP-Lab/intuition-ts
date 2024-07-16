79bb8ab56753020976af76c686c775861c3090bf
Vitalsine85 <rob@creative-element.net>
2024-07-16 03:20:28 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@16fd235f6d248dffd4b2ba5619aa13a787810c48 🚀

---
+++ assets/ClaimStatus-CJ2mZWcY.js
@@ -0,0 +1,18 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as r}from"./index-DSkyVWTJ.js";import{M as s,T as a,d as n,C as e}from"./index-cHm_41BO.js";import"./QuestCard-CksNzbg6.js";import"./Icon.types-DvvrcWKg.js";import"./Label-BSnWqvXc.js";import"./PieChart-B_dtFskq.js";import"./SegmentedControl-C43o2uqF.js";import"./Separator-8RGezXAr.js";import"./Table-C0qIXiUM.js";import"./Text-DsVeH9qI.js";import"./Textarea-CUiVvNKI.js";import"./ValueDisplay-D2U_pdiu.js";import"./QuestSetCard-gHXGTuki.js";import"./ProgressBar-D75vLLzs.js";import"./QuestStatusIndicator-_HPKLqrf.js";import{S as p,B as l}from"./ClaimStatus.stories-pTEENqJm.js";import"./index-CTjT7uj6.js";import"./iframe-Dkp6X5_T.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-DgtlF2hQ.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function m(o){const i={h2:"h2",...r(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:p}),`
+`,t.jsx(a,{}),`
+`,t.jsx(i.h2,{id:"a-component-showing-the-status-of-the-positions-on-a-claim",children:"A component showing the status of the positions on a Claim"}),`
+`,t.jsx(n,{dark:!0,language:"tsx",code:`
+import { ClaimStatus, Claim } from './ClaimStatus'
+
+<ClaimStatus claimsFor={10} claimsAgainst={5}>
+  <Claim
+    size="sm"
+    subject={{label: '0xintuition' }}
+    predicate={{ label: 'is really' }}
+    object={{ label: 'cool' }}
+
+/>
+
+</ClaimStatus>
+`}),`
+`,t.jsx(e,{of:l,sourceState:"none"})]})}function H(o={}){const{wrapper:i}={...r(),...o.components};return i?t.jsx(i,{...o,children:t.jsx(m,{...o})}):m(o)}export{H as default};
---
+++ assets/RadioGroup.stories-DddpUUYo.js
@@ -0,0 +1,13 @@
+import{j as a}from"./jsx-runtime-Cw0GR0a5.js";import{al as l,am as t,an as n,ao as m,ap as p}from"./QuestCard-CksNzbg6.js";const c={title:"Components/Form Elements/RadioGroup",component:l},o=[{id:"minimum",value:"minimum",displayValue:"Minimum",displaySubValue:"+0.001 ETH"},{id:"minimum",value:"default",displayValue:"Default",displaySubValue:"+0.01 ETH"},{id:"minimum",value:"strong",displayValue:"Strong",displaySubValue:"+0.05 ETH"}],v=o.length,i={render:()=>a.jsx("div",{className:"w-[300px]",children:a.jsx(l,{defaultValue:o[0].value,children:o.map((e,u)=>a.jsxs("div",{children:[a.jsxs(t,{children:[a.jsx(n,{htmlFor:e.id,value:e.displayValue,subValue:e.displaySubValue}),a.jsx(m,{value:e.value,id:e.id})]}),u+1<v&&a.jsx(p,{})]},u))})})};var r,d,s;i.parameters={...i.parameters,docs:{...(r=i.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: () => <div className="w-[300px]">
+      <RadioGroup defaultValue={radioGroupData[0].value}>
+        {radioGroupData.map((item, index) => <div key={index}>
+            <RadioGroupItemContainer>
+              <RadioGroupItemLabel htmlFor={item.id} value={item.displayValue} subValue={item.displaySubValue} />
+              <RadioGroupItem value={item.value} id={item.id} />
+            </RadioGroupItemContainer>
+            {index + 1 < numberOfRadioGroupItems && <RadioGroupDivider />}
+          </div>)}
+      </RadioGroup>
+    </div>
+}`,...(s=(d=i.parameters)==null?void 0:d.docs)==null?void 0:s.source}}};const G=["BasicUsage"],b=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:i,__namedExportsOrder:G,default:c},Symbol.toStringTag,{value:"Module"}));export{i as B,b as S};
