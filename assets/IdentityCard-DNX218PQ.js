85329074a851bbdcdc612856205863765db9b4d5
jonathanprozzi <jonathanprozzi@gmail.com>
2024-07-16 02:34:02 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@f1ee3f1b22b4ba30ba9fd980150a65903d30955d 🚀

---
+++ assets/IdentityCard-DNX218PQ.js
@@ -0,0 +1,31 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as a}from"./index-DSkyVWTJ.js";import{M as s,T as m,d as i,C as n,e as p}from"./index-DKKjXN8P.js";import{S as d,B as c,E as u}from"./IdentityCard.stories-BiI0eg-4.js";import"./index-CTjT7uj6.js";import"./iframe-xBtqdK1R.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./QuestCard-EX4D-EbA.js";import"./QuestStatusIndicator-_HPKLqrf.js";import"./Icon.types-DvvrcWKg.js";import"./index-DgtlF2hQ.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./Text-DsVeH9qI.js";import"./index-Bb4qSo10.js";import"./Label-BSnWqvXc.js";import"./PieChart-B_dtFskq.js";import"./SegmentedControl-C43o2uqF.js";import"./Separator-8RGezXAr.js";import"./Table-C0qIXiUM.js";import"./Textarea-CUiVvNKI.js";import"./ValueDisplay-D2U_pdiu.js";import"./ProgressBar-D75vLLzs.js";import"./QuestSetCard-gHXGTuki.js";function o(e){const r={h2:"h2",h3:"h3",h4:"h4",...a(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:d}),`
+`,t.jsx(m,{}),`
+`,t.jsx(r.h4,{id:"displays-an-identity-or-entity-and-its-key-data",children:"Displays an identity or entity and its key data."}),`
+`,t.jsx(i,{dark:!0,language:"tsx",code:`
+import { IdentityCard } from '@0xintuition/1ui'
+
+<IdentityCard
+  variant="user"
+  avatarSrc="image.jpg"
+  name="Super Dave"
+  value={4.123}
+  currency="ETH"
+  walletAddress="0x1234567890abcdef1234567890abcdef12345678"
+/>
+`}),`
+`,t.jsx(n,{of:c,sourceState:"none"}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(p,{}),`
+`,t.jsx(r.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(r.h3,{id:"other",children:"Other"}),`
+`,t.jsx(i,{dark:!0,language:"tsx",code:`
+<IdentityCard
+  variant="non-user"
+  avatarSrc="https://avatars.githubusercontent.com/u/94311139?s=200&v=4"
+  name="Intuition"
+  value={7.892}
+  currency="ETH"
+  walletAddress="0x1234567890abcdef1234567890abcdef12345678"
+/>
+`}),`
+`,t.jsx(n,{of:u,sourceState:"none"})]})}function q(e={}){const{wrapper:r}={...a(),...e.components};return r?t.jsx(r,{...e,children:t.jsx(o,{...e})}):o(e)}export{q as default};
--- assets/Switch.stories-DGgzRebW.js
+++
@@ -1,7 +0,0 @@
-import{j as r}from"./jsx-runtime-Cw0GR0a5.js";import{aQ as s}from"./QuestCard-BYQ2a1mA.js";import"./Icon.types-DvvrcWKg.js";import{L as m}from"./Label-BSnWqvXc.js";import"./PieChart-B_dtFskq.js";import"./SegmentedControl-C43o2uqF.js";import"./Separator-8RGezXAr.js";import"./Table-C0qIXiUM.js";import"./Text-DsVeH9qI.js";import"./Textarea-CUiVvNKI.js";import"./ValueDisplay-D2U_pdiu.js";import"./QuestSetCard-gHXGTuki.js";import"./ProgressBar-D75vLLzs.js";import"./QuestStatusIndicator-_HPKLqrf.js";const n={title:"Components/Form Elements/Switch",component:s},e={args:{},render:i=>r.jsxs("div",{className:"flex items-center space-x-3",children:[r.jsx(s,{id:"airplane-mode",...i}),r.jsx(m,{htmlFor:"airplane-mode",children:"Airplane Mode"})]})};var o,t,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {},
-  render: args => <div className="flex items-center space-x-3">
-      <Switch id="airplane-mode" {...args} />
-      <Label htmlFor="airplane-mode">Airplane Mode</Label>
-    </div>
-}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const p=["BasicUsage"],w=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:e,__namedExportsOrder:p,default:n},Symbol.toStringTag,{value:"Module"}));export{e as B,w as S};
