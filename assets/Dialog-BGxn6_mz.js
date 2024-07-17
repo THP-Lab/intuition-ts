f1db6e2650773603acecbf43aa9a22542d9f651d
0xjojikun <110207608+0xjojikun@users.noreply.github.com>
2024-07-17 02:38:53 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@55b435aace9b3bc7cae27f28afaea9825ca850a2 🚀

---
+++ assets/Dialog-BGxn6_mz.js
@@ -0,0 +1,29 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as r}from"./index-DSkyVWTJ.js";import{M as n,T as a,d as m,C as p}from"./index-CNALDu_h.js";import{B as s}from"./QuestCard-Bg3s4-37.js";import"./Icon.types-DvvrcWKg.js";import"./Label-BSnWqvXc.js";import"./PieChart-B_dtFskq.js";import"./SegmentedControl-C43o2uqF.js";import"./Separator-8RGezXAr.js";import"./Table-C0qIXiUM.js";import"./Text-DsVeH9qI.js";import"./Textarea-CUiVvNKI.js";import"./ValueDisplay-D2U_pdiu.js";import"./QuestSetCard-gHXGTuki.js";import"./ProgressBar-D75vLLzs.js";import"./QuestStatusIndicator-_HPKLqrf.js";import{S as l,B as d}from"./Dialog.stories-Cf8I9yH6.js";import"./index-CTjT7uj6.js";import"./iframe-D9IF7_Ow.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-DgtlF2hQ.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function e(i){const t={h4:"h4",...r(),...i.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{of:l}),`
+`,o.jsx(a,{}),`
+`,o.jsx(t.h4,{id:"a-window-overlaid-on-either-the-primary-window-or-another-dialog-window-rendering-the-content-underneath-inert",children:"A window overlaid on either the primary window or another dialog window, rendering the content underneath inert."}),`
+`,o.jsx(s,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/dialog#api-reference","_blank"),children:"API Reference"}),`
+`,o.jsx(m,{dark:!0,language:"tsx",code:`
+import { 
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger, } from '@0xintuition/1ui'
+
+<Dialog>
+  <DialogTrigger>
+    <Button size="lg">Open dialog</Button>
+  </DialogTrigger>
+  <DialogContent>
+    <DialogHeader>
+      <DialogTitle>Are you absolutely sure?</DialogTitle>
+      <DialogDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </DialogDescription>
+    </DialogHeader>
+  </DialogContent>
+</Dialog>
+`}),`
+`,o.jsx(p,{of:d,sourceState:"none"})]})}function U(i={}){const{wrapper:t}={...r(),...i.components};return t?o.jsx(t,{...i,children:o.jsx(e,{...i})}):e(i)}export{U as default};
---
+++ assets/Trunctacular-KKTN-gVz.js
@@ -0,0 +1,22 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as i}from"./index-DSkyVWTJ.js";import{M as s,T as l,d as n,C as a,e as m}from"./index-CNALDu_h.js";import{S as p,B as u,W as c,N as d}from"./Trunctacular.stories-BMASCAW7.js";import"./index-CTjT7uj6.js";import"./iframe-D9IF7_Ow.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./QuestCard-Bg3s4-37.js";import"./QuestStatusIndicator-_HPKLqrf.js";import"./Icon.types-DvvrcWKg.js";import"./index-DgtlF2hQ.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./Text-DsVeH9qI.js";import"./index-Bb4qSo10.js";import"./Label-BSnWqvXc.js";import"./PieChart-B_dtFskq.js";import"./SegmentedControl-C43o2uqF.js";import"./Separator-8RGezXAr.js";import"./Table-C0qIXiUM.js";import"./Textarea-CUiVvNKI.js";import"./ValueDisplay-D2U_pdiu.js";import"./ProgressBar-D75vLLzs.js";import"./QuestSetCard-gHXGTuki.js";function e(r){const o={h2:"h2",h3:"h3",h4:"h4",...i(),...r.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:p}),`
+`,t.jsx(l,{}),`
+`,t.jsx(o.h4,{id:"a-super-component-that-truncates-long-strings---including-wallet-addresses---and-adds-a-tooltip-to-display-the-full-value",children:"A super component that truncates long strings - including wallet addresses - and adds a tooltip to display the full value."}),`
+`,t.jsx(n,{dark:!0,language:"tsx",code:`
+import { Trunctacular } from '@0xintuition/1ui'
+
+<Trunctacular value="reallyReallyLongName" />
+`}),`
+`,t.jsx(a,{of:u,sourceState:"none"}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(m,{}),`
+`,t.jsx(o.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(o.h3,{id:"wallet-usage",children:"Wallet Usage"}),`
+`,t.jsx(n,{dark:!0,language:"tsx",code:`
+<Trunctacular value="0x1234567890abcdef1234567890abcdef12345678" />
+`}),`
+`,t.jsx(a,{of:c,sourceState:"none"}),`
+`,t.jsx(o.h3,{id:"no-truncationtooltip",children:"No Truncation/Tooltip"}),`
+`,t.jsx(n,{dark:!0,language:"tsx",code:`
+Trunctacular value="shortName" />
+`}),`
+`,t.jsx(a,{of:d,sourceState:"none"})]})}function G(r={}){const{wrapper:o}={...i(),...r.components};return o?t.jsx(o,{...r,children:t.jsx(e,{...r})}):e(r)}export{G as default};
