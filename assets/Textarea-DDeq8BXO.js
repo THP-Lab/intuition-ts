e0978ac162c447ec090e5cbd7b70537a1024d7bf
Vitalsine85 <rob@creative-element.net>
2024-07-19 21:04:56 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@ae8cc3bb1c7c254e35613fff39f8718385364053 🚀

---
+++ assets/Textarea-DDeq8BXO.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as m}from"./index-DSkyVWTJ.js";import{M as e,T as p,d as a,C as n,e as s}from"./index-BS8_hubh.js";import"./Trunctacular-BWPGzJcf.js";import"./Icon.types-Csn6eVsj.js";import"./Label-l7hlfg-Q.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-CZqSfr2M.js";import"./ProgressCard-HIGHqO49.js";import"./QuestCriteriaCard-DU_nEwuR.js";import"./QuestCriteriaDisplay-BOc2v1Da.js";import"./QuestPointsDisplay-DOFHvDSK.js";import"./QuestSetCard-CAqByP9R.js";import"./QuestStatusIndicator-DjnhoyJG.js";import"./SegmentedControl-Dt9e-2dS.js";import"./Separator-DqkY-AaL.js";import"./Skeleton-CGZOk8OX.js";import"./Table-Cj9ooZRp.js";import"./Text-CIyuOUAi.js";import"./Textarea-BN2eJ_eZ.js";import"./ValueDisplay-OcmgwD6b.js";import{S as x,B as c}from"./Textarea.stories-CA9le8Wy.js";import"./index-CTjT7uj6.js";import"./iframe-MNuN-dHR.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-jZxEZZ9l.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./progress-DxZziHAc.js";import"./quest-BMAtC6cg.js";function i(o){const r={h2:"h2",h4:"h4",...m(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:x}),`
+`,t.jsx(p,{}),`
+`,t.jsx(r.h4,{id:"displays-a-form-textarea-or-a-component-that-looks-like-a-textarea",children:"Displays a form textarea or a component that looks like a textarea."}),`
+`,t.jsx(a,{dark:!0,language:"tsx",code:`
+import { Textarea } from '@0xintuition/1ui'
+
+<Textarea />
+`}),`
+`,t.jsx(n,{of:c,sourceState:"none"}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(s,{})]})}function V(o={}){const{wrapper:r}={...m(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{V as default};
---
+++ assets/TransactionStatusIndicator-CxMDmvjR.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as n}from"./index-DSkyVWTJ.js";import{M as s,T as m,d as a,C as p,e}from"./index-BS8_hubh.js";import{S as c,B as u}from"./TransactionStatusIndicator.stories-Ciim6a1p.js";import"./index-CTjT7uj6.js";import"./iframe-MNuN-dHR.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./Trunctacular-BWPGzJcf.js";import"./index-Bb4qSo10.js";import"./index-jZxEZZ9l.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./Icon.types-Csn6eVsj.js";import"./Text-CIyuOUAi.js";import"./Label-l7hlfg-Q.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-CZqSfr2M.js";import"./ProgressCard-HIGHqO49.js";import"./progress-DxZziHAc.js";import"./QuestCriteriaCard-DU_nEwuR.js";import"./QuestCriteriaDisplay-BOc2v1Da.js";import"./quest-BMAtC6cg.js";import"./QuestPointsDisplay-DOFHvDSK.js";import"./QuestSetCard-CAqByP9R.js";import"./QuestStatusIndicator-DjnhoyJG.js";import"./SegmentedControl-Dt9e-2dS.js";import"./Separator-DqkY-AaL.js";import"./Skeleton-CGZOk8OX.js";import"./Table-Cj9ooZRp.js";import"./Textarea-BN2eJ_eZ.js";import"./ValueDisplay-OcmgwD6b.js";function i(o){const r={h2:"h2",h4:"h4",...n(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:c}),`
+`,t.jsx(m,{}),`
+`,t.jsx(r.h4,{id:"displays-an-icon-and-label-per-transaction-status",children:"Displays an icon and label per transaction status."}),`
+`,t.jsx(a,{dark:!0,language:"tsx",code:`
+import { TransactionStatusIndicator } from '@0xintuition/1ui'
+
+<TransactionStatusIndicator status="awaiting" />
+`}),`
+`,t.jsx(p,{of:u,sourceState:"none"}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(e,{})]})}function V(o={}){const{wrapper:r}={...n(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{V as default};
