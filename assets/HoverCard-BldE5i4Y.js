dd5d9d7d13ffb697535696ba610f0759e48c6cd6
alexander-mann <alexandercmann@gmail.com>
2024-07-19 17:14:19 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@83ed8acb5bf71112ac7f858e3307e4b3c738c95a 🚀

---
+++ assets/HoverCard-BldE5i4Y.js
@@ -0,0 +1,42 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as i}from"./index-DSkyVWTJ.js";import{M as a,T as n,d as m,C as p}from"./index-DBzUcJPO.js";import{B as s}from"./Trunctacular-el536ZLH.js";import"./Icon.types-Csn6eVsj.js";import"./Label-l7hlfg-Q.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-CZqSfr2M.js";import"./ProgressCard-HIGHqO49.js";import"./QuestCriteriaCard-DU_nEwuR.js";import"./QuestCriteriaDisplay-BOc2v1Da.js";import"./QuestPointsDisplay-DOFHvDSK.js";import"./QuestSetCard-CAqByP9R.js";import"./QuestStatusIndicator-DjnhoyJG.js";import"./SegmentedControl-Dt9e-2dS.js";import"./Separator-DqkY-AaL.js";import"./Skeleton-CGZOk8OX.js";import"./Table-Cj9ooZRp.js";import"./Text-CIyuOUAi.js";import"./Textarea-BN2eJ_eZ.js";import"./ValueDisplay-OcmgwD6b.js";import{S as d,B as v}from"./HoverCard.stories-C_0EFPv9.js";import"./index-CTjT7uj6.js";import"./iframe-Bs5wG8tQ.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-jZxEZZ9l.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./progress-DxZziHAc.js";import"./quest-BMAtC6cg.js";function o(r){const e={h4:"h4",...i(),...r.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{of:d}),`
+`,t.jsx(n,{}),`
+`,t.jsx(e.h4,{id:"for-sighted-users-to-preview-content-available-behind-a-link",children:"For sighted users to preview content available behind a link."}),`
+`,t.jsx(s,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/hover-card#api-reference","_blank"),children:"API Reference"}),`
+`,t.jsx(m,{dark:!0,language:"tsx",code:`
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '@0xintuition/1ui'
+
+<HoverCard>
+  <HoverCardTrigger asChild>
+    <Button variant="text">@super_dave</Button>
+  </HoverCardTrigger>
+  <HoverCardContent className="w-60">
+    <div className="flex gap-4">
+      <Avatar src="image.jpg" name="Super Dave" />
+      <div>
+        <Text variant="body">Super Dave</Text>
+        <Text variant="caption" className="text-primary/50">
+          @super_dave
+        </Text>
+      </div>
+    </div>
+    <Text variant="caption">
+      I am Super Dave, the most super Dave you&apos;ll ever meet.
+    </Text>
+    <div className="flex gap-10">
+      <div>
+        <Text variant="body">9122</Text>
+        <Text variant="caption" className="text-primary/50">
+          followers
+        </Text>
+      </div>
+      <div>
+        <Text variant="body">1239</Text>
+        <Text variant="caption" className="text-primary/50">
+          following
+        </Text>
+      </div>
+    </div>
+  </HoverCardContent>
+</HoverCard>
+`}),`
+`,t.jsx(p,{of:v,sourceState:"none"})]})}function V(r={}){const{wrapper:e}={...i(),...r.components};return e?t.jsx(e,{...r,children:t.jsx(o,{...r})}):o(r)}export{V as default};
