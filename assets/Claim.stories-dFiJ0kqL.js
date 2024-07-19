0e8e55ecd321a66c3911cd0b9c830d4bdd61e7fd
jonathanprozzi <jonathanprozzi@gmail.com>
2024-07-19 02:12:07 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@5235bfdd8b596621458a5161a29a2187ec8f5371 🚀

---
+++ assets/Claim.stories-dFiJ0kqL.js
@@ -0,0 +1,35 @@
+import{j as n}from"./jsx-runtime-Cw0GR0a5.js";import{k as r,l as d}from"./Trunctacular-U3O8QJJw.js";const u={title:"Components/Claim/Claim",component:r,argTypes:{subject:{description:"Subject of the claim",table:{type:{summary:"string"}}},predicate:{description:"Predicate of the claim",table:{type:{summary:"string"}}},object:{description:"Object of the claim",table:{type:{summary:"string"}}},size:{description:"Size of component",options:Object.values(d),table:{type:{summary:"string"},defaultValue:{summary:"default"}},control:"select"}}},e={args:{size:"md",disabled:!1,subject:{variant:"non-user",label:"0xintution"},predicate:{variant:"non-user",label:"is really"},object:{variant:"non-user",label:"cool"}},render:m=>n.jsx("div",{className:"w-max margin",children:n.jsx(r,{...m})})},a={render:()=>n.jsx("div",{className:"w-max margin",children:n.jsx(r,{size:"md",subject:{variant:"user",label:"Alice",imgSrc:"https://m.media-amazon.com/images/M/MV5BNDhiMWYzMjgtNTRiYi00ZTA3LThlODctNDRkMDk0NzFkMWI3L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTg0MTkzMzA@._V1_.jpg"},predicate:{variant:"non-user",label:"likes"},object:{variant:"non-user",label:"pizza"}})})};var s,t,i;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    size: 'md',
+    disabled: false,
+    subject: {
+      variant: 'non-user',
+      label: '0xintution'
+    },
+    predicate: {
+      variant: 'non-user',
+      label: 'is really'
+    },
+    object: {
+      variant: 'non-user',
+      label: 'cool'
+    }
+  },
+  render: args => <div className="w-max margin">
+      <Claim {...args} />
+    </div>
+}`,...(i=(t=e.parameters)==null?void 0:t.docs)==null?void 0:i.source}}};var l,o,c;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: () => <div className="w-max margin">
+      <Claim size="md" subject={{
+      variant: 'user',
+      label: 'Alice',
+      imgSrc: 'https://m.media-amazon.com/images/M/MV5BNDhiMWYzMjgtNTRiYi00ZTA3LThlODctNDRkMDk0NzFkMWI3L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTg0MTkzMzA@._V1_.jpg'
+    }} predicate={{
+      variant: 'non-user',
+      label: 'likes'
+    }} object={{
+      variant: 'non-user',
+      label: 'pizza'
+    }} />
+    </div>
+}`,...(c=(o=a.parameters)==null?void 0:o.docs)==null?void 0:c.source}}};const p=["BasicUsage","User"],j=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:e,User:a,__namedExportsOrder:p,default:u},Symbol.toStringTag,{value:"Module"}));export{e as B,j as S,a as U};
---
+++ assets/Resizable-dplwQERT.js
@@ -0,0 +1,64 @@
+import{j as e}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as o}from"./index-DSkyVWTJ.js";import{M as l,T as s,d as t,C as n}from"./index-DSu8U3b8.js";import{B as p}from"./Trunctacular-U3O8QJJw.js";import"./Icon.types-Csn6eVsj.js";import"./Label-l7hlfg-Q.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-CZqSfr2M.js";import"./ProgressCard-HIGHqO49.js";import"./QuestCriteriaCard-DU_nEwuR.js";import"./QuestCriteriaDisplay-BOc2v1Da.js";import"./QuestPointsDisplay-DOFHvDSK.js";import"./QuestSetCard-CAqByP9R.js";import"./QuestStatusIndicator-DjnhoyJG.js";import"./SegmentedControl-Dt9e-2dS.js";import"./Separator-DqkY-AaL.js";import"./Skeleton-CGZOk8OX.js";import"./Table-Cj9ooZRp.js";import"./Text-CIyuOUAi.js";import"./Textarea-BN2eJ_eZ.js";import"./ValueDisplay-OcmgwD6b.js";import{S as d,B as m,V as b,a as u}from"./Resizable.stories-Dl04I2IX.js";import"./index-CTjT7uj6.js";import"./iframe-DPYnlbcS.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-jZxEZZ9l.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./progress-DxZziHAc.js";import"./quest-BMAtC6cg.js";function r(i){const a={h2:"h2",h3:"h3",h4:"h4",...o(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(l,{of:d}),`
+`,e.jsx(s,{}),`
+`,e.jsx(a.h4,{id:"accessible-resizable-panel-groups-and-layouts-with-keyboard-support",children:"Accessible resizable panel groups and layouts with keyboard support."}),`
+`,e.jsx(p,{variant:"accent",onClick:()=>window.open("https://github.com/bvaughn/react-resizable-panels/tree/main/packages/react-resizable-panels","_blank"),children:"API Reference"}),`
+`,e.jsx(t,{dark:!0,language:"tsx",code:`
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@0xintuition/1ui'
+
+<ResizablePanelGroup
+  direction="horizontal"
+>
+  <ResizablePanel defaultSize={50}>
+    <div>
+      <span>One</span>
+    </div>
+  </ResizablePanel>
+  <ResizableHandle />
+  <ResizablePanel defaultSize={50}>
+    <ResizablePanelGroup direction="vertical">
+      <ResizablePanel defaultSize={25}>
+        <div>
+          <span>Two</span>
+        </div>
+      </ResizablePanel>
+      <ResizableHandle />
+      <ResizablePanel defaultSize={75}>
+        <div>
+          <span>Three</span>
+        </div>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  </ResizablePanel>
+`}),`
+`,e.jsx(n,{of:m,sourceState:"none"}),`
+`,e.jsx(a.h2,{id:"variants",children:"Variants"}),`
+`,e.jsx(a.h3,{id:"vertical",children:"Vertical"}),`
+`,e.jsx(t,{dark:!0,language:"tsx",code:`
+<ResizablePanelGroup
+  direction="vertical"
+>
+  <ResizablePanel defaultSize={50}>
+    Top
+  </ResizablePanel>
+  <ResizableHandle />
+  <ResizablePanel defaultSize={50}>
+    Bottom
+  </ResizablePanel>
+</ResizablePanelGroup>
+`}),`
+`,e.jsx(n,{of:b,sourceState:"none"}),`
+`,e.jsx(a.h3,{id:"sidebar",children:"Sidebar"}),`
+`,e.jsx(t,{dark:!0,language:"tsx",code:`
+<ResizablePanelGroup
+  direction="horizontal"
+>
+  <ResizablePanel defaultSize={30}>
+    Sidebar
+  </ResizablePanel>
+  <ResizableHandle withHandle />
+  <ResizablePanel defaultSize={70}>
+    Content
+  </ResizablePanel>
+</ResizablePanelGroup>
+`}),`
+`,e.jsx(n,{of:u,sourceState:"none"})]})}function Y(i={}){const{wrapper:a}={...o(),...i.components};return a?e.jsx(a,{...i,children:e.jsx(r,{...i})}):r(i)}export{Y as default};
