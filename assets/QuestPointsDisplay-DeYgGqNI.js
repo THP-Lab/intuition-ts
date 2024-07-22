c81e7157c46ce972f1161f39aa4ae3e148871e53
alexander-mann <alexandercmann@gmail.com>
2024-07-22 17:25:17 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@1a6466d1a5cfdffe44ebf4db0b404f3d26a79992 🚀

---
+++ assets/QuestPointsDisplay-DeYgGqNI.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as n}from"./index-DSkyVWTJ.js";import{M as e,T as i,d as a,C as m,e as p}from"./index-C702Sgmu.js";import{S as u,B as c}from"./QuestPointsDisplay.stories-Cz8GspWr.js";import"./index-CTjT7uj6.js";import"./iframe-DWZaUx6a.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./quest-BMAtC6cg.js";import"./QuestPointsDisplay-DOFHvDSK.js";import"./Text-CIyuOUAi.js";import"./index-Bb4qSo10.js";import"./index-jZxEZZ9l.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";function r(o){const s={h2:"h2",h4:"h4",...n(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:u}),`
+`,t.jsx(i,{}),`
+`,t.jsx(s.h4,{id:"display-to-show-number-of-points-of-a-quest-based-on-quest-status",children:"Display to show number of points of a quest based on quest status."}),`
+`,t.jsx(a,{dark:!0,language:"tsx",code:`
+import { QuestPointsDisplay } from '@0xintuition/1ui'
+
+<QuestPointsDisplay points={100} questStatus="not-started" />
+`}),`
+`,t.jsx(m,{of:c,sourceState:"none"}),`
+`,t.jsx(s.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(p,{})]})}function _(o={}){const{wrapper:s}={...n(),...o.components};return s?t.jsx(s,{...o,children:t.jsx(r,{...o})}):r(o)}export{_ as default};
---
+++ assets/SocialLinks-Cep45fRo.js
@@ -0,0 +1,25 @@
+import{j as i}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as t}from"./index-DSkyVWTJ.js";import{M as a,T as s,d as n,C as m}from"./index-C702Sgmu.js";import{S as p,B as c}from"./SocialLinks.stories-Dupfv0dg.js";import"./index-CTjT7uj6.js";import"./iframe-DWZaUx6a.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./Trunctacular-jWlQkj6Y.js";import"./index-Bb4qSo10.js";import"./index-jZxEZZ9l.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./Icon.types-Csn6eVsj.js";import"./Text-CIyuOUAi.js";import"./Label-l7hlfg-Q.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-CZqSfr2M.js";import"./ProgressCard-HIGHqO49.js";import"./progress-DxZziHAc.js";import"./QuestCriteriaCard-DU_nEwuR.js";import"./QuestCriteriaDisplay-BOc2v1Da.js";import"./quest-BMAtC6cg.js";import"./QuestPointsDisplay-DOFHvDSK.js";import"./QuestSetCard-CAqByP9R.js";import"./QuestStatusIndicator-DjnhoyJG.js";import"./SegmentedControl-Dt9e-2dS.js";import"./Separator-DqkY-AaL.js";import"./Skeleton-CGZOk8OX.js";import"./Table-Cj9ooZRp.js";import"./Textarea-BN2eJ_eZ.js";import"./ValueDisplay-OcmgwD6b.js";function e(o){const r={h4:"h4",...t(),...o.components};return i.jsxs(i.Fragment,{children:[i.jsx(a,{of:p}),`
+`,i.jsx(s,{}),`
+`,i.jsx(r.h4,{id:"displays-a-users-social-media-connections",children:"Displays a user's social media connections."}),`
+`,i.jsx(n,{dark:!0,language:"tsx",code:`
+import { 
+  SocialLinks
+  SocialLinksBadge,
+  SocialLinksBadges,
+  SocialLinksButton, 
+} from '@0xintuition/1ui'
+
+<SocialLinks>
+  <SocialLinksBadges>
+    <SocialLinksBadge isVerified platform="discord" username="@superdave" />
+    <SocialLinksBadge isVerified platform="x" username="@superdave" />
+    <SocialLinksBadge platform="farcaster" username="@superdave" />
+    <SocialLinksBadge platform="lens" username="@superdave" />
+    <SocialLinksBadge isVerified platform="calendly" username="@superdave" />
+    <SocialLinksBadge platform="github" username="@superdave" />
+    <SocialLinksBadge platform="medium" username="@superdave" />
+  </SocialLinksBadges>
+  <SocialLinksButton onClick={() => doSomething()} />
+</SocialLinks>
+`}),`
+`,i.jsx(m,{of:c,sourceState:"none"})]})}function P(o={}){const{wrapper:r}={...t(),...o.components};return r?i.jsx(r,{...o,children:i.jsx(e,{...o})}):e(o)}export{P as default};
