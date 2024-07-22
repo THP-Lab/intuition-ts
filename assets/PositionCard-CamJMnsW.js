9cd6e74fa1dac2525f330ce0341e3e422fcdd85f
alexander-mann <alexandercmann@gmail.com>
2024-07-22 18:18:07 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@d9d1cb17c46045587b8d7e4e6cb406c768e8d365 🚀

---
+++ assets/PositionCard-CamJMnsW.js
@@ -0,0 +1,26 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as n}from"./index-DSkyVWTJ.js";import{M as e,T as s,d as a,C as p,e as m}from"./index-jD1mmVJQ.js";import"./Trunctacular-BB92s1Jy.js";import{S as d,B as c}from"./PositionCard.stories-TVIxE9oW.js";import"./index-CTjT7uj6.js";import"./iframe-CnGYYJrY.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-jZxEZZ9l.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./Icon.types-Csn6eVsj.js";import"./Text-CIyuOUAi.js";import"./Label-l7hlfg-Q.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-CZqSfr2M.js";import"./ProgressCard-HIGHqO49.js";import"./progress-DxZziHAc.js";import"./QuestCriteriaCard-DU_nEwuR.js";import"./QuestCriteriaDisplay-BOc2v1Da.js";import"./quest-BMAtC6cg.js";import"./QuestPointsDisplay-DOFHvDSK.js";import"./QuestSetCard-CAqByP9R.js";import"./QuestStatusIndicator-DjnhoyJG.js";import"./SegmentedControl-Dt9e-2dS.js";import"./Separator-DqkY-AaL.js";import"./Skeleton-CGZOk8OX.js";import"./Table-Cj9ooZRp.js";import"./Textarea-BN2eJ_eZ.js";import"./ValueDisplay-OcmgwD6b.js";function r(t){const i={h2:"h2",h4:"h4",...n(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(e,{of:d}),`
+`,o.jsx(s,{}),`
+`,o.jsx(i.h4,{id:"positioncard---a-card-to-display-a-position",children:"PositionCard - A card to display a position"}),`
+`,o.jsx(a,{dark:!0,language:"tsx",code:`
+import { PositionCard, PositionCardStaked, PositionCardOwnership, PositionCardFeesAccrued, PositionCardLastUpdated } from '@0xintuition/1ui'
+
+const ExampleUsage = () => {
+const handleSell = () => {
+console.log('Sell button clicked')
+}
+
+return (
+
+<PositionCard onButtonClick={handleSell}>
+  <PositionCardStaked amount={0.512} />
+  <PositionCardOwnership percentOwnership={24} />
+  <PositionCardFeesAccrued amount={0.005} />
+  <PositionCardLastUpdated timestamp="2024-05-10T00:00:00Z" />
+</PositionCard>
+) }
+
+export default ExampleUsage
+`}),`
+`,o.jsx(p,{of:c,sourceState:"none"}),`
+`,o.jsx(i.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(m,{})]})}function Q(t={}){const{wrapper:i}={...n(),...t.components};return i?o.jsx(i,{...t,children:o.jsx(r,{...t})}):r(t)}export{Q as default};
