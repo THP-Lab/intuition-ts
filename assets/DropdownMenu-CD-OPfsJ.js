735a6c2dca9f9f78d2bf906bbd5fd599622c9aac
jonathanprozzi <jonathanprozzi@gmail.com>
2024-07-22 16:03:00 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@1b61d3dc94b829e76354b5e711c9d8d6eeebf2c2 🚀

---
+++ assets/DropdownMenu-CD-OPfsJ.js
@@ -0,0 +1,29 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as e}from"./index-DSkyVWTJ.js";import{M as i,T as p,d as m,C as u}from"./index-CRnqZJse.js";import{B as s}from"./Trunctacular-Btpgyr5l.js";import"./Icon.types-Csn6eVsj.js";import"./Label-l7hlfg-Q.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-CZqSfr2M.js";import"./ProgressCard-HIGHqO49.js";import"./QuestCriteriaCard-DU_nEwuR.js";import"./QuestCriteriaDisplay-BOc2v1Da.js";import"./QuestPointsDisplay-DOFHvDSK.js";import"./QuestSetCard-CAqByP9R.js";import"./QuestStatusIndicator-DjnhoyJG.js";import"./SegmentedControl-Dt9e-2dS.js";import"./Separator-DqkY-AaL.js";import"./Skeleton-CGZOk8OX.js";import"./Table-Cj9ooZRp.js";import"./Text-CIyuOUAi.js";import"./Textarea-BN2eJ_eZ.js";import"./ValueDisplay-OcmgwD6b.js";import{S as a,B as d}from"./DropdownMenu.stories-DZvBaNZx.js";import"./index-CTjT7uj6.js";import"./iframe-D07OJwqG.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-jZxEZZ9l.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./progress-DxZziHAc.js";import"./quest-BMAtC6cg.js";function r(n){const t={h4:"h4",...e(),...n.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{of:a}),`
+`,o.jsx(p,{}),`
+`,o.jsx(t.h4,{id:"displays-a-menu-to-the-user--such-as-a-set-of-actions-or-functions--triggered-by-a-button",children:"Displays a menu to the user — such as a set of actions or functions — triggered by a button."}),`
+`,o.jsx(s,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/dropdown-menu#api-reference","_blank"),children:"API Reference"}),`
+`,o.jsx(m,{dark:!0,language:"tsx",code:`
+import { 
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger, 
+} from '@0xintuition/1ui'
+
+<DropdownMenu>
+  <DropdownMenuTrigger>
+    <Button size="lg">Open menu</Button>
+  </DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>Profile</DropdownMenuItem>
+    <DropdownMenuItem>Billing</DropdownMenuItem>
+    <DropdownMenuItem>Team</DropdownMenuItem>
+    <DropdownMenuItem>Subscription</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+`}),`
+`,o.jsx(u,{of:d,sourceState:"none"})]})}function V(n={}){const{wrapper:t}={...e(),...n.components};return t?o.jsx(t,{...n,children:o.jsx(r,{...n})}):r(n)}export{V as default};
