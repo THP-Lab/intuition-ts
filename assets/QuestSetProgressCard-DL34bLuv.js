17760bafadf419479a0a699be31b396635fc1d91
Vitalsine85 <rob@creative-element.net>
2024-07-23 17:32:41 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@1f3f0ca50a25770e245c2c0f920debb99e80e52f 🚀

---
+++ assets/QuestSetProgressCard-DL34bLuv.js
@@ -0,0 +1,19 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as e}from"./index-DSkyVWTJ.js";import{M as s,T as m,d as n,C as p,e as a}from"./index-E5-lsTla.js";import{S as u,B as c}from"./QuestSetProgressCard.stories-CC233Ky_.js";import"./index-CTjT7uj6.js";import"./iframe-Co1_NKgx.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./TransactionStatusIndicator-C6RVtnax.js";import"./index-jZxEZZ9l.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Icon.types-Csn6eVsj.js";import"./Text-CIyuOUAi.js";import"./Label-l7hlfg-Q.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-CZqSfr2M.js";import"./ProgressCard-HIGHqO49.js";import"./progress-DxZziHAc.js";import"./QuestCriteriaCard-DU_nEwuR.js";import"./QuestCriteriaDisplay-BOc2v1Da.js";import"./quest-BMAtC6cg.js";import"./QuestPointsDisplay-DOFHvDSK.js";import"./QuestSetCard-CAqByP9R.js";import"./QuestStatusIndicator-DjnhoyJG.js";import"./SegmentedControl-Dt9e-2dS.js";import"./Separator-DqkY-AaL.js";import"./Skeleton-CGZOk8OX.js";import"./Table-Cj9ooZRp.js";import"./Textarea-BN2eJ_eZ.js";import"./ValueDisplay-OcmgwD6b.js";function i(o){const r={h2:"h2",h4:"h4",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:u}),`
+`,t.jsx(m,{}),`
+`,t.jsx(r.h4,{id:"a-card-to-show-a-users-latest-progress-on-a-quest-set",children:"A card to show a user's latest progress on a quest set."}),`
+`,t.jsx(n,{dark:!0,language:"tsx",code:`
+import { QuestSetProgressCard } from '@0xintuition/1ui'
+
+<QuestSetProgressCard
+  imgSrc={
+    'https://m.media-amazon.com/images/M/MV5BNDhiMWYzMjgtNTRiYi00ZTA3LThlODctNDRkMDk0NzFkMWI3L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTg0MTkzMzA@._V1_.jpg'
+  }
+  title={'Tutorial Island: The Primitive Elements'}
+  numberQuests={10}
+  numberCompletedQuests={5}
+  onButtonClick={() => console.log('Button clicked')}
+/>
+`}),`
+`,t.jsx(p,{of:c,sourceState:"none"}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(a,{})]})}function Z(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{Z as default};
