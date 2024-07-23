6ac781470ee38e58b1de3926b183f96c9a75cede
Vitalsine85 <rob@creative-element.net>
2024-07-23 20:24:57 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@4b85133067be46d2fe4181e03b12bce89ec024f4 🚀

---
+++ assets/ClaimRow-CnkuyNRG.js
@@ -0,0 +1,21 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as m}from"./index-DSkyVWTJ.js";import{M as e,T as n,d as a,C as p,e as s}from"./index-CBjJEHwp.js";import{S as l,B as c}from"./ClaimRow.stories-DHUFu7d_.js";import"./index-CTjT7uj6.js";import"./iframe-B0zvm98V.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./TransactionStatusIndicator-nbxCD90L.js";import"./index-jZxEZZ9l.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Icon.types-Csn6eVsj.js";import"./Text-CIyuOUAi.js";import"./Label-l7hlfg-Q.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-CZqSfr2M.js";import"./ProgressCard-HIGHqO49.js";import"./progress-DxZziHAc.js";import"./QuestCriteriaCard-DU_nEwuR.js";import"./QuestCriteriaDisplay-BOc2v1Da.js";import"./quest-BMAtC6cg.js";import"./QuestPointsDisplay-DOFHvDSK.js";import"./QuestSetCard-CAqByP9R.js";import"./QuestStatusIndicator-DjnhoyJG.js";import"./SegmentedControl-Dt9e-2dS.js";import"./Separator-DqkY-AaL.js";import"./Skeleton-CGZOk8OX.js";import"./Table-Cj9ooZRp.js";import"./Textarea-BN2eJ_eZ.js";import"./ValueDisplay-OcmgwD6b.js";function r(o){const i={h2:"h2",h4:"h4",...m(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:l}),`
+`,t.jsx(n,{}),`
+`,t.jsx(i.h4,{id:"a-representation-of-claim-details",children:"A representation of claim details."}),`
+`,t.jsx(a,{dark:!0,language:"tsx",code:`
+import { ClaimRow, Claim } from '@0xintuition/1ui'
+
+<ClaimRow claimsFor={736} claimsAgainst={234} amount={0.383}>
+  <Claim
+    size="sm"
+    subject={{
+      variant: 'default',
+      label: '0xintuition',
+    }}
+    predicate={{ label: 'is really' }}
+    object={{ label: 'cool' }}
+  />
+</ClaimRow>
+`}),`
+`,t.jsx(p,{of:c,sourceState:"none"}),`
+`,t.jsx(i.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(s,{})]})}function V(o={}){const{wrapper:i}={...m(),...o.components};return i?t.jsx(i,{...o,children:t.jsx(r,{...o})}):r(o)}export{V as default};
---
+++ assets/ClaimStatus-C3dgxN-t.js
@@ -0,0 +1,18 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as r}from"./index-DSkyVWTJ.js";import{M as s,T as a,d as n,C as e}from"./index-CBjJEHwp.js";import"./TransactionStatusIndicator-nbxCD90L.js";import"./Icon.types-Csn6eVsj.js";import"./Label-l7hlfg-Q.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-CZqSfr2M.js";import"./ProgressCard-HIGHqO49.js";import"./QuestCriteriaCard-DU_nEwuR.js";import"./QuestCriteriaDisplay-BOc2v1Da.js";import"./QuestPointsDisplay-DOFHvDSK.js";import"./QuestSetCard-CAqByP9R.js";import"./QuestStatusIndicator-DjnhoyJG.js";import"./SegmentedControl-Dt9e-2dS.js";import"./Separator-DqkY-AaL.js";import"./Skeleton-CGZOk8OX.js";import"./Table-Cj9ooZRp.js";import"./Text-CIyuOUAi.js";import"./Textarea-BN2eJ_eZ.js";import"./ValueDisplay-OcmgwD6b.js";import{S as p,B as l}from"./ClaimStatus.stories-CW1Serlg.js";import"./index-CTjT7uj6.js";import"./iframe-B0zvm98V.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-jZxEZZ9l.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./progress-DxZziHAc.js";import"./quest-BMAtC6cg.js";function m(o){const i={h2:"h2",...r(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:p}),`
+`,t.jsx(a,{}),`
+`,t.jsx(i.h2,{id:"a-component-showing-the-status-of-the-positions-on-a-claim",children:"A component showing the status of the positions on a Claim"}),`
+`,t.jsx(n,{dark:!0,language:"tsx",code:`
+import { ClaimStatus, Claim } from './ClaimStatus'
+
+<ClaimStatus claimsFor={10} claimsAgainst={5}>
+  <Claim
+    size="sm"
+    subject={{label: '0xintuition' }}
+    predicate={{ label: 'is really' }}
+    object={{ label: 'cool' }}
+
+/>
+
+</ClaimStatus>
+`}),`
+`,t.jsx(e,{of:l,sourceState:"none"})]})}function Q(o={}){const{wrapper:i}={...r(),...o.components};return i?t.jsx(i,{...o,children:t.jsx(m,{...o})}):m(o)}export{Q as default};
