a08c889db44cd200e1e7f1215d96512c21cd1cac
alexander-mann <alexandercmann@gmail.com>
2024-07-23 19:00:03 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@fab96b85d7c985ffe395271b3a4fb4e4f561980e 🚀

---
+++ assets/QuestHeaderCard-CjaFYdFd.js
@@ -0,0 +1,17 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as e}from"./index-DSkyVWTJ.js";import{M as m,T as s,d as n,C as p,e as a}from"./index-Ccu2PWYV.js";import{S as u,B as d}from"./QuestHeaderCard.stories-DFV-t8o7.js";import"./index-CTjT7uj6.js";import"./iframe-By5qRJrg.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./TransactionStatusIndicator-DYmLWzfI.js";import"./index-jZxEZZ9l.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Icon.types-Csn6eVsj.js";import"./Text-CIyuOUAi.js";import"./Label-l7hlfg-Q.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-CZqSfr2M.js";import"./ProgressCard-HIGHqO49.js";import"./progress-DxZziHAc.js";import"./QuestCriteriaCard-DU_nEwuR.js";import"./QuestCriteriaDisplay-BOc2v1Da.js";import"./quest-BMAtC6cg.js";import"./QuestPointsDisplay-DOFHvDSK.js";import"./QuestSetCard-CAqByP9R.js";import"./QuestStatusIndicator-DjnhoyJG.js";import"./SegmentedControl-Dt9e-2dS.js";import"./Separator-DqkY-AaL.js";import"./Skeleton-CGZOk8OX.js";import"./Table-Cj9ooZRp.js";import"./Textarea-BN2eJ_eZ.js";import"./ValueDisplay-OcmgwD6b.js";function i(o){const r={h2:"h2",h4:"h4",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{of:u}),`
+`,t.jsx(s,{}),`
+`,t.jsx(r.h4,{id:"a-card-to-show-quest-data-to-a-user",children:"A card to show quest data to a user."}),`
+`,t.jsx(n,{dark:!0,language:"tsx",code:`
+import { QuestHeaderCard } from '@0xintuition/1ui'
+
+<QuestHeaderCard
+  title="Primitive Island"
+  subtitle="Continue your journey."
+  numberOfCompletedQuests={1}
+  totalNumberOfQuests={10}
+  onButtonClick={() => doSomething}
+/>
+`}),`
+`,t.jsx(p,{of:d,sourceState:"none"}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(a,{})]})}function V(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{V as default};
