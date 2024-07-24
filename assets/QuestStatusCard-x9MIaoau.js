db1bd4662333dc2cc44dd21a268efe98676752b6
Vitalsine85 <rob@creative-element.net>
2024-07-24 20:56:33 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@b72647c7212bd7302048fc9b689f8d97805f80e4 🚀

---
+++ assets/QuestStatusCard-x9MIaoau.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as s}from"./index-DSkyVWTJ.js";import{M as p,T as n,d as a,C as m,e}from"./index-CqPRa9cC.js";import{S as u,B as c}from"./QuestStatusCard.stories-Dn_lREwz.js";import"./index-CTjT7uj6.js";import"./iframe-CkTZhusz.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./quest-BMAtC6cg.js";import"./TransactionStatusIndicator-BsFkQWl8.js";import"./index-jZxEZZ9l.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Icon.types-cQmbtfph.js";import"./Text-CIyuOUAi.js";import"./Label-l7hlfg-Q.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-CZqSfr2M.js";import"./ProgressCard-8XoeDUlW.js";import"./QuestCriteriaCard-DKN7bypV.js";import"./QuestCriteriaDisplay-13wHbsZ-.js";import"./QuestPointsDisplay-DOFHvDSK.js";import"./QuestStatusIndicator-DRoc4dCN.js";import"./SegmentedControl-Dt9e-2dS.js";import"./Separator-DqkY-AaL.js";import"./Skeleton-CGZOk8OX.js";import"./Table-Cj9ooZRp.js";import"./Textarea-BN2eJ_eZ.js";import"./ValueDisplay-OcmgwD6b.js";function i(o){const r={h2:"h2",h4:"h4",...s(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(p,{of:u}),`
+`,t.jsx(n,{}),`
+`,t.jsx(r.h4,{id:"displays-a-card-with-an-icon-and-label-per-quest-status-with-an-optional-tooltip",children:"Displays a card with an icon and label per quest status with an optional tooltip."}),`
+`,t.jsx(a,{dark:!0,language:"tsx",code:`
+import { QuestStatusCard } from '@0xintuition/1ui'
+
+<QuestStatusCard status="not-started" tooltip="Example text" />
+`}),`
+`,t.jsx(m,{of:c,sourceState:"none"}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(e,{})]})}function N(o={}){const{wrapper:r}={...s(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{N as default};
