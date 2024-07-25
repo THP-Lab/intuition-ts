81f758bcaa5d3e6d43b011d5274e0b66f4a60556
0xjojikun <110207608+0xjojikun@users.noreply.github.com>
2024-07-25 01:32:23 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@7755115e28da8af79596a83effbaa24d4a66c8a9 🚀

---
+++ assets/TransactionStatusIndicator-CGrzwBbv.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as n}from"./index-DSkyVWTJ.js";import{M as s,T as a,d as m,C as e,e as p}from"./index-BwdgC6FK.js";import{S as c,B as u}from"./TransactionStatusIndicator.stories-ZJmW60F3.js";import"./index-CTjT7uj6.js";import"./iframe-B5ldTAuX.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./TransactionStatusIndicator-DeNK6_YW.js";import"./index-jZxEZZ9l.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Icon.types-cQmbtfph.js";import"./Text-CIyuOUAi.js";import"./Label-l7hlfg-Q.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-CZqSfr2M.js";import"./ProgressCard-8XoeDUlW.js";import"./SegmentedControl-BdOzZl2k.js";import"./Separator-DqkY-AaL.js";import"./Skeleton-CGZOk8OX.js";import"./Table-Cj9ooZRp.js";import"./Textarea-BN2eJ_eZ.js";import"./ValueDisplay-OcmgwD6b.js";function i(o){const r={h2:"h2",h4:"h4",...n(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:c}),`
+`,t.jsx(a,{}),`
+`,t.jsx(r.h4,{id:"displays-an-icon-and-label-per-transaction-status",children:"Displays an icon and label per transaction status."}),`
+`,t.jsx(m,{dark:!0,language:"tsx",code:`
+import { TransactionStatusIndicator } from '@0xintuition/1ui'
+
+<TransactionStatusIndicator status="awaiting" />
+`}),`
+`,t.jsx(e,{of:u,sourceState:"none"}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(p,{})]})}function G(o={}){const{wrapper:r}={...n(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{G as default};
