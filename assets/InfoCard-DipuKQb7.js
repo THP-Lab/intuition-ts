947fd00b2f584592cbab54717272b0f2ccaf21fc
rahulr8 <37151383+rahulr8@users.noreply.github.com>
2024-07-26 15:48:37 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@6744af3115523a7533b5f365b55ea500632c3aa6 🚀

---
+++ assets/InfoCard-DipuKQb7.js
@@ -0,0 +1,16 @@
+import{j as r}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as m}from"./index-DSkyVWTJ.js";import{M as e,T as n,d as a,C as p,e as s}from"./index-DhlueKbY.js";import{S as c,B as u}from"./InfoCard.stories-BVnhBHeJ.js";import"./index-CTjT7uj6.js";import"./iframe-BxHuTExO.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./TransactionStatusIndicator-B4AGxD12.js";import"./index-jZxEZZ9l.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Icon.types-cQmbtfph.js";import"./Text-CIyuOUAi.js";import"./Label-l7hlfg-Q.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-CZqSfr2M.js";import"./ProgressCard-8XoeDUlW.js";import"./SegmentedControl-BdOzZl2k.js";import"./Separator-DqkY-AaL.js";import"./Skeleton-CGZOk8OX.js";import"./Table-Cj9ooZRp.js";import"./Textarea-BN2eJ_eZ.js";import"./ValueDisplay-OcmgwD6b.js";function i(t){const o={h2:"h2",h4:"h4",...m(),...t.components};return r.jsxs(r.Fragment,{children:[r.jsx(e,{of:c}),`
+`,r.jsx(n,{}),`
+`,r.jsx(o.h4,{id:"claim-creator-info-card",children:"Claim creator info card."}),`
+`,r.jsx(a,{dark:!0,language:"tsx",code:`
+import { InfoCard } from '@0xintuition/1ui'
+
+<InfoCard
+  variant="user"
+  username="super dave"
+  avatarImgSrc="image.jpg"
+  timestamp="2024-05-10T00:00:00Z"
+/>
+`}),`
+`,r.jsx(p,{of:u,sourceState:"none"}),`
+`,r.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,r.jsx(s,{})]})}function A(t={}){const{wrapper:o}={...m(),...t.components};return o?r.jsx(o,{...t,children:r.jsx(i,{...t})}):i(t)}export{A as default};
