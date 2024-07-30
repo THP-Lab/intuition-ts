2c42de95a47f734e7ced8e5ebe0f2d39ae169b96
jonathanprozzi <jonathanprozzi@gmail.com>
2024-07-30 18:56:52 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@cceaf8a695ab8e807036d4bbb087744652b7082c 🚀

---
+++ assets/HoverCard-D8sPjWUZ.js
@@ -0,0 +1,42 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as a}from"./index-DSkyVWTJ.js";import{M as i,T as n,d as s,C as m}from"./index-DaADIiZH.js";import{B as p}from"./TransactionStatusIndicator-CR1OKTF0.js";import"./Icon.types-DihBWXYw.js";import"./Label-l7hlfg-Q.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-CZqSfr2M.js";import"./ProgressCard-8XoeDUlW.js";import"./SegmentedControl-BdOzZl2k.js";import"./Separator-DqkY-AaL.js";import"./Skeleton-CGZOk8OX.js";import"./Table-Cj9ooZRp.js";import"./Text-CIyuOUAi.js";import"./Textarea-BN2eJ_eZ.js";import"./ValueDisplay-OcmgwD6b.js";import{S as d,B as v}from"./HoverCard.stories-D2ukRFnp.js";import"./index-CTjT7uj6.js";import"./iframe-CErI41CN.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-jZxEZZ9l.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function o(e){const r={h4:"h4",...a(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:d}),`
+`,t.jsx(n,{}),`
+`,t.jsx(r.h4,{id:"for-sighted-users-to-preview-content-available-behind-a-link",children:"For sighted users to preview content available behind a link."}),`
+`,t.jsx(p,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/hover-card#api-reference","_blank"),children:"API Reference"}),`
+`,t.jsx(s,{dark:!0,language:"tsx",code:`
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '@0xintuition/1ui'
+
+<HoverCard>
+  <HoverCardTrigger asChild>
+    <Button variant="text">@super_dave</Button>
+  </HoverCardTrigger>
+  <HoverCardContent className="w-60">
+    <div className="flex gap-4">
+      <Avatar src="image.jpg" name="Super Dave" />
+      <div>
+        <Text variant="body">Super Dave</Text>
+        <Text variant="caption" className="text-primary/50">
+          @super_dave
+        </Text>
+      </div>
+    </div>
+    <Text variant="caption">
+      I am Super Dave, the most super Dave you&apos;ll ever meet.
+    </Text>
+    <div className="flex gap-10">
+      <div>
+        <Text variant="body">9122</Text>
+        <Text variant="caption" className="text-primary/50">
+          followers
+        </Text>
+      </div>
+      <div>
+        <Text variant="body">1239</Text>
+        <Text variant="caption" className="text-primary/50">
+          following
+        </Text>
+      </div>
+    </div>
+  </HoverCardContent>
+</HoverCard>
+`}),`
+`,t.jsx(m,{of:v,sourceState:"none"})]})}function q(e={}){const{wrapper:r}={...a(),...e.components};return r?t.jsx(r,{...e,children:t.jsx(o,{...e})}):o(e)}export{q as default};
