ce0ecd07b5fbfbf32b31e2ce230ff129c9286b5f
Vitalsine85 <rob@creative-element.net>
2024-07-30 15:28:39 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@9a6e1c8d8f5a1017958812967504fe2695538cac 🚀

---
+++ assets/ClaimRow.stories-Cy2U-w80.js
@@ -0,0 +1,21 @@
+import{j as r}from"./jsx-runtime-Cw0GR0a5.js";import{n as t,k as s}from"./TransactionStatusIndicator-waHXA7SQ.js";import"./Icon.types-DihBWXYw.js";import"./Label-l7hlfg-Q.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-CZqSfr2M.js";import"./ProgressCard-8XoeDUlW.js";import"./SegmentedControl-BdOzZl2k.js";import"./Separator-DqkY-AaL.js";import"./Skeleton-CGZOk8OX.js";import"./Table-Cj9ooZRp.js";import"./Text-CIyuOUAi.js";import"./Textarea-BN2eJ_eZ.js";import"./ValueDisplay-OcmgwD6b.js";const l={title:"Components/Claim/ClaimRow",component:t},a={args:{claimsFor:736,claimsAgainst:234,amount:.383},render:i=>r.jsx("div",{className:"w-[600px]",children:r.jsx(t,{...i,children:r.jsx(s,{subject:{variant:"non-user",label:"0xintuition"},predicate:{variant:"non-user",label:"is really"},object:{variant:"non-user",label:"cool"}})})})};var n,e,o;a.parameters={...a.parameters,docs:{...(n=a.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    claimsFor: 736,
+    claimsAgainst: 234,
+    amount: 0.383
+  },
+  render: args => <div className="w-[600px]">
+      <ClaimRow {...args}>
+        <Claim subject={{
+        variant: 'non-user',
+        label: '0xintuition'
+      }} predicate={{
+        variant: 'non-user',
+        label: 'is really'
+      }} object={{
+        variant: 'non-user',
+        label: 'cool'
+      }} />
+      </ClaimRow>
+    </div>
+}`,...(o=(e=a.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const m=["BasicUsage"],S=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:a,__namedExportsOrder:m,default:l},Symbol.toStringTag,{value:"Module"}));export{a as B,S};
