d95912d1f0091155835711849445cf78dec49857
0xjojikun <110207608+0xjojikun@users.noreply.github.com>
2024-07-30 22:29:59 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@c0fd44ab1ffdc86ec0335b08c5573bb129cd2944 🚀

---
+++ assets/Skeleton-O43FLSZq.js
@@ -0,0 +1,9 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as r}from"./index-DSkyVWTJ.js";import{M as i,T as m,d as s,C as a}from"./index-DZyOOjeG.js";import{S as p,B as c}from"./Skeleton.stories-CCT5BTKT.js";import"./index-CTjT7uj6.js";import"./iframe-CtAMZdAm.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./Skeleton-CGZOk8OX.js";import"./index-jZxEZZ9l.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";function e(t){const n={h4:"h4",...r(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{of:p}),`
+`,o.jsx(m,{}),`
+`,o.jsx(n.h4,{id:"skeleton-component-for-fallback-ui",children:"Skeleton component for fallback UI"}),`
+`,o.jsx(s,{dark:!0,language:"tsx",code:`
+import { Skeleton } from '@0xintuition/1ui'
+
+<Skeleton />
+`}),`
+`,o.jsx(a,{of:c,sourceState:"none"})]})}function U(t={}){const{wrapper:n}={...r(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(e,{...t})}):e(t)}export{U as default};
