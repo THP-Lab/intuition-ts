86dde9e84aa8d9f7e8bec81aeb75605bddfc631d
Vitalsine85 <rob@creative-element.net>
2024-07-30 15:40:12 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@ef441d600960882766bddbc2d50b908e48c55c17 🚀

---
+++ assets/Switch-hzXFdROl.js
@@ -0,0 +1,13 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as i}from"./index-DSkyVWTJ.js";import{M as n,T as m,d as a,C as p}from"./index-mgu4_Zv_.js";import{B as s}from"./TransactionStatusIndicator-Ch7yAnuv.js";import"./Icon.types-DihBWXYw.js";import"./Label-l7hlfg-Q.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-CZqSfr2M.js";import"./ProgressCard-8XoeDUlW.js";import"./SegmentedControl-BdOzZl2k.js";import"./Separator-DqkY-AaL.js";import"./Skeleton-CGZOk8OX.js";import"./Table-Cj9ooZRp.js";import"./Text-CIyuOUAi.js";import"./Textarea-BN2eJ_eZ.js";import"./ValueDisplay-OcmgwD6b.js";import{S as c,B as d}from"./Switch.stories-gOYJYJcY.js";import"./index-CTjT7uj6.js";import"./iframe-DD1GKNW1.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-jZxEZZ9l.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function r(o){const e={h4:"h4",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:c}),`
+`,t.jsx(m,{}),`
+`,t.jsx(e.h4,{id:"a-control-that-allows-the-user-to-toggle-between-checked-and-not-checked",children:"A control that allows the user to toggle between checked and not checked."}),`
+`,t.jsx(s,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/switch#api-reference","_blank"),children:"API Reference"}),`
+`,t.jsx(a,{dark:!0,language:"tsx",code:`
+import { Switch } from '@0xintuition/1ui'
+
+<div>
+  <Switch id="airplane-mode" />
+  <Label htmlFor="airplane-mode">Airplane Mode</Label>
+</div>
+`}),`
+`,t.jsx(p,{of:d,sourceState:"none"})]})}function G(o={}){const{wrapper:e}={...i(),...o.components};return e?t.jsx(e,{...o,children:t.jsx(r,{...o})}):r(o)}export{G as default};
--- assets/Switch.stories-cqdp73nn.js
+++
@@ -1,7 +0,0 @@
-import{j as r}from"./jsx-runtime-Cw0GR0a5.js";import{aS as s}from"./TransactionStatusIndicator-waHXA7SQ.js";import"./Icon.types-DihBWXYw.js";import{L as m}from"./Label-l7hlfg-Q.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-CZqSfr2M.js";import"./ProgressCard-8XoeDUlW.js";import"./SegmentedControl-BdOzZl2k.js";import"./Separator-DqkY-AaL.js";import"./Skeleton-CGZOk8OX.js";import"./Table-Cj9ooZRp.js";import"./Text-CIyuOUAi.js";import"./Textarea-BN2eJ_eZ.js";import"./ValueDisplay-OcmgwD6b.js";const n={title:"Components/Form Elements/Switch",component:s},e={args:{},render:i=>r.jsxs("div",{className:"flex items-center space-x-3",children:[r.jsx(s,{id:"airplane-mode",...i}),r.jsx(m,{htmlFor:"airplane-mode",children:"Airplane Mode"})]})};var o,t,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {},
-  render: args => <div className="flex items-center space-x-3">
-      <Switch id="airplane-mode" {...args} />
-      <Label htmlFor="airplane-mode">Airplane Mode</Label>
-    </div>
-}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const p=["BasicUsage"],w=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:e,__namedExportsOrder:p,default:n},Symbol.toStringTag,{value:"Module"}));export{e as B,w as S};
