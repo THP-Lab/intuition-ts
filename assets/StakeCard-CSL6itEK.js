b53dfeca6fd88350a1c191997724f0883ffa5ddf
rahulr8 <37151383+rahulr8@users.noreply.github.com>
2024-07-31 21:53:18 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@ddfc9ce9ea164f3f65cb1a18722092551c24f4d0 🚀

---
+++ assets/StakeCard-CSL6itEK.js
@@ -0,0 +1,16 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as s}from"./index-DSkyVWTJ.js";import{M as e,T as n,d as m,C as p,e as a}from"./index-Bzave4_Z.js";import{S as c,B as d}from"./StakeCard.stories-Dan5L0S5.js";import"./index-CTjT7uj6.js";import"./iframe-DafhDQDX.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./TransactionStatusIndicator-D_VxfvH8.js";import"./index-jZxEZZ9l.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Icon.types-D9GNVByd.js";import"./Text-CIyuOUAi.js";import"./Label-l7hlfg-Q.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-CZqSfr2M.js";import"./ProgressCard-Cezx5cpz.js";import"./SegmentedControl-BdOzZl2k.js";import"./Separator-DqkY-AaL.js";import"./Skeleton-CGZOk8OX.js";import"./Table-Cj9ooZRp.js";import"./Textarea-BN2eJ_eZ.js";import"./ValueDisplay-OcmgwD6b.js";function i(t){const r={h2:"h2",h4:"h4",...s(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(e,{of:c}),`
+`,o.jsx(n,{}),`
+`,o.jsx(r.h4,{id:"a-display-card-to-show-a-users-stake-position",children:"A display card to show a user's stake position."}),`
+`,o.jsx(m,{dark:!0,language:"tsx",code:`
+import { StakeCard } from '@0xintuition/1ui'
+
+<StakeCard
+  tvl={4.928}
+  holders={69}
+  onBuyClick={() => doSomething()}
+  onViewAllClick={() => doSomething()}
+/>
+`}),`
+`,o.jsx(p,{of:d,sourceState:"none"}),`
+`,o.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(a,{})]})}function G(t={}){const{wrapper:r}={...s(),...t.components};return r?o.jsx(r,{...t,children:o.jsx(i,{...t})}):i(t)}export{G as default};
