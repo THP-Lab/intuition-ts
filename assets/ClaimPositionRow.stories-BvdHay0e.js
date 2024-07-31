ae21e1911e79f5a5921ed532a584197144ce17cf
Vitalsine85 <rob@creative-element.net>
2024-07-31 19:08:36 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@eb067b42e607d6c7344b451e53f35d16f4a2f3f9 🚀

---
+++ assets/ClaimPositionRow.stories-BvdHay0e.js
@@ -0,0 +1,38 @@
+import{j as e}from"./jsx-runtime-Cw0GR0a5.js";import{m as t,k as d}from"./TransactionStatusIndicator-BFrOJT6A.js";import"./Icon.types-DihBWXYw.js";import"./Label-l7hlfg-Q.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-CZqSfr2M.js";import"./ProgressCard-Cezx5cpz.js";import"./SegmentedControl-BdOzZl2k.js";import"./Separator-DqkY-AaL.js";import"./Skeleton-CGZOk8OX.js";import"./Table-Cj9ooZRp.js";import"./Text-CIyuOUAi.js";import"./Textarea-BN2eJ_eZ.js";import"./ValueDisplay-OcmgwD6b.js";const p={title:"Components/Claim/ClaimPositionRow",component:t},a={args:{variant:"user",position:"claimFor",name:"John Doe",walletAddress:"0x1234567890abcdef1234567890abcdef12345678",avatarSrc:'https://avatars.githubusercontent.com/u/94311139?s=200&v=4"',amount:1.21,feesAccrued:.005,updatedAt:"2021-10-01T16:00:00Z"},render:r=>e.jsx("div",{className:"w-[800px]",children:e.jsx(t,{...r})})},n={args:{variant:"claim",position:"claimAgainst",claimsFor:30,claimsAgainst:70,amount:1.21,feesAccrued:.005},render:r=>e.jsx("div",{className:"w-[800px]",children:e.jsx(t,{...r,children:e.jsx(d,{subject:{variant:"non-user",label:"0xintuition"},predicate:{variant:"non-user",label:"is really"},object:{variant:"non-user",label:"cool"}})})})};var o,i,s;a.parameters={...a.parameters,docs:{...(o=a.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    variant: 'user',
+    position: 'claimFor',
+    name: 'John Doe',
+    walletAddress: '0x1234567890abcdef1234567890abcdef12345678',
+    avatarSrc: 'https://avatars.githubusercontent.com/u/94311139?s=200&v=4"',
+    amount: 1.21,
+    feesAccrued: 0.005,
+    updatedAt: '2021-10-01T16:00:00Z'
+  },
+  render: args => <div className="w-[800px]">
+      <ClaimPositionRow {...args}></ClaimPositionRow>
+    </div>
+}`,...(s=(i=a.parameters)==null?void 0:i.docs)==null?void 0:s.source}}};var c,m,l;n.parameters={...n.parameters,docs:{...(c=n.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    variant: 'claim',
+    position: 'claimAgainst',
+    claimsFor: 30,
+    claimsAgainst: 70,
+    amount: 1.21,
+    feesAccrued: 0.005
+  },
+  render: args => <div className="w-[800px]">
+      <ClaimPositionRow {...args}>
+        <Claim subject={{
+        variant: 'non-user',
+        label: '0xintuition'
+      }} predicate={{
+        variant: 'non-user',
+        label: 'is really'
+      }} object={{
+        variant: 'non-user',
+        label: 'cool'
+      }} />
+      </ClaimPositionRow>
+    </div>
+}`,...(l=(m=n.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};const u=["UserVariant","IdentityVariant"],_=Object.freeze(Object.defineProperty({__proto__:null,IdentityVariant:n,UserVariant:a,__namedExportsOrder:u,default:p},Symbol.toStringTag,{value:"Module"}));export{n as I,_ as S,a as U};
---
+++ assets/Dialog.stories-DSXu_NHa.js
@@ -0,0 +1,17 @@
+import{j as e}from"./jsx-runtime-Cw0GR0a5.js";import{D as n,y as s,B as l,z as g,E as c,F as m,G as p}from"./TransactionStatusIndicator-BFrOJT6A.js";import"./Icon.types-DihBWXYw.js";import"./Label-l7hlfg-Q.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-CZqSfr2M.js";import"./ProgressCard-Cezx5cpz.js";import"./SegmentedControl-BdOzZl2k.js";import"./Separator-DqkY-AaL.js";import"./Skeleton-CGZOk8OX.js";import"./Table-Cj9ooZRp.js";import"./Text-CIyuOUAi.js";import"./Textarea-BN2eJ_eZ.js";import"./ValueDisplay-OcmgwD6b.js";const d={title:"Components/Dialog",component:n},o={args:{},render:t=>e.jsxs(n,{...t,children:[e.jsx(s,{children:e.jsx(l,{size:"lg",children:"Open dialog"})}),e.jsx(g,{children:e.jsxs(c,{children:[e.jsx(m,{children:"Are you absolutely sure?"}),e.jsx(p,{children:"This action cannot be undone. This will permanently delete your account and remove your data from our servers."})]})})]})};var r,i,a;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {},
+  render: args => <Dialog {...args}>
+      <DialogTrigger>
+        <Button size="lg">Open dialog</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+}`,...(a=(i=o.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};const u=["BasicUsage"],C=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:o,__namedExportsOrder:u,default:d},Symbol.toStringTag,{value:"Module"}));export{o as B,C as S};
