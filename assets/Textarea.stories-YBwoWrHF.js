81634332e24a3ddece0f42fddef425271ae49cf5
alexander-mann <alexandercmann@gmail.com>
2024-08-01 15:24:38 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@2b7d689990740a174b14445fcbea663a828eb0f5 🚀

---
+++ assets/Textarea.stories-YBwoWrHF.js
@@ -0,0 +1,6 @@
+import{j as n}from"./jsx-runtime-Cw0GR0a5.js";import{T as t}from"./Textarea-CSqXU4Tb.js";const c={title:"Components/Form Elements/Textarea",component:t,argTypes:{placeholder:{description:"Placeholder text",table:{type:{summary:"string"}},control:"text"}}},e={args:{placeholder:""},render:o=>n.jsx(t,{...o,className:"w-[400px]"})};var r,a,s;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    placeholder: ''
+  },
+  render: args => <Textarea {...args} className="w-[400px]" />
+}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const l=["BasicUsage"],d=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:e,__namedExportsOrder:l,default:c},Symbol.toStringTag,{value:"Module"}));export{e as B,d as S};
