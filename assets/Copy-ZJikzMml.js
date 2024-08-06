734f0d5c520bbf66fd1d3f26fbe02c08022413cf
jonathanprozzi <jonathanprozzi@gmail.com>
2024-08-06 06:41:36 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@184434f66236dee2ed53c2f1e02de9d301608d43 🚀

---
+++ assets/Copy-ZJikzMml.js
@@ -0,0 +1,17 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as n}from"./index-DSkyVWTJ.js";import{M as s,T as a,d as p,C as r,e as m}from"./index-CaJqm-ab.js";import{B as c}from"./TransactionStatusIndicator-D_0pFZsA.js";import"./Icon.types-xxOQRm7p.js";import"./Label-CLv4BT0x.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-BEma7yvq.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Text-COZqca-9.js";import"./Textarea-CSqXU4Tb.js";import"./ValueDisplay-BLpqF4VW.js";import{S as d,B as x,D as h,a as l}from"./Copy.stories-CZYLc1gL.js";import"./index-CTjT7uj6.js";import"./iframe-WS1JgdDT.js";import"../sb-preview/runtime.js";import"./index-BJDhk0Ya.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function e(i){const o={h2:"h2",h3:"h3",h4:"h4",...n(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:d}),`
+`,t.jsx(a,{}),`
+`,t.jsx(o.h4,{id:"copies-text-that-is-fed-into-it-as-a-prop",children:"Copies text that is fed into it as a prop."}),`
+`,t.jsx(c,{variant:"accent",onClick:()=>window.open("link_goes_here","_blank"),children:"API Reference"}),`
+`,t.jsx(p,{dark:!0,language:"tsx",code:`
+import { Copy } from '@0xintuition/1ui'
+
+<Copy text="Text to copy" />
+`}),`
+`,t.jsx(r,{of:x,sourceState:"none"}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(m,{}),`
+`,t.jsx(o.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(o.h3,{id:"default",children:"Default"}),`
+`,t.jsx(r,{of:h,sourceState:"none"}),`
+`,t.jsx(o.h3,{id:"disabled",children:"Disabled"}),`
+`,t.jsx(r,{of:l,sourceState:"none"})]})}function J(i={}){const{wrapper:o}={...n(),...i.components};return o?t.jsx(o,{...i,children:t.jsx(e,{...i})}):e(i)}export{J as default};
