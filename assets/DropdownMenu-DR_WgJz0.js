83dc43152fce7e2ed00c74a78ab759f6d89ad0e3
rahulr8 <37151383+rahulr8@users.noreply.github.com>
2024-08-06 20:46:20 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@717d2ed7329534393582ded6454396ddd1975713 🚀

---
+++ assets/DropdownMenu-DR_WgJz0.js
@@ -0,0 +1,29 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as r}from"./index-DSkyVWTJ.js";import{M as i,T as p,d as m,C as u}from"./index-NMeLcqYt.js";import{B as s}from"./TransactionStatusIndicator-C50Xb2VF.js";import"./Icon.types-BGCTrsKU.js";import"./Label-BcH9Dz7N.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-CECijyNn.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Text-COZqca-9.js";import"./Textarea-CSqXU4Tb.js";import"./ValueDisplay-BLpqF4VW.js";import{S as a,B as d}from"./DropdownMenu.stories-OMTqbSg0.js";import"./index-CTjT7uj6.js";import"./iframe-Cr0cexqO.js";import"../sb-preview/runtime.js";import"./index-C5rLi0fd.js";import"./index-CjlJelaC.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function e(n){const t={h4:"h4",...r(),...n.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{of:a}),`
+`,o.jsx(p,{}),`
+`,o.jsx(t.h4,{id:"displays-a-menu-to-the-user--such-as-a-set-of-actions-or-functions--triggered-by-a-button",children:"Displays a menu to the user — such as a set of actions or functions — triggered by a button."}),`
+`,o.jsx(s,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/dropdown-menu#api-reference","_blank"),children:"API Reference"}),`
+`,o.jsx(m,{dark:!0,language:"tsx",code:`
+import { 
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger, 
+} from '@0xintuition/1ui'
+
+<DropdownMenu>
+  <DropdownMenuTrigger>
+    <Button size="lg">Open menu</Button>
+  </DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>Profile</DropdownMenuItem>
+    <DropdownMenuItem>Billing</DropdownMenuItem>
+    <DropdownMenuItem>Team</DropdownMenuItem>
+    <DropdownMenuItem>Subscription</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+`}),`
+`,o.jsx(u,{of:d,sourceState:"none"})]})}function q(n={}){const{wrapper:t}={...r(),...n.components};return t?o.jsx(t,{...n,children:o.jsx(e,{...n})}):e(n)}export{q as default};
---
+++ assets/Pagination-Dp4SbXkW.js
@@ -0,0 +1,69 @@
+import{j as i}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as r}from"./index-DSkyVWTJ.js";import{M as g,T as m,d as a,C as o}from"./index-NMeLcqYt.js";import{S as s,B as P,A as p}from"./Pagination.stories-BF1oCweF.js";import"./index-CTjT7uj6.js";import"./iframe-Cr0cexqO.js";import"../sb-preview/runtime.js";import"./index-C5rLi0fd.js";import"./index-CjlJelaC.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./TransactionStatusIndicator-C50Xb2VF.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Icon.types-BGCTrsKU.js";import"./Text-COZqca-9.js";import"./Label-BcH9Dz7N.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-CECijyNn.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Textarea-CSqXU4Tb.js";import"./ValueDisplay-BLpqF4VW.js";function e(n){const t={h2:"h2",h3:"h3",h4:"h4",...r(),...n.components};return i.jsxs(i.Fragment,{children:[i.jsx(g,{of:s}),`
+`,i.jsx(m,{}),`
+`,i.jsx(t.h4,{id:"pagination-with-page-navigation-next-and-previous-links",children:"Pagination with page navigation, next and previous links."}),`
+`,i.jsx(a,{dark:!0,language:"tsx",code:`
+import { 
+  Pagination,
+  PaginationContent,
+  PaginationFirst,
+  PaginationItem,
+  PaginationLast,
+  PaginationNext,
+  PaginationPageCounter,
+  PaginationPrevious,
+  PaginationRowSelection,
+  PaginationSummary, 
+  } from '@0xintuition/1ui'
+
+<Pagination className="flex w-[800px] justify-between">
+  <PaginationSummary totalEntries={100} label="users" />
+  <div className="flex">
+    <PaginationRowSelection defaultValue="10" />
+    <PaginationPageCounter currentPage={1} totalPages={10} />
+    <PaginationContent>
+      <PaginationItem>
+        <PaginationFirst href="#" disabled />
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationPrevious href="#" disabled />
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationNext href="#" />
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLast href="#" />
+      </PaginationItem>
+    </PaginationContent>
+  </div>
+</Pagination>
+`}),`
+`,i.jsx(o,{of:P,sourceState:"none"}),`
+`,i.jsx(t.h2,{id:"variants",children:"Variants"}),`
+`,i.jsx(t.h3,{id:"alternate-setup",children:"Alternate Setup"}),`
+`,i.jsx(a,{dark:!0,language:"tsx",code:`
+  <Pagination>
+    <PaginationContent>
+      <PaginationItem>
+        <PaginationPrevious href="#" disabled />
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink href="#" isActive>
+          1
+        </PaginationLink>
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink href="#">2</PaginationLink>
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink href="#">3</PaginationLink>
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationEllipsis />
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationNext href="#" />
+      </PaginationItem>
+    </PaginationContent>
+  </Pagination>
+`}),`
+`,i.jsx(o,{of:p,sourceState:"none"})]})}function q(n={}){const{wrapper:t}={...r(),...n.components};return t?i.jsx(t,{...n,children:i.jsx(e,{...n})}):e(n)}export{q as default};
