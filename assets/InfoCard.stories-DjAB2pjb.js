7a337de36a81eae00999c3001e8b082fb8c1d7e2
alexander-mann <alexandercmann@gmail.com>
2024-08-06 14:26:56 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@fc2e334b010520212d6929c4ed02c8c9e193153d 🚀

--- assets/InfoCard.stories-DjAB2pjb.js
+++
@@ -1,9 +0,0 @@
-import{j as n}from"./jsx-runtime-Cw0GR0a5.js";import{Z as s,I as m}from"./TransactionStatusIndicator-DjLmF9qZ.js";const i={title:"Components/InfoCard",component:s,argTypes:{variant:{description:"Variant of identity tag component",options:Object.values(m),table:{type:{summary:"string"},defaultValue:{summary:"user"}},control:"select"}}},e={args:{variant:"user",username:"super dave",avatarImgSrc:"https://m.media-amazon.com/images/M/MV5BNDhiMWYzMjgtNTRiYi00ZTA3LThlODctNDRkMDk0NzFkMWI3L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTg0MTkzMzA@._V1_.jpg",timestamp:"2024-05-10T00:00:00Z"},render:o=>n.jsx(s,{...o})};var a,t,r;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    variant: 'user',
-    username: 'super dave',
-    avatarImgSrc: 'https://m.media-amazon.com/images/M/MV5BNDhiMWYzMjgtNTRiYi00ZTA3LThlODctNDRkMDk0NzFkMWI3L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTg0MTkzMzA@._V1_.jpg',
-    timestamp: '2024-05-10T00:00:00Z'
-  },
-  render: args => <InfoCard {...args} />
-}`,...(r=(t=e.parameters)==null?void 0:t.docs)==null?void 0:r.source}}};const c=["BasicUsage"],l=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:e,__namedExportsOrder:c,default:i},Symbol.toStringTag,{value:"Module"}));export{e as B,l as S};
---
+++ assets/Table-AesOeKCW.js
@@ -0,0 +1,44 @@
+import{j as e}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as l}from"./index-DSkyVWTJ.js";import{M as i,T as r,d as n,C as m}from"./index-CPyWA_zp.js";import"./TransactionStatusIndicator-C37lfnQ_.js";import"./Icon.types-xxOQRm7p.js";import"./Label-BcH9Dz7N.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-CECijyNn.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Text-COZqca-9.js";import"./Textarea-CSqXU4Tb.js";import"./ValueDisplay-BLpqF4VW.js";import{S as s,B as T}from"./Table.stories-DcVQETyi.js";import"./index-CTjT7uj6.js";import"./iframe-C-x6X97r.js";import"../sb-preview/runtime.js";import"./index-C5rLi0fd.js";import"./index-CjlJelaC.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function t(o){const a={h4:"h4",...l(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:s}),`
+`,e.jsx(r,{}),`
+`,e.jsx(a.h4,{id:"a-responsive-table-component",children:"A responsive table component."}),`
+`,e.jsx(n,{dark:!0,language:"tsx",code:`
+import { 
+  Table
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow, 
+} from '@0xintuition/1ui'
+
+<Table>
+  <TableCaption>A list of your recent invoices.</TableCaption>
+  <TableHeader>
+    <TableRow>
+      <TableHead className="w-[100px]">Invoice</TableHead>
+      <TableHead>Status</TableHead>
+      <TableHead>Method</TableHead>
+      <TableHead className="text-right">Amount</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    {invoices.map((invoice) => (
+      <TableRow key={invoice.invoice}>
+        <TableCell className="font-medium">{invoice.invoice}</TableCell>
+        <TableCell>{invoice.paymentStatus}</TableCell>
+        <TableCell>{invoice.paymentMethod}</TableCell>
+        <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+      </TableRow>
+    ))}
+  </TableBody>
+  <TableFooter>
+    <TableRow>
+      <TableCell colSpan={3}>Total</TableCell>
+      <TableCell className="text-right">$2,500.00</TableCell>
+    </TableRow>
+  </TableFooter>
+</Table>
+`}),`
+`,e.jsx(m,{of:T,sourceState:"none"})]})}function $(o={}){const{wrapper:a}={...l(),...o.components};return a?e.jsx(a,{...o,children:e.jsx(t,{...o})}):t(o)}export{$ as default};
