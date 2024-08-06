c48554708749ec58cc145cbf6440753f7cb12a07
jonathanprozzi <jonathanprozzi@gmail.com>
2024-08-06 02:30:54 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@a34701ca7489661cd73a80ca5bb58af961da02fe 🚀

---
+++ assets/ProfileCard.stories-Gztz4kh6.js
@@ -0,0 +1,37 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{am as r,I as m,B as u}from"./TransactionStatusIndicator-BWnpvmiC.js";const p={title:"Components/ProfileCard",component:r,argTypes:{variant:{description:"Variant of avatar",options:Object.values(m),table:{type:{summary:"string"},defaultValue:{summary:"user"}},control:"select"},avatarSrc:{description:"URL of the avatar image",table:{type:{summary:"string"}}},name:{description:"Name of the identity",table:{type:{summary:"string"}}},walletAddress:{description:"Wallet address of the identity",table:{type:{summary:"string"}}},stats:{description:"Statistics related to the identity",table:{type:{summary:"{ numberOfFollowers: number, numberOfFollowing?: number, points?: number }"}}},ipfsLink:{description:"Link related IPFS document",table:{type:{summary:"string"},defaultValue:{summary:"undefined"}}},externalLink:{description:"Link related to the entity (optional)",table:{type:{summary:"string"},defaultValue:{summary:"undefined"}}},bio:{description:"Bio or description of the identity",table:{type:{summary:"string"},defaultValue:{summary:"undefined"}}}}},e={args:{variant:"user",avatarSrc:'https://avatars.githubusercontent.com/u/94311139?s=200&v=4"',name:"John Doe",walletAddress:"0x1234567890abcdef1234567890abcdef12345678",stats:{numberOfFollowers:123,numberOfFollowing:45,points:671234},bio:"John Doe is a blockchain enthusiast. He loves to learn new things and share his knowledge with others. He is also a contributor to various open-source projects."},render:n=>t.jsx("div",{className:"w-[500px]",children:t.jsx(r,{...n,children:t.jsx(u,{variant:"secondary",size:"lg",className:"w-full",onClick:()=>window.open("https://example.com","_blank"),children:"Follow"})})})},a={args:{variant:"non-user",avatarSrc:'https://avatars.githubusercontent.com/u/94311139?s=200&v=4"',name:"Blockchain Corp",walletAddress:"0x1234567890abcdef1234567890abcdef12345678",stats:{numberOfFollowers:300},ipfsLink:"https://ipfs.io/ipfs/QmYch4WMF5p7yxjEcuZJxNa7AFR1ZeQhCRsn9xG7P3koXo",externalLink:"https://blockchaincorp.com",bio:"Blockchain Corp is a leading company in blockchain technology. Visit our website for more information about how you can benefit from our services."},render:n=>t.jsx("div",{className:"w-[500px]",children:t.jsx(r,{...n})})};var o,s,i;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    variant: 'user',
+    avatarSrc: 'https://avatars.githubusercontent.com/u/94311139?s=200&v=4"',
+    name: 'John Doe',
+    walletAddress: '0x1234567890abcdef1234567890abcdef12345678',
+    stats: {
+      numberOfFollowers: 123,
+      numberOfFollowing: 45,
+      points: 671234
+    },
+    bio: 'John Doe is a blockchain enthusiast. He loves to learn new things and share his knowledge with others. He is also a contributor to various open-source projects.'
+  },
+  render: (args: ProfileCardProps) => <div className="w-[500px]">
+      <ProfileCard {...args}>
+        <Button variant="secondary" size="lg" className="w-full" onClick={() => window.open('https://example.com', '_blank')}>
+          Follow
+        </Button>
+      </ProfileCard>
+    </div>
+}`,...(i=(s=e.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};var l,c,d;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    variant: 'non-user',
+    avatarSrc: 'https://avatars.githubusercontent.com/u/94311139?s=200&v=4"',
+    name: 'Blockchain Corp',
+    walletAddress: '0x1234567890abcdef1234567890abcdef12345678',
+    stats: {
+      numberOfFollowers: 300
+    },
+    ipfsLink: 'https://ipfs.io/ipfs/QmYch4WMF5p7yxjEcuZJxNa7AFR1ZeQhCRsn9xG7P3koXo',
+    externalLink: 'https://blockchaincorp.com',
+    bio: 'Blockchain Corp is a leading company in blockchain technology. Visit our website for more information about how you can benefit from our services.'
+  },
+  render: (args: ProfileCardProps) => <div className="w-[500px]">
+      <ProfileCard {...args} />
+    </div>
+}`,...(d=(c=a.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};const f=["BasicUsage","EntityProfile"],y=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:e,EntityProfile:a,__namedExportsOrder:f,default:p},Symbol.toStringTag,{value:"Module"}));export{e as B,a as E,y as S};
---
+++ assets/TagsListInput-BRS3TEC0.js
@@ -0,0 +1,55 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as e}from"./index-DSkyVWTJ.js";import{M as m,T as g,C as o,d as s}from"./index-DAK4h6dq.js";import{S as d,T as r,E as p,a as c}from"./TagsListInput.stories-BbO5GUuF.js";import"./index-CTjT7uj6.js";import"./iframe-D6PpZ4_p.js";import"../sb-preview/runtime.js";import"./index-DP8FJ8Qs.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./TransactionStatusIndicator-BWnpvmiC.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Icon.types-xxOQRm7p.js";import"./Text-COZqca-9.js";import"./Label-CLv4BT0x.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-BEma7yvq.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Textarea-CSqXU4Tb.js";import"./ValueDisplay-BLpqF4VW.js";function n(i){const a={h3:"h3",h4:"h4",...e(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{of:d}),`
+`,t.jsx(g,{}),`
+`,t.jsx(a.h4,{id:"displays-list-of-tags-or-trust-circles-with-the-ability-to-add-more",children:"Displays list of Tags or Trust circles with the ability to add more"}),`
+`,t.jsx(o,{of:r,sourceState:"none"}),`
+`,t.jsx(a.h3,{id:"empty-state-variant",children:"Empty State Variant"}),`
+`,t.jsx(o,{of:p}),`
+`,t.jsx(s,{dark:!0,language:"tsx",code:`
+import { TagsListInput } from '@0xintuition/1ui'
+
+<TagsListInput
+  variant="trustCircle"
+  tags={[]}
+  maxTags={5}
+  onAddTag={() => console.log('Add Tag')}
+  onRemoveTag={(id: string) => console.log('Remove Tag', id)}
+/>
+`}),`
+`,t.jsx(a.h3,{id:"trust-circles-variant",children:"Trust Circles Variant"}),`
+`,t.jsx(o,{of:r}),`
+`,t.jsx(s,{dark:!0,language:"tsx",code:`
+import { TagsListInput } from '@0xintuition/1ui'
+
+const trustCirclesTags = [
+{ name: 'Tag Name 1', id: '1' },
+{ name: 'Tag Name 2', id: '2' },
+{ name: 'Tag Name 3', id: '3' },
+];
+
+<TagsListInput
+  variant="trustCircle"
+  tags={trustCirclesTags}
+  maxTags={5}
+  onAddTag={() => console.log('Add Tag')}
+  onRemoveTag={(id: string) => console.log('Remove Tag', id)}
+/>
+`}),`
+`,t.jsx(a.h3,{id:"tags-variant",children:"Tags Variant"}),`
+`,t.jsx(o,{of:c}),`
+`,t.jsx(s,{dark:!0,language:"tsx",code:`
+import { TagsListInput } from '@0xintuition/1ui'
+
+const tags = [
+{ name: 'Tag Name 1', id: '1' },
+{ name: 'Tag Name 2', id: '2' },
+{ name: 'Tag Name 3', id: '3' },
+];
+
+<TagsListInput
+  variant="tag"
+  tags={tags}
+  maxTags={5}
+  onAddTag={() => console.log('Add Tag')}
+  onRemoveTag={(id: string) => console.log('Remove Tag', id)}
+/>
+`})]})}function H(i={}){const{wrapper:a}={...e(),...i.components};return a?t.jsx(a,{...i,children:t.jsx(n,{...i})}):n(i)}export{H as default};
