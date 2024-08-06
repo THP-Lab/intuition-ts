58ee29aa6248e0ece1b9bd963f350cd72c5a4785
Vitalsine85 <rob@creative-element.net>
2024-08-06 04:50:54 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@ae7bc5ed3c0bc90cc8894b7bf5e08cc912fa2b6f 🚀

---
+++ assets/Toaster-91H0EML3.js
@@ -0,0 +1,59 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as e}from"./index-DSkyVWTJ.js";import{M as i,T as c,d as r,C as n,e as m}from"./index-BS8QQvAK.js";import{S as u,B as p,I as x,a as d,E as h}from"./Toaster.stories-CI0qRUEP.js";import"./index-CTjT7uj6.js";import"./iframe-6pxX8DgC.js";import"../sb-preview/runtime.js";import"./index-BJDhk0Ya.js";import"./index-BI02G6dj.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./TransactionStatusIndicator-BzWo86JT.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Icon.types-xxOQRm7p.js";import"./Text-COZqca-9.js";import"./Label-CLv4BT0x.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-BEma7yvq.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Textarea-CSqXU4Tb.js";import"./ValueDisplay-BLpqF4VW.js";function a(s){const o={h2:"h2",h3:"h3",h4:"h4",...e(),...s.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:u}),`
+`,t.jsx(c,{}),`
+`,t.jsx(o.h4,{id:"a-toaster-component-with-a-toast-utility",children:"A toaster component with a toast utility."}),`
+`,t.jsx(r,{dark:!0,language:"tsx",code:`
+import { Toaster, toast } from '@0xintuition/1ui'
+
+<>
+  <Toaster />
+  <Button size="lg" onClick={() => toast('I am a toast!')}>
+    Launch toast
+  </Button>
+</>
+`}),`
+`,t.jsx(n,{of:p,sourceState:"none"}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(m,{}),`
+`,t.jsx(o.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(o.h3,{id:"info",children:"Info"}),`
+`,t.jsx(r,{dark:!0,language:"tsx",code:`
+<>
+  <Toaster />
+  <Button
+    size="lg"
+    variant="accent"
+    onClick={() => toast.info('I am an info toast!')}
+  >
+    Launch toast
+  </Button>
+</>
+`}),`
+`,t.jsx(n,{of:x,sourceState:"none"}),`
+`,t.jsx(o.h3,{id:"success",children:"Success"}),`
+`,t.jsx(r,{dark:!0,language:"tsx",code:`
+<>
+  <Toaster />
+  <Button
+    size="lg"
+    variant="success"
+    onClick={() => toast.success('I am a success toast!')}
+  >
+    Launch toast
+  </Button>
+</>
+`}),`
+`,t.jsx(n,{of:d,sourceState:"none"}),`
+`,t.jsx(o.h3,{id:"error",children:"Error"}),`
+`,t.jsx(r,{dark:!0,language:"tsx",code:`
+<>
+  <Toaster />
+  <Button
+    size="lg"
+    variant="destructive"
+    onClick={() => toast.error('I am an error toast!')}
+  >
+    Launch toast
+  </Button>
+</>
+`}),`
+`,t.jsx(n,{of:h,sourceState:"none"})]})}function J(s={}){const{wrapper:o}={...e(),...s.components};return o?t.jsx(o,{...s,children:t.jsx(a,{...s})}):a(s)}export{J as default};
