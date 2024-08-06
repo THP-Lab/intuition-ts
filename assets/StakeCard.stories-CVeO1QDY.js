7f0550106c130723f4689abee7418e05039460fa
Vitalsine85 <rob@creative-element.net>
2024-08-06 06:55:54 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@8a60456475771bee2b7dd0b6ea033d4a6c3fba7f 🚀

---
+++ assets/StakeCard.stories-CVeO1QDY.js
@@ -0,0 +1,9 @@
+import{j as r}from"./jsx-runtime-Cw0GR0a5.js";import{aQ as n}from"./TransactionStatusIndicator-CXZM9Unb.js";const l={title:"Components/StakeCard",component:n},e={args:{tvl:4.928,holders:69},render:t=>r.jsx("div",{className:"w-[250px]",children:r.jsx(n,{...t,onBuyClick:()=>null,onViewAllClick:()=>null})})};var s,a,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    tvl: 4.928,
+    holders: 69
+  },
+  render: args => <div className="w-[250px]">
+      <StakeCard {...args} onBuyClick={() => null} onViewAllClick={() => null} />
+    </div>
+}`,...(o=(a=e.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const c=["BasicUsage"],m=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:e,__namedExportsOrder:c,default:l},Symbol.toStringTag,{value:"Module"}));export{e as B,m as S};
