414a7111e283fcc38e55f1084aeb0195392725ea
Vitalsine85 <rob@creative-element.net>
2024-08-06 14:02:09 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@4c9d0402612a188bedbf53f04a7b6310aeb224e5 🚀

---
+++ assets/InfoCard-BI7nh1AE.js
@@ -0,0 +1,16 @@
+import{j as r}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as m}from"./index-DSkyVWTJ.js";import{M as e,T as n,d as a,C as p,e as s}from"./index-DpMacQJZ.js";import{S as c,B as u}from"./InfoCard.stories-DjAB2pjb.js";import"./index-CTjT7uj6.js";import"./iframe-B1NX9wS5.js";import"../sb-preview/runtime.js";import"./index-C5rLi0fd.js";import"./index-CjlJelaC.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./TransactionStatusIndicator-DjLmF9qZ.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Icon.types-xxOQRm7p.js";import"./Text-COZqca-9.js";import"./Label-BcH9Dz7N.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-CECijyNn.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Textarea-CSqXU4Tb.js";import"./ValueDisplay-BLpqF4VW.js";function i(t){const o={h2:"h2",h4:"h4",...m(),...t.components};return r.jsxs(r.Fragment,{children:[r.jsx(e,{of:c}),`
+`,r.jsx(n,{}),`
+`,r.jsx(o.h4,{id:"claim-creator-info-card",children:"Claim creator info card."}),`
+`,r.jsx(a,{dark:!0,language:"tsx",code:`
+import { InfoCard } from '@0xintuition/1ui'
+
+<InfoCard
+  variant="user"
+  username="super dave"
+  avatarImgSrc="image.jpg"
+  timestamp="2024-05-10T00:00:00Z"
+/>
+`}),`
+`,r.jsx(p,{of:u,sourceState:"none"}),`
+`,r.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,r.jsx(s,{})]})}function A(t={}){const{wrapper:o}={...m(),...t.components};return o?r.jsx(o,{...t,children:r.jsx(i,{...t})}):i(t)}export{A as default};
---
+++ assets/Switch-DZ1OD30u.js
@@ -0,0 +1,13 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as i}from"./index-DSkyVWTJ.js";import{M as n,T as m,d as a,C as p}from"./index-DpMacQJZ.js";import{B as s}from"./TransactionStatusIndicator-DjLmF9qZ.js";import"./Icon.types-xxOQRm7p.js";import"./Label-BcH9Dz7N.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-CECijyNn.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Text-COZqca-9.js";import"./Textarea-CSqXU4Tb.js";import"./ValueDisplay-BLpqF4VW.js";import{S as c,B as d}from"./Switch.stories-BCuEEQpn.js";import"./index-CTjT7uj6.js";import"./iframe-B1NX9wS5.js";import"../sb-preview/runtime.js";import"./index-C5rLi0fd.js";import"./index-CjlJelaC.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function r(o){const e={h4:"h4",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:c}),`
+`,t.jsx(m,{}),`
+`,t.jsx(e.h4,{id:"a-control-that-allows-the-user-to-toggle-between-checked-and-not-checked",children:"A control that allows the user to toggle between checked and not checked."}),`
+`,t.jsx(s,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/switch#api-reference","_blank"),children:"API Reference"}),`
+`,t.jsx(a,{dark:!0,language:"tsx",code:`
+import { Switch } from '@0xintuition/1ui'
+
+<div>
+  <Switch id="airplane-mode" />
+  <Label htmlFor="airplane-mode">Airplane Mode</Label>
+</div>
+`}),`
+`,t.jsx(p,{of:d,sourceState:"none"})]})}function G(o={}){const{wrapper:e}={...i(),...o.components};return e?t.jsx(e,{...o,children:t.jsx(r,{...o})}):r(o)}export{G as default};
