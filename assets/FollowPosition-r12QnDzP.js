cebbd31e92c846ac781c6f368298f0316645b9d7
jonathanprozzi <jonathanprozzi@gmail.com>
2024-08-08 03:58:12 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@6e62c2a49ec1197b88500ffaef979ac9dd962830 🚀

---
+++ assets/FollowPosition-r12QnDzP.js
@@ -0,0 +1,19 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as s}from"./index-DSkyVWTJ.js";import{M as n,T as e,d as m,C as p,e as a}from"./index-B1dWkb-U.js";import"./TransactionStatusIndicator-DyrgFxDP.js";import"./Checkbox-hjarc4kX.js";import"./Icon.types-D9mbN25E.js";import"./Label-BcH9Dz7N.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-CECijyNn.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Text-COZqca-9.js";import"./Textarea-CSqXU4Tb.js";import"./ValueDisplay-BLpqF4VW.js";import{S as l,B as c}from"./FollowPosition.stories-AUOJKX0E.js";import"./index-CTjT7uj6.js";import"./iframe-cLLZLZkh.js";import"../sb-preview/runtime.js";import"./index-C5rLi0fd.js";import"./index-CjlJelaC.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function r(t){const i={h2:"h2",h4:"h4",...s(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{of:l}),`
+`,o.jsx(e,{}),`
+`,o.jsx(i.h4,{id:"a-follow-position-component-is-used-to-display-a-position-or-a-list-of-positions-relating-to-follows",children:"A Follow Position component is used to display a position or a list of positions relating to follows"}),`
+`,o.jsx(m,{dark:!0,language:"tsx",code:`
+import { FollowPosition } from '@0xintuition/1ui'
+
+<FollowPosition
+  position="claimFor"
+  avatarSrc="https://avatars.githubusercontent.com/u/94311139?s=200&v=4"
+  name="John Doe"
+  walletAddress="0x1234567890abcdef1234567890abcdef12345678"
+  amount={1.21}
+  feesAccrued={0.005}
+  updatedAt="2021-10-01T16:00:00Z"
+/>
+`}),`
+`,o.jsx(p,{of:c,sourceState:"none"}),`
+`,o.jsx(i.h2,{id:"properties",children:"Properties"}),`
+`,o.jsx(a,{})]})}function G(t={}){const{wrapper:i}={...s(),...t.components};return i?o.jsx(i,{...t,children:o.jsx(r,{...t})}):r(t)}export{G as default};
