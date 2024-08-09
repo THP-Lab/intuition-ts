359d6297ac08c407855235a8e91cd6244e676cb6
Vitalsine85 <rob@creative-element.net>
2024-08-09 20:02:20 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@dce3d03ac7c446f5e42f7e64f7552cb3f06364c0 🚀

---
+++ assets/ClaimRow-C4Zgun3Q.js
@@ -0,0 +1,21 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as e}from"./index-DSkyVWTJ.js";import{M as m,T as n,d as a,C as s,e as p}from"./index-C_HhO6YI.js";import{S as l,B as c}from"./ClaimRow.stories-1C5Wvbv9.js";import"./index-CTjT7uj6.js";import"./iframe-CkyTXqKN.js";import"../sb-preview/runtime.js";import"./index-C5rLi0fd.js";import"./index-CjlJelaC.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./TransactionStatusIndicator-CND3FGVI.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Checkbox-BUqe9RSH.js";import"./Icon.types-C1sI-gES.js";import"./Text-COZqca-9.js";import"./Label-BcH9Dz7N.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-CECijyNn.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";function r(o){const i={h2:"h2",h4:"h4",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{of:l}),`
+`,t.jsx(n,{}),`
+`,t.jsx(i.h4,{id:"a-representation-of-claim-details",children:"A representation of claim details."}),`
+`,t.jsx(a,{dark:!0,language:"tsx",code:`
+import { ClaimRow, Claim } from '@0xintuition/1ui'
+
+<ClaimRow claimsFor={736} claimsAgainst={234} amount={0.383}>
+  <Claim
+    size="sm"
+    subject={{
+      variant: 'default',
+      label: '0xintuition',
+    }}
+    predicate={{ label: 'is really' }}
+    object={{ label: 'cool' }}
+  />
+</ClaimRow>
+`}),`
+`,t.jsx(s,{of:c,sourceState:"none"}),`
+`,t.jsx(i.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(p,{})]})}function I(o={}){const{wrapper:i}={...e(),...o.components};return i?t.jsx(i,{...o,children:t.jsx(r,{...o})}):r(o)}export{I as default};
