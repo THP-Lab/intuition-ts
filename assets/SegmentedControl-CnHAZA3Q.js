05d784426a1cc3b3106b8f78fadebd77d2808782
alexander-mann <alexandercmann@gmail.com>
2024-08-09 16:08:00 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@a17f34254e7fbe86f2069743c0e1cc98233536ab 🚀

---
+++ assets/SegmentedControl-CnHAZA3Q.js
@@ -0,0 +1,32 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as n}from"./index-DSkyVWTJ.js";import{M as r,T as m,d as s,C as a}from"./index-E7duvW0b.js";import"./TransactionStatusIndicator-DZFNFObF.js";import"./Checkbox-BUqe9RSH.js";import"./Icon.types-C1sI-gES.js";import"./Label-BcH9Dz7N.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-CECijyNn.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Text-COZqca-9.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";import{S as p,B as l}from"./SegmentedControl.stories-BTa4XpLF.js";import"./index-CTjT7uj6.js";import"./iframe-DhUSPYtG.js";import"../sb-preview/runtime.js";import"./index-C5rLi0fd.js";import"./index-CjlJelaC.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function i(e){const o={h4:"h4",...n(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:p}),`
+`,t.jsx(m,{}),`
+`,t.jsx(o.h4,{id:"used-to-switch-between-views-within-a-page",children:"Used to switch between views within a page."}),`
+`,t.jsx(s,{dark:!0,language:"tsx",code:`
+import React from 'react'
+
+import { SegmentedControl } from '@0xintuition/1ui'
+
+const options = [
+{ value: 'overview', label: 'Overview' },
+{ value: 'claims', label: 'Claims' },
+{ value: 'positions', label: 'Positions' },
+{ value: 'activity', label: 'Activity' },
+]
+
+const [selectedTab, setSelectedTab] = React.useState(options[0].value)
+
+return (
+
+<SegmentedControl>
+  {options.map((option, index) => (
+    <SegmentedControlItem
+      key={index}
+      isActive={selectedTab === option.value}
+      onClick={() => setSelectedTab(option.value)}
+    >
+      {option.label}
+    </SegmentedControlItem>
+  ))}
+</SegmentedControl>
+) `}),`
+`,t.jsx(a,{of:l,sourceState:"none"})]})}function z(e={}){const{wrapper:o}={...n(),...e.components};return o?t.jsx(o,{...e,children:t.jsx(i,{...e})}):i(e)}export{z as default};
