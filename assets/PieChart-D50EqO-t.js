5bb8649ec8389674ebacaa21444f626762cd7d40
jonathanprozzi <jonathanprozzi@gmail.com>
2024-08-09 03:00:11 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@3e66c8618fab49aa51cd61200c237b2481c8b691 🚀

---
+++ assets/PieChart-D50EqO-t.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as n}from"./index-DSkyVWTJ.js";import{M as i,T as s,d as a,C as m,e as p}from"./index-BMggS-pw.js";import{S as c,B as h}from"./PieChart.stories-BelHIgHA.js";import"./index-CTjT7uj6.js";import"./iframe-_CZlN9o7.js";import"../sb-preview/runtime.js";import"./index-C5rLi0fd.js";import"./index-CjlJelaC.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./PieChart-B_dtFskq.js";function r(e){const o={h2:"h2",h4:"h4",...n(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:c}),`
+`,t.jsx(s,{}),`
+`,t.jsx(o.h4,{id:"a-pie-chart-to-show-percentage-filled",children:"A pie chart to show percentage filled."}),`
+`,t.jsx(a,{dark:!0,language:"tsx",code:`
+import { PieChart } from '@0xintuition/1ui'
+
+<PieChart percentage={50} />
+`}),`
+`,t.jsx(m,{of:h,sourceState:"none"}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(p,{})]})}function B(e={}){const{wrapper:o}={...n(),...e.components};return o?t.jsx(o,{...e,children:t.jsx(r,{...e})}):r(e)}export{B as default};
