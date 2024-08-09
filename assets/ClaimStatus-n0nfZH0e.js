a0fe74efae1b4b58005c87373ba84405b4d9e64c
Vitalsine85 <rob@creative-element.net>
2024-08-09 20:58:23 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@6248cebef89eb3b9f509bc6dcb64d068a341d4ba 🚀

---
+++ assets/ClaimStatus-n0nfZH0e.js
@@ -0,0 +1,18 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as r}from"./index-DSkyVWTJ.js";import{M as s,T as a,d as n,C as e}from"./index-EEmHqJKe.js";import"./TransactionStatusIndicator-Bc2tHjYS.js";import"./Checkbox-BUqe9RSH.js";import"./Icon.types-C1sI-gES.js";import"./Label-BcH9Dz7N.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-CECijyNn.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Text-COZqca-9.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";import{S as p,B as l}from"./ClaimStatus.stories-Bm9XqVxK.js";import"./index-CTjT7uj6.js";import"./iframe-Drtey7_E.js";import"../sb-preview/runtime.js";import"./index-C5rLi0fd.js";import"./index-CjlJelaC.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function m(o){const i={h2:"h2",...r(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:p}),`
+`,t.jsx(a,{}),`
+`,t.jsx(i.h2,{id:"a-component-showing-the-status-of-the-positions-on-a-claim",children:"A component showing the status of the positions on a Claim"}),`
+`,t.jsx(n,{dark:!0,language:"tsx",code:`
+import { ClaimStatus, Claim } from './ClaimStatus'
+
+<ClaimStatus claimsFor={10} claimsAgainst={5}>
+  <Claim
+    size="sm"
+    subject={{label: '0xintuition' }}
+    predicate={{ label: 'is really' }}
+    object={{ label: 'cool' }}
+
+/>
+
+</ClaimStatus>
+`}),`
+`,t.jsx(e,{of:l,sourceState:"none"})]})}function I(o={}){const{wrapper:i}={...r(),...o.components};return i?t.jsx(i,{...o,children:t.jsx(m,{...o})}):m(o)}export{I as default};
