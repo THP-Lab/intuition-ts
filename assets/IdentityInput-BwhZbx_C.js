16177844eed34d74bf0931911869684edaa39e2d
Vitalsine85 <rob@creative-element.net>
2024-08-10 16:34:15 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@2b1b320e84d7b54c7e1762d913de827923170402 🚀

---
+++ assets/IdentityInput-BwhZbx_C.js
@@ -0,0 +1,47 @@
+import{j as e}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as a}from"./index-DSkyVWTJ.js";import{M as s,T as m,d as i,C as n,e as c}from"./index-TZpUzYtb.js";import{S as l,B as p,O as d}from"./IdentityInput.stories-Ce0hJxBD.js";import"./index-CTjT7uj6.js";import"./iframe-DCJQvOO7.js";import"../sb-preview/runtime.js";import"./index-C5rLi0fd.js";import"./index-CjlJelaC.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./TransactionStatusIndicator-DxtPX5Kh.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Checkbox-DUkdGkqO.js";import"./Icon.types-7PkJ3kFX.js";import"./Text-COZqca-9.js";import"./Label-BcH9Dz7N.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-CECijyNn.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";function r(o){const t={h2:"h2",h3:"h3",h4:"h4",...a(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:l}),`
+`,e.jsx(m,{}),`
+`,e.jsx(t.h4,{id:"allows-users-to-create-a-claim",children:"Allows users to create a claim."}),`
+`,e.jsx(i,{dark:!0,language:"tsx",code:`
+import { IdentityInput } from '@0xintuition/1ui'
+
+<IdentityInput
+  showLabels
+  subject={{
+    selectedValue: { name: 'Super Dave' },
+    onClick: () => doSomething,
+  }}
+  predicate={{
+    selectedValue: { name: 'is a', variant: 'non-user' },
+    onClick: () => doSomething,
+  }}
+  object={{
+    selectedValue: { name: 'daredevil', variant: 'non-user' },
+    onClick: () => doSomething,
+  }}
+/>
+`}),`
+`,e.jsx(n,{of:p,sourceState:"none"}),`
+`,e.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(c,{}),`
+`,e.jsx(t.h2,{id:"variants",children:"Variants"}),`
+`,e.jsx(t.h3,{id:"other",children:"Other"}),`
+`,e.jsx(i,{dark:!0,language:"tsx",code:`
+<IdentityInput
+  subject={{
+    placeholder: 'Select an identity',
+    selectedValue: {},
+    onClick: () => doSomething,
+  }}
+  predicate={{
+    placeholder: 'Select an identity',
+    selectedValue: {},
+    onClick: () => doSomething,
+  }}
+  object={{
+    placeholder: 'Select an identity',
+    selectedValue: {},
+    onClick: () => doSomething,
+  }}
+/>
+`}),`
+`,e.jsx(n,{of:d,sourceState:"none"})]})}function G(o={}){const{wrapper:t}={...a(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(r,{...o})}):r(o)}export{G as default};
