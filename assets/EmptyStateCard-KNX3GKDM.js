ed6fe98fbbb0c1f205364a0c9adfd211373e3539
Vitalsine85 <rob@creative-element.net>
2024-08-12 16:05:29 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@c1c4aa528004fbbc8425be98621b76dfd40679e1 🚀

---
+++ assets/EmptyStateCard-KNX3GKDM.js
@@ -0,0 +1,21 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as s}from"./index-DSkyVWTJ.js";import{M as a,T as m,C as i,d as e,e as p}from"./index-BFJAe364.js";import{S as d,B as h,W as u}from"./EmptyStateCard.stories-Bx5CcTNr.js";import"./index-CTjT7uj6.js";import"./iframe-BOvHnaCl.js";import"../sb-preview/runtime.js";import"./index-C5rLi0fd.js";import"./index-CjlJelaC.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./TransactionStatusIndicator-DYdHX477.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Checkbox-DUkdGkqO.js";import"./Icon.types-7PkJ3kFX.js";import"./Text-COZqca-9.js";import"./Label-BcH9Dz7N.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-CECijyNn.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";function n(r){const o={h2:"h2",h3:"h3",h4:"h4",...s(),...r.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{of:d}),`
+`,t.jsx(m,{}),`
+`,t.jsx(o.h4,{id:"a-display-card-to-show-an-empty-state",children:"A display card to show an empty state"}),`
+`,t.jsx(i,{of:h,sourceState:"none"}),`
+`,t.jsx(e,{dark:!0,language:"tsx",code:`
+import { EmptyStateCard } from '@0xintuition/1ui'
+
+<EmptyStateCard message="You have no stake positions." />
+`}),`
+`,t.jsx(o.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(o.h3,{id:"with-children",children:"With Children"}),`
+`,t.jsx(i,{of:u,sourceState:"none"}),`
+`,t.jsx(e,{dark:!0,language:"tsx",code:`
+import { EmptyStateCard, Button } from '@0xintuition/1ui'
+
+<EmptyStateCard message="You have no stake positions.">
+  <Button>Stake Now</Button>
+</EmptyStateCard>
+`}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(p,{})]})}function z(r={}){const{wrapper:o}={...s(),...r.components};return o?t.jsx(o,{...r,children:t.jsx(n,{...r})}):n(r)}export{z as default};
