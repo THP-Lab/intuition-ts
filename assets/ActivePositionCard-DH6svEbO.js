aabd33efe12b7b438eff17ff0151c949419556dc
Vitalsine85 <rob@creative-element.net>
2024-08-13 02:38:33 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@541a1b82fe49271cfc42e5ecaa318b7f48029dfb 🚀

---
+++ assets/ActivePositionCard-DH6svEbO.js
@@ -0,0 +1,22 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as a}from"./index-DSkyVWTJ.js";import{M as e,T as m,d as r,C as s,e as p}from"./index-Dhh-j9Bn.js";import{S as c,B as d,F as u,A as x}from"./ActivePositionCard.stories-DKjA17Kz.js";import"./index-CTjT7uj6.js";import"./iframe-CdafzUhA.js";import"../sb-preview/runtime.js";import"./index-C5rLi0fd.js";import"./index-CjlJelaC.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./TransactionStatusIndicator-YjNYn45K.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Checkbox-DUkdGkqO.js";import"./Icon.types-7PkJ3kFX.js";import"./Text-COZqca-9.js";import"./Label-BcH9Dz7N.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-CECijyNn.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";function n(i){const o={h2:"h2",h3:"h3",h4:"h4",...a(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:c}),`
+`,t.jsx(m,{}),`
+`,t.jsx(o.h4,{id:"a-card-that-displays-a-users-active-position",children:"A card that displays a user's active position."}),`
+`,t.jsx(r,{dark:!0,language:"tsx",code:`
+import { ActivePositionCard } from '@0xintuition/1ui'
+
+<ActivePositionCard value={0.567} />
+`}),`
+`,t.jsx(s,{of:d,sourceState:"none"}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(p,{}),`
+`,t.jsx(o.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(o.h3,{id:"for",children:"For"}),`
+`,t.jsx(r,{dark:!0,language:"tsx",code:`
+<ActivePositionCard value={0.567} claimPosition="claimFor" />
+`}),`
+`,t.jsx(s,{of:u,sourceState:"none"}),`
+`,t.jsx(o.h3,{id:"against",children:"Against"}),`
+`,t.jsx(r,{dark:!0,language:"tsx",code:`
+<ActivePositionCard value={0.567} claimPosition="claimAgainst" />
+`}),`
+`,t.jsx(s,{of:x,sourceState:"none"})]})}function J(i={}){const{wrapper:o}={...a(),...i.components};return o?t.jsx(o,{...i,children:t.jsx(n,{...i})}):n(i)}export{J as default};
