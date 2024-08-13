d294c9e9bdc69bcd3c373925d5c3ed5a2ff2d1d3
Vitalsine85 <rob@creative-element.net>
2024-08-13 01:19:38 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@15dc696f75b8dd85f8a949197dfce6f2133ae145 🚀

---
+++ assets/ErrorStateCard-BbYaV_n4.js
@@ -0,0 +1,21 @@
+import{j as r}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as s}from"./index-DSkyVWTJ.js";import{M as a,T as m,C as i,d as e,e as p}from"./index-D2Dc-kv1.js";import{S as d,B as c,W as h}from"./ErrorStateCard.stories-B5_F78oN.js";import"./index-CTjT7uj6.js";import"./iframe-CZyScso9.js";import"../sb-preview/runtime.js";import"./index-C5rLi0fd.js";import"./index-CjlJelaC.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./TransactionStatusIndicator-D4KkkdA5.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Checkbox-DUkdGkqO.js";import"./Icon.types-7PkJ3kFX.js";import"./Text-COZqca-9.js";import"./Label-BcH9Dz7N.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-CECijyNn.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";function n(o){const t={h2:"h2",h3:"h3",h4:"h4",...s(),...o.components};return r.jsxs(r.Fragment,{children:[r.jsx(a,{of:d}),`
+`,r.jsx(m,{}),`
+`,r.jsx(t.h4,{id:"a-display-card-to-show-an-error-state",children:"A display card to show an error state"}),`
+`,r.jsx(i,{of:c,sourceState:"none"}),`
+`,r.jsx(e,{dark:!0,language:"tsx",code:`
+import { ErrorStateCard } from '@0xintuition/1ui'
+
+<ErrorStateCard message="An error occured" />
+`}),`
+`,r.jsx(t.h2,{id:"variants",children:"Variants"}),`
+`,r.jsx(t.h3,{id:"with-children",children:"With Children"}),`
+`,r.jsx(i,{of:h,sourceState:"none"}),`
+`,r.jsx(e,{dark:!0,language:"tsx",code:`
+import { ErrorStateCard, Button } from '@0xintuition/1ui'
+
+<ErrorStateCard message="An error occured">
+  <Button>Retry</Button>
+</ErrorStateCard>
+`}),`
+`,r.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,r.jsx(p,{})]})}function H(o={}){const{wrapper:t}={...s(),...o.components};return t?r.jsx(t,{...o,children:r.jsx(n,{...o})}):n(o)}export{H as default};
---
+++ assets/TransactionStatusCard-D7QfWdaP.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as n}from"./index-DSkyVWTJ.js";import{M as a,T as s,d as m,C as p,e}from"./index-D2Dc-kv1.js";import{S as c,B as u}from"./TransactionStatusCard.stories--DIS58ze.js";import"./index-CTjT7uj6.js";import"./iframe-CZyScso9.js";import"../sb-preview/runtime.js";import"./index-C5rLi0fd.js";import"./index-CjlJelaC.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./TransactionStatusIndicator-D4KkkdA5.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Checkbox-DUkdGkqO.js";import"./Icon.types-7PkJ3kFX.js";import"./Text-COZqca-9.js";import"./Label-BcH9Dz7N.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-CECijyNn.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";function i(o){const r={h2:"h2",h4:"h4",...n(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{of:c}),`
+`,t.jsx(s,{}),`
+`,t.jsx(r.h4,{id:"displays-a-card-with-an-icon-and-label-per-transaction-status",children:"Displays a card with an icon and label per transaction status."}),`
+`,t.jsx(m,{dark:!0,language:"tsx",code:`
+import { TransactionStatusCard } from '@0xintuition/1ui'
+
+<TransactionStatusCard status="awaiting" />
+`}),`
+`,t.jsx(p,{of:u,sourceState:"none"}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(e,{})]})}function I(o={}){const{wrapper:r}={...n(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{I as default};
