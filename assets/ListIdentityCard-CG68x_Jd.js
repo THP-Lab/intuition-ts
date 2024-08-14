7254f6a7569a0ef2f10113ee3e0e514a91e51ab2
Vitalsine85 <rob@creative-element.net>
2024-08-14 20:15:48 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@b73c5dfc10dffe8065e273e401ef84b944c62165 🚀

---
+++ assets/ListIdentityCard-CG68x_Jd.js
@@ -0,0 +1,18 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as e}from"./index-DSkyVWTJ.js";import{M as n,T as m,d as s,C as p,e as a}from"./index-gNXEwJ-l.js";import{S as c,B as d}from"./ListIdentityCard.stories-YuRsG5fr.js";import"./index-CTjT7uj6.js";import"./iframe-DF_uLAqP.js";import"../sb-preview/runtime.js";import"./index-C5rLi0fd.js";import"./index-CjlJelaC.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./TransactionStatusIndicator-BrImopvy.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Checkbox-DUkdGkqO.js";import"./Icon.types-7PkJ3kFX.js";import"./Text-COZqca-9.js";import"./Label-BcH9Dz7N.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-CECijyNn.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";import"./Banner-BQ9cebq8.js";function i(o){const r={h2:"h2",h4:"h4",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:c}),`
+`,t.jsx(m,{}),`
+`,t.jsx(r.h4,{id:"a-card-component-to-display-identity-information",children:"A card component to display identity information"}),`
+`,t.jsx(s,{dark:!0,language:"tsx",code:`
+import { ListIdentityCard } from '@0xintuition/1ui'
+
+<ListIdentityCard
+  displayName="Best Crypto Portfolio Trackers"
+  imgSrc="https://avatars.githubusercontent.com/u/94311139?s=200&v=4"
+  identitiesCount={45}
+  savedAmount="0.047"
+  onSaveClick={() => alert('Best Crypto Portfolio Trackers saved!')}
+  isSaved={true}
+/>
+`}),`
+`,t.jsx(p,{of:d,sourceState:"none"}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(a,{})]})}function H(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{H as default};
---
+++ assets/ValueDisplay-C-rb-CHF.js
@@ -0,0 +1,32 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as n}from"./index-DSkyVWTJ.js";import{M as s,T as p,d as i,C as e,e as m}from"./index-gNXEwJ-l.js";import"./TransactionStatusIndicator-BrImopvy.js";import"./Checkbox-DUkdGkqO.js";import"./Icon.types-7PkJ3kFX.js";import"./Label-BcH9Dz7N.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-CECijyNn.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Text-COZqca-9.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";import"./Banner-BQ9cebq8.js";import{S as l,B as u,D as c,a as d,A as x,b as j}from"./ValueDisplay.stories-x5_CNlXG.js";import"./index-CTjT7uj6.js";import"./iframe-DF_uLAqP.js";import"../sb-preview/runtime.js";import"./index-C5rLi0fd.js";import"./index-CjlJelaC.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function a(o){const r={h2:"h2",h3:"h3",h4:"h4",...n(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:l}),`
+`,t.jsx(p,{}),`
+`,t.jsx(r.h4,{id:"a-representation-of-staking-values",children:"A representation of staking values."}),`
+`,t.jsx(i,{dark:!0,language:"tsx",code:`
+import { ValueDisplay } from '@0xintuition/1ui'
+
+<ValueDisplay>-</ValueDisplay>
+`}),`
+`,t.jsx(e,{of:u,sourceState:"none"}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(m,{}),`
+`,t.jsx(r.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(r.h3,{id:"direction-for",children:"Direction For"}),`
+`,t.jsx(i,{dark:!0,language:"tsx",code:`
+<ValueDisplay variant="directionFor">0.003</ValueDisplay>
+`}),`
+`,t.jsx(e,{of:c,sourceState:"none"}),`
+`,t.jsx(r.h3,{id:"direction-against",children:"Direction Against"}),`
+`,t.jsx(i,{dark:!0,language:"tsx",code:`
+<ValueDisplay variant="directionAgainst">12345</ValueDisplay>
+`}),`
+`,t.jsx(e,{of:d,sourceState:"none"}),`
+`,t.jsx(r.h3,{id:"attestor-for",children:"Attestor For"}),`
+`,t.jsx(i,{dark:!0,language:"tsx",code:`
+<ValueDisplay variant="attestorFor">12345</ValueDisplay>
+`}),`
+`,t.jsx(e,{of:x,sourceState:"none"}),`
+`,t.jsx(r.h3,{id:"attestor-against",children:"Attestor Against"}),`
+`,t.jsx(i,{dark:!0,language:"tsx",code:`
+<ValueDisplay variant="attestorAgainst">12345</ValueDisplay>
+`}),`
+`,t.jsx(e,{of:j,sourceState:"none"})]})}function O(o={}){const{wrapper:r}={...n(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(a,{...o})}):a(o)}export{O as default};
