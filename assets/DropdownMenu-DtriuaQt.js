9586982289f06393d696928d0c209319b29f754f
alexander-mann <alexandercmann@gmail.com>
2024-08-15 13:47:14 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@e8159677fdd92f059fd858cf3c902d6ee70f6828 🚀

---
+++ assets/DropdownMenu-DtriuaQt.js
@@ -0,0 +1,29 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as e}from"./index-DSkyVWTJ.js";import{M as i,T as p,d as m,C as u}from"./index-CVPBA-pi.js";import{B as s}from"./TransactionStatusIndicator-DF0ORTNg.js";import"./Checkbox-DUkdGkqO.js";import"./Icon.types-7PkJ3kFX.js";import"./Label-BcH9Dz7N.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-CECijyNn.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Text-COZqca-9.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";import"./Banner-BQ9cebq8.js";import{S as a,B as d}from"./DropdownMenu.stories-DRPXvJmt.js";import"./index-CTjT7uj6.js";import"./iframe-ChhbhtuU.js";import"../sb-preview/runtime.js";import"./index-C5rLi0fd.js";import"./index-CjlJelaC.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function r(n){const t={h4:"h4",...e(),...n.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{of:a}),`
+`,o.jsx(p,{}),`
+`,o.jsx(t.h4,{id:"displays-a-menu-to-the-user--such-as-a-set-of-actions-or-functions--triggered-by-a-button",children:"Displays a menu to the user — such as a set of actions or functions — triggered by a button."}),`
+`,o.jsx(s,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/dropdown-menu#api-reference","_blank"),children:"API Reference"}),`
+`,o.jsx(m,{dark:!0,language:"tsx",code:`
+import { 
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger, 
+} from '@0xintuition/1ui'
+
+<DropdownMenu>
+  <DropdownMenuTrigger>
+    <Button size="lg">Open menu</Button>
+  </DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>Profile</DropdownMenuItem>
+    <DropdownMenuItem>Billing</DropdownMenuItem>
+    <DropdownMenuItem>Team</DropdownMenuItem>
+    <DropdownMenuItem>Subscription</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+`}),`
+`,o.jsx(u,{of:d,sourceState:"none"})]})}function H(n={}){const{wrapper:t}={...e(),...n.components};return t?o.jsx(t,{...n,children:o.jsx(r,{...n})}):r(n)}export{H as default};
---
+++ assets/Select-cpDSyQzD.js
@@ -0,0 +1,10 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as e}from"./index-DSkyVWTJ.js";import{M as n,T as m,d as p,C as s}from"./index-CVPBA-pi.js";import{B as a}from"./TransactionStatusIndicator-DF0ORTNg.js";import"./Checkbox-DUkdGkqO.js";import"./Icon.types-7PkJ3kFX.js";import"./Label-BcH9Dz7N.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-CECijyNn.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Text-COZqca-9.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";import"./Banner-BQ9cebq8.js";import{S as c,B as u}from"./Select.stories-DJKSkWxg.js";import"./index-CTjT7uj6.js";import"./iframe-ChhbhtuU.js";import"../sb-preview/runtime.js";import"./index-C5rLi0fd.js";import"./index-CjlJelaC.js";import"./index-BhgyLgKK.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";function i(o){const r={h4:"h4",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:c}),`
+`,t.jsx(m,{}),`
+`,t.jsx(r.h4,{id:"displays-a-list-of-options-for-the-user-to-pick-fromtriggered-by-a-button",children:"Displays a list of options for the user to pick from—triggered by a button."}),`
+`,t.jsx(a,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/select#api-reference","_blank"),children:"API Reference"}),`
+`,t.jsx(p,{dark:!0,language:"tsx",code:`
+import { Select } from '@0xintuition/1ui'
+
+<Select />
+`}),`
+`,t.jsx(s,{of:u,sourceState:"none"})]})}function J(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{J as default};
