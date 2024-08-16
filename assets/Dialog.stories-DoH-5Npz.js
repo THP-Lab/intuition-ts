2d699e953f62b7d6a26f654566c5d6183d9083c3
jonathanprozzi <jonathanprozzi@gmail.com>
2024-08-16 02:31:59 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@414137a4fb560af0997fbebf3e3b09ad9aa67c52 🚀

---
+++ assets/Dialog.stories-DoH-5Npz.js
@@ -0,0 +1,17 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{L as a,M as s,B as l,N as g,O as m,P as p,Q as c}from"./AlertDialog-DPCOpuWM.js";import"./Checkbox-BaXYeBzr.js";import"./Icon.types-Ck8KU8EG.js";import"./Label-CkICs1_r.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-juOyYLvc.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Text-COZqca-9.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";import"./Banner-BbS_KexI.js";const d={title:"Components/Dialog",component:a},e={args:{},render:n=>o.jsxs(a,{...n,children:[o.jsx(s,{children:o.jsx(l,{size:"lg",children:"Open dialog"})}),o.jsx(g,{children:o.jsxs(m,{children:[o.jsx(p,{children:"Are you absolutely sure?"}),o.jsx(c,{children:"This action cannot be undone. This will permanently delete your account and remove your data from our servers."})]})})]})};var r,i,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {},
+  render: args => <Dialog {...args}>
+      <DialogTrigger>
+        <Button size="lg">Open dialog</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+}`,...(t=(i=e.parameters)==null?void 0:i.docs)==null?void 0:t.source}}};const u=["BasicUsage"],w=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:e,__namedExportsOrder:u,default:d},Symbol.toStringTag,{value:"Module"}));export{e as B,w as S};
---
+++ assets/SegmentedControl-BALNqkhN.js
@@ -0,0 +1,32 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as r}from"./index-DSkyVWTJ.js";import{M as n,T as m,d as s,C as a}from"./index-D0Zxh8L5.js";import"./AlertDialog-DPCOpuWM.js";import"./Checkbox-BaXYeBzr.js";import"./Icon.types-Ck8KU8EG.js";import"./Label-CkICs1_r.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-juOyYLvc.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Text-COZqca-9.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";import"./Banner-BbS_KexI.js";import{S as p,B as l}from"./SegmentedControl.stories-BTa4XpLF.js";import"./index-CTjT7uj6.js";import"./iframe-CCMa4GbX.js";import"../sb-preview/runtime.js";import"./index-BqnMPuz1.js";import"./index-DVBfyTD9.js";import"./index-9r8iugjR.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";function i(e){const o={h4:"h4",...r(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:p}),`
+`,t.jsx(m,{}),`
+`,t.jsx(o.h4,{id:"used-to-switch-between-views-within-a-page",children:"Used to switch between views within a page."}),`
+`,t.jsx(s,{dark:!0,language:"tsx",code:`
+import React from 'react'
+
+import { SegmentedControl } from '@0xintuition/1ui'
+
+const options = [
+{ value: 'overview', label: 'Overview' },
+{ value: 'claims', label: 'Claims' },
+{ value: 'positions', label: 'Positions' },
+{ value: 'activity', label: 'Activity' },
+]
+
+const [selectedTab, setSelectedTab] = React.useState(options[0].value)
+
+return (
+
+<SegmentedControl>
+  {options.map((option, index) => (
+    <SegmentedControlItem
+      key={index}
+      isActive={selectedTab === option.value}
+      onClick={() => setSelectedTab(option.value)}
+    >
+      {option.label}
+    </SegmentedControlItem>
+  ))}
+</SegmentedControl>
+) `}),`
+`,t.jsx(a,{of:l,sourceState:"none"})]})}function G(e={}){const{wrapper:o}={...r(),...e.components};return o?t.jsx(o,{...e,children:t.jsx(i,{...e})}):i(e)}export{G as default};
