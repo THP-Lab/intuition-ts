11b9e844774636b6861201f3e7747ee45c7677b7
Vitalsine85 <rob@creative-element.net>
2024-08-16 20:52:44 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@96085ed383c60a330015697680024fb607f70e4b 🚀

---
+++ assets/Copy-Bh7PV1ar.js
@@ -0,0 +1,17 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as n}from"./index-DSkyVWTJ.js";import{M as s,T as a,d as p,C as r,e as m}from"./index-DWgfcvKB.js";import{B as c}from"./AlertDialog-DKxCxLDc.js";import"./Checkbox-BaXYeBzr.js";import"./Icon.types-Ck8KU8EG.js";import"./Label-CkICs1_r.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-juOyYLvc.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Text-COZqca-9.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";import{S as d,B as x,D as h,a as l}from"./Copy.stories-B-sn4-35.js";import"./index-CTjT7uj6.js";import"./iframe-DzMeXS2K.js";import"../sb-preview/runtime.js";import"./index-BqnMPuz1.js";import"./index-DVBfyTD9.js";import"./index-9r8iugjR.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";function e(i){const o={h2:"h2",h3:"h3",h4:"h4",...n(),...i.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:d}),`
+`,t.jsx(a,{}),`
+`,t.jsx(o.h4,{id:"copies-text-that-is-fed-into-it-as-a-prop",children:"Copies text that is fed into it as a prop."}),`
+`,t.jsx(c,{variant:"accent",onClick:()=>window.open("link_goes_here","_blank"),children:"API Reference"}),`
+`,t.jsx(p,{dark:!0,language:"tsx",code:`
+import { Copy } from '@0xintuition/1ui'
+
+<Copy text="Text to copy" />
+`}),`
+`,t.jsx(r,{of:x,sourceState:"none"}),`
+`,t.jsx(o.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(m,{}),`
+`,t.jsx(o.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(o.h3,{id:"default",children:"Default"}),`
+`,t.jsx(r,{of:h,sourceState:"none"}),`
+`,t.jsx(o.h3,{id:"disabled",children:"Disabled"}),`
+`,t.jsx(r,{of:l,sourceState:"none"})]})}function K(i={}){const{wrapper:o}={...n(),...i.components};return o?t.jsx(o,{...i,children:t.jsx(e,{...i})}):e(i)}export{K as default};
--- assets/TransactionStatusIndicator.stories-DeUUiA95.js
+++
@@ -1,7 +0,0 @@
-import{j as c}from"./jsx-runtime-Cw0GR0a5.js";import{bl as n,bk as r,bm as o}from"./AlertDialog-CjgPvxu0.js";const p={title:"Components/TransactionStatus/TransactionStatusIndicator",component:n,argTypes:{status:{description:"Status of transaction",options:Object.values(r),table:{type:{summary:"string"}},control:"select"},type:{description:"Type of transaction",options:Object.values(o),table:{type:{summary:"string"}},control:"select"}}},t={args:{status:r.awaiting,type:o.identity},render:i=>c.jsx(n,{...i})};var a,s,e;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    status: TransactionStatus.awaiting,
-    type: Transaction.identity
-  },
-  render: args => <TransactionStatusIndicator {...args} />
-}`,...(e=(s=t.parameters)==null?void 0:s.docs)==null?void 0:e.source}}};const u=["BasicUsage"],l=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:t,__namedExportsOrder:u,default:p},Symbol.toStringTag,{value:"Module"}));export{t as B,l as S};
