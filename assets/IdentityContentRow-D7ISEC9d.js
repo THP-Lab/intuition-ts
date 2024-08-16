8ff07ff1e16badea679a9cd957430aafb925e585
Vitalsine85 <rob@creative-element.net>
2024-08-16 20:31:10 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@531c79ce24a2e6564291fb6ebb78d9caf78ddb16 🚀

---
+++ assets/IdentityContentRow-D7ISEC9d.js
@@ -0,0 +1,59 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as i}from"./index-DSkyVWTJ.js";import{M as s,T as m,d as n,C as r,e as l}from"./index-U1Ti8TsA.js";import"./AlertDialog-CjgPvxu0.js";import"./Checkbox-BaXYeBzr.js";import"./Icon.types-Ck8KU8EG.js";import"./Label-CkICs1_r.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-juOyYLvc.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Text-COZqca-9.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";import{S as p,U as u,E as d}from"./IdentityContentRow.stories-HV3GPzZj.js";import"./index-CTjT7uj6.js";import"./iframe-DVSgXuqv.js";import"../sb-preview/runtime.js";import"./index-BqnMPuz1.js";import"./index-DVBfyTD9.js";import"./index-9r8iugjR.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";function a(o){const e={h2:"h2",h3:"h3",h4:"h4",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:p}),`
+`,t.jsx(m,{}),`
+`,t.jsx(e.h4,{id:"an-identity-content-row-is-a-component-that-displays-the-identity-information-for-a-user-or-an-entity",children:"An Identity Content Row is a component that displays the identity information for a user or an entity"}),`
+`,t.jsx(n,{dark:!0,language:"tsx",code:`
+import { IdentityContentRow } from '@0xintuition/1ui'
+import { Text } from 'components/Text'
+
+<IdentityContentRow
+  variant="user"
+  avatarSrc="https://avatars.githubusercontent.com/u/94311139?s=200&v=4"
+  name="John Doe"
+  walletAddress="0x1234567890abcdef1234567890abcdef12345678"
+  amount={1.21}
+  totalFollowers={305}
+  tags={[
+    { label: 'keyboard', value: 34 },
+    { label: 'ergonomic', value: 56 },
+    { label: 'wireless', value: 12 },
+    { label: 'gaming', value: 77 },
+    { label: 'work', value: 11 },
+    { label: 'home', value: 34 },
+  ]}
+>
+  <Text variant="body" className="text-primary-foreground">
+    Extra Content
+  </Text>
+</IdentityContentRow>
+`}),`
+`,t.jsx(r,{of:u,sourceState:"none"}),`
+`,t.jsx(e.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(l,{}),`
+`,t.jsx(e.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(e.h3,{id:"entity",children:"Entity"}),`
+`,t.jsx(n,{dark:!0,language:"tsx",code:`
+import { IdentityContentRow } from '@0xintuition/1ui'
+import { Text } from 'components/Text'
+
+<IdentityContentRow
+  variant="non-user"
+  avatarSrc="https://avatars.githubusercontent.com/u/94311139?s=200&v=4"
+  name="Amazon"
+  walletAddress="0x1234567890abcdef1234567890abcdef12345678"
+  amount={0.321}
+  totalFollowers={123}
+  tags={[
+    { label: 'keyboard', value: 34 },
+    { label: 'ergonomic', value: 56 },
+    { label: 'wireless', value: 12 },
+    { label: 'gaming', value: 77 },
+    { label: 'work', value: 11 },
+    { label: 'home', value: 34 },
+  ]}
+>
+  <Text variant="body" className="text-primary-foreground">
+    Extra Content
+  </Text>
+</IdentityContentRow>
+`}),`
+`,t.jsx(r,{of:d,sourceState:"none"})]})}function B(o={}){const{wrapper:e}={...i(),...o.components};return e?t.jsx(e,{...o,children:t.jsx(a,{...o})}):a(o)}export{B as default};
