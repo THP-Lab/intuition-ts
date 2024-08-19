2ef9933d6da148a6fe5b1c1d047ea265036685ec
Vitalsine85 <rob@creative-element.net>
2024-08-19 17:17:50 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@4e47420dd1667591b5f6c80c8c9d454118b033eb 🚀

---
+++ assets/Avatar-Sj_NenT4.js
@@ -0,0 +1,31 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as o}from"./index-DSkyVWTJ.js";import{M as s,T as m,d as e,C as a,e as p}from"./index-CmWL_-pq.js";import{S as c,B as l,E as u,U as x,a as d}from"./Avatar.stories-B22AdC6d.js";import"./index-CTjT7uj6.js";import"./iframe-aWdCAB5P.js";import"../sb-preview/runtime.js";import"./index-BqnMPuz1.js";import"./index-DVBfyTD9.js";import"./index-9r8iugjR.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./AlertDialog-7HsbxEn8.js";import"./Checkbox-xhbVBHnK.js";import"./Icon.types-b6Lel7VE.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Text-COZqca-9.js";import"./Label-CkICs1_r.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-juOyYLvc.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";function i(r){const n={h2:"h2",h3:"h3",h4:"h4",...o(),...r.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:c}),`
+`,t.jsx(m,{}),`
+`,t.jsx(n.h4,{id:"an-image-element-with-a-fallback-for-representing-an-identity",children:"An image element with a fallback for representing an identity."}),`
+`,t.jsx(e,{dark:!0,language:"tsx",code:`
+import { Avatar } from '@0xintuition/1ui'
+
+<Avatar src="image.jpg" name="Super Dave" />
+`}),`
+`,t.jsx(a,{of:l,sourceState:"none"}),`
+`,t.jsx(n.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(p,{}),`
+`,t.jsx(n.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(n.h3,{id:"entity",children:"Entity"}),`
+`,t.jsx(e,{dark:!0,language:"tsx",code:`
+<Avatar
+  variant="non-user"
+  src="image.jpg"
+  name="Intuition"
+/>
+`}),`
+`,t.jsx(a,{of:u,sourceState:"none"}),`
+`,t.jsx(n.h3,{id:"user-fallback",children:"User Fallback"}),`
+`,t.jsx(e,{dark:!0,language:"tsx",code:`
+<Avatar name="Super Dave" />
+`}),`
+`,t.jsx(a,{of:x,sourceState:"none"}),`
+`,t.jsx(n.h3,{id:"entity-fallback",children:"Entity Fallback"}),`
+`,t.jsx(e,{dark:!0,language:"tsx",code:`
+<Avatar variant="non-user" name="Intuition" />
+`}),`
+`,t.jsx(a,{of:d,sourceState:"none"})]})}function K(r={}){const{wrapper:n}={...o(),...r.components};return n?t.jsx(n,{...r,children:t.jsx(i,{...r})}):i(r)}export{K as default};
---
+++ assets/Resizable-zPwFbM5h.js
@@ -0,0 +1,64 @@
+import{j as e}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as l}from"./index-DSkyVWTJ.js";import{M as o,T as s,d as n,C as t}from"./index-CmWL_-pq.js";import{B as p}from"./AlertDialog-7HsbxEn8.js";import"./Checkbox-xhbVBHnK.js";import"./Icon.types-b6Lel7VE.js";import"./Label-CkICs1_r.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-juOyYLvc.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Text-COZqca-9.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";import{S as d,B as m,V as b,a as u}from"./Resizable.stories-CPTJluHI.js";import"./index-CTjT7uj6.js";import"./iframe-aWdCAB5P.js";import"../sb-preview/runtime.js";import"./index-BqnMPuz1.js";import"./index-DVBfyTD9.js";import"./index-9r8iugjR.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";function r(i){const a={h2:"h2",h3:"h3",h4:"h4",...l(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(o,{of:d}),`
+`,e.jsx(s,{}),`
+`,e.jsx(a.h4,{id:"accessible-resizable-panel-groups-and-layouts-with-keyboard-support",children:"Accessible resizable panel groups and layouts with keyboard support."}),`
+`,e.jsx(p,{variant:"accent",onClick:()=>window.open("https://github.com/bvaughn/react-resizable-panels/tree/main/packages/react-resizable-panels","_blank"),children:"API Reference"}),`
+`,e.jsx(n,{dark:!0,language:"tsx",code:`
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@0xintuition/1ui'
+
+<ResizablePanelGroup
+  direction="horizontal"
+>
+  <ResizablePanel defaultSize={50}>
+    <div>
+      <span>One</span>
+    </div>
+  </ResizablePanel>
+  <ResizableHandle />
+  <ResizablePanel defaultSize={50}>
+    <ResizablePanelGroup direction="vertical">
+      <ResizablePanel defaultSize={25}>
+        <div>
+          <span>Two</span>
+        </div>
+      </ResizablePanel>
+      <ResizableHandle />
+      <ResizablePanel defaultSize={75}>
+        <div>
+          <span>Three</span>
+        </div>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  </ResizablePanel>
+`}),`
+`,e.jsx(t,{of:m,sourceState:"none"}),`
+`,e.jsx(a.h2,{id:"variants",children:"Variants"}),`
+`,e.jsx(a.h3,{id:"vertical",children:"Vertical"}),`
+`,e.jsx(n,{dark:!0,language:"tsx",code:`
+<ResizablePanelGroup
+  direction="vertical"
+>
+  <ResizablePanel defaultSize={50}>
+    Top
+  </ResizablePanel>
+  <ResizableHandle />
+  <ResizablePanel defaultSize={50}>
+    Bottom
+  </ResizablePanel>
+</ResizablePanelGroup>
+`}),`
+`,e.jsx(t,{of:b,sourceState:"none"}),`
+`,e.jsx(a.h3,{id:"sidebar",children:"Sidebar"}),`
+`,e.jsx(n,{dark:!0,language:"tsx",code:`
+<ResizablePanelGroup
+  direction="horizontal"
+>
+  <ResizablePanel defaultSize={30}>
+    Sidebar
+  </ResizablePanel>
+  <ResizableHandle withHandle />
+  <ResizablePanel defaultSize={70}>
+    Content
+  </ResizablePanel>
+</ResizablePanelGroup>
+`}),`
+`,e.jsx(t,{of:u,sourceState:"none"})]})}function q(i={}){const{wrapper:a}={...l(),...i.components};return a?e.jsx(a,{...i,children:e.jsx(r,{...i})}):r(i)}export{q as default};
