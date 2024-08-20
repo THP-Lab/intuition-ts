553127833653167878b23d8cdd7a4eb0c2b33b20
Vitalsine85 <rob@creative-element.net>
2024-08-20 04:59:59 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@7985a3454792a3fc86347508ce655fbdc22c9223 🚀

---
+++ assets/Tooltip-DyDRb-hg.js
@@ -0,0 +1,26 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as r}from"./index-DSkyVWTJ.js";import{M as n,T as p,d as m,C as s}from"./index-bqb-4Y67.js";import{B as a}from"./AlertDialog-Bo4U6WCi.js";import"./Checkbox-ZeP1BfCD.js";import"./Icon.types-DaL_GC-u.js";import"./Label-CkICs1_r.js";import"./PieChart-B_dtFskq.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-juOyYLvc.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Text-COZqca-9.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";import{S as l,B as c}from"./Tooltip.stories-DuHw4i1q.js";import"./index-CTjT7uj6.js";import"./iframe-BQQUy3_-.js";import"../sb-preview/runtime.js";import"./index-BqnMPuz1.js";import"./index-DVBfyTD9.js";import"./index-9r8iugjR.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";function i(t){const e={h4:"h4",...r(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{of:l}),`
+`,o.jsx(p,{}),`
+`,o.jsx(e.h4,{id:"a-popup-that-displays-information-related-to-an-element-when-the-element-receives-keyboard-focus-or-the-mouse-hovers-over-it",children:"A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it."}),`
+`,o.jsx(a,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/tooltip#api-reference","_blank"),children:"API Reference"}),`
+`,o.jsx(m,{dark:!0,language:"tsx",code:`
+import { 
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger, 
+} from '@0xintuition/1ui'
+
+<TooltipProvider>
+  <Tooltip>
+    <TooltipTrigger asChild>
+      <Button variant="ghost" size="lg">
+        Hover
+      </Button>
+    </TooltipTrigger>
+    <TooltipContent>
+      <Text variant="body">Some text</Text>
+    </TooltipContent>
+  </Tooltip>
+</TooltipProvider>
+`}),`
+`,o.jsx(s,{of:c,sourceState:"none"})]})}function G(t={}){const{wrapper:e}={...r(),...t.components};return e?o.jsx(e,{...t,children:o.jsx(i,{...t})}):i(t)}export{G as default};
