4e13f7d846d64d5b88423155177f433a05c9fa04
Vitalsine85 <rob@creative-element.net>
2024-08-20 05:31:05 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@c37e9c0a3777936ed989c9ba2a334d737a536dd8 🚀

---
+++ assets/Select-CuywFp-F.js
@@ -0,0 +1,10 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as e}from"./index-DSkyVWTJ.js";import{M as n,T as m,d as s,C as p}from"./index-DYMRUBrM.js";import{B as a}from"./AlertDialog-ijeA-tbu.js";import"./Checkbox-ZeP1BfCD.js";import"./Icon.types-DaL_GC-u.js";import"./Label-CkICs1_r.js";import"./PieChart-9tI5Soxs.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-juOyYLvc.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Text-COZqca-9.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";import{S as c,B as u}from"./Select.stories-D8BcgErb.js";import"./index-CTjT7uj6.js";import"./iframe-8TnZFXdc.js";import"../sb-preview/runtime.js";import"./index-BqnMPuz1.js";import"./index-DVBfyTD9.js";import"./index-9r8iugjR.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";function i(o){const r={h4:"h4",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:c}),`
+`,t.jsx(m,{}),`
+`,t.jsx(r.h4,{id:"displays-a-list-of-options-for-the-user-to-pick-fromtriggered-by-a-button",children:"Displays a list of options for the user to pick from—triggered by a button."}),`
+`,t.jsx(a,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/select#api-reference","_blank"),children:"API Reference"}),`
+`,t.jsx(s,{dark:!0,language:"tsx",code:`
+import { Select } from '@0xintuition/1ui'
+
+<Select />
+`}),`
+`,t.jsx(p,{of:u,sourceState:"none"})]})}function H(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{H as default};
