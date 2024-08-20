5aa30871702e1ddc0c18387f0cef8fe3b60d9d88
Vitalsine85 <rob@creative-element.net>
2024-08-20 08:27:34 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@cf9711c5cedc1810e178fa0323e1af792a8d55d2 🚀

---
+++ assets/Claim-gQGJZ2cp.js
@@ -0,0 +1,50 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as a}from"./index-DSkyVWTJ.js";import{M as s,T as m,d as r,C as o,e as p}from"./index-1gKu10g8.js";import"./AlertDialog-DWG-bC6z.js";import"./Checkbox-ZeP1BfCD.js";import"./Icon.types-DaL_GC-u.js";import"./Label-CkICs1_r.js";import"./PieChart-9tI5Soxs.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-juOyYLvc.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Text-COZqca-9.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";import{S as c,B as l,U as d}from"./Claim.stories-BWz9YoaY.js";import"./index-CTjT7uj6.js";import"./iframe-bwU_3mJj.js";import"../sb-preview/runtime.js";import"./index-BqnMPuz1.js";import"./index-DVBfyTD9.js";import"./index-9r8iugjR.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";function n(e){const i={code:"code",h2:"h2",h3:"h3",h4:"h4",p:"p",...a(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:c}),`
+`,t.jsx(m,{}),`
+`,t.jsx(i.h4,{id:"to-display-a-claim-with-subject-predicate-and-object",children:"To display a claim with subject, predicate, and object"}),`
+`,t.jsx(r,{dark:!0,language:"tsx",code:`
+import { Claim } from '@0xintuition/1ui'
+
+<Claim
+  size="md"
+  subject={{
+    variant: 'non-user',
+    label: '0xintuition',
+  }}
+  predicate={{
+    variant: 'non-user',
+    label: 'is really',
+  }}
+  object={{
+    variant: 'non-user',
+    label: 'cool',
+  }}
+/>
+`}),`
+`,t.jsx(o,{of:l,sourceState:"none"}),`
+`,t.jsx(i.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(p,{}),`
+`,t.jsx(i.h2,{id:"variants",children:"Variants"}),`
+`,t.jsx(i.h3,{id:"user",children:"User"}),`
+`,t.jsxs(i.p,{children:["With the ",t.jsx(i.code,{children:"user"})," variant of ",t.jsx(i.code,{children:"Identity"})]}),`
+`,t.jsx(r,{dark:!0,language:"tsx",code:`
+import { Claim } from '@0xintuition/1ui'
+
+<Claim
+  size="md"
+  subject={{
+    variant: 'user',
+    label: 'Alice',
+    imgSrc: 'https://m.media-amazon.com/images/M/MV5BNDhiMWYzMjgtNTRiYi00ZTA3LThlODctNDRkMDk0NzFkMWI3L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTg0MTkzMzA@._V1_.jpg',
+  }}
+  predicate={{
+    variant: 'non-user',
+    label: 'likes',
+  }}
+  object={{
+    variant: 'non-user',
+    label: 'pizza',
+  }}
+
+/>
+`}),`
+`,t.jsx(o,{of:d,sourceState:"none"})]})}function q(e={}){const{wrapper:i}={...a(),...e.components};return i?t.jsx(i,{...e,children:t.jsx(n,{...e})}):n(e)}export{q as default};
