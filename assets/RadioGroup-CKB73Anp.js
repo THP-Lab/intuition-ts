f4badcf54cc7381228b5ad5624dce107ef476652
0xjojikun <110207608+0xjojikun@users.noreply.github.com>
2024-08-20 22:51:23 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@66dc7582bbf4f81315c7fea46b3fb1f6c14ee8e1 🚀

---
+++ assets/RadioGroup-CKB73Anp.js
@@ -0,0 +1,90 @@
+import{j as e}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as n}from"./index-DSkyVWTJ.js";import{M as d,T as m,d as a,C as t}from"./index-CJ6bG7ka.js";import{B as u}from"./AlertDialog-CwENANUf.js";import"./Checkbox-ZeP1BfCD.js";import"./Icon.types-DaL_GC-u.js";import"./Label-CkICs1_r.js";import"./PieChart-9tI5Soxs.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-juOyYLvc.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Text-COZqca-9.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";import{S as p,B as s,a as l,b as c}from"./RadioGroup.stories-18VhYZBj.js";import"./index-CTjT7uj6.js";import"./iframe-BbTpSWzd.js";import"../sb-preview/runtime.js";import"./index-BqnMPuz1.js";import"./index-DVBfyTD9.js";import"./index-9r8iugjR.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";function r(o){const i={code:"code",h2:"h2",h3:"h3",h4:"h4",p:"p",...n(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(d,{of:p}),`
+`,e.jsx(m,{}),`
+`,e.jsx(i.h4,{id:"a-set-of-checkable-buttonsknown-as-radio-buttonswhere-no-more-than-one-of-the-buttons-can-be-checked-at-a-time",children:"A set of checkable buttons—known as radio buttons—where no more than one of the buttons can be checked at a time."}),`
+`,e.jsx(u,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/radio-group#api-reference","_blank"),children:"API Reference"}),`
+`,e.jsx(a,{dark:!0,language:"tsx",code:`
+import { 
+  RadioGroup,
+  RadioGroupDivider,
+  RadioGroupItem,
+  RadioGroupItemContainer,
+  RadioGroupItemLabel,
+} from '@0xintuition/1ui'
+
+const radioGroupData = [
+{
+id: 'minimum',
+value: 'minimum',
+displayValue: 'Minimum',
+displaySubValue: '+0.001 ETH',
+},
+{
+id: 'minimum',
+value: 'default',
+displayValue: 'Default',
+displaySubValue: '+0.01 ETH',
+},
+{
+id: 'minimum',
+value: 'strong',
+displayValue: 'Strong',
+displaySubValue: '+0.05 ETH',
+},
+]
+
+const numberOfRadioGroupItems = radioGroupData.length
+
+<RadioGroup defaultValue={radioGroupData[0].value}>
+  {radioGroupData.map((item, index) => (
+    <div key={index}>
+      <RadioGroupItemContainer>
+        <RadioGroupItemLabel
+          htmlFor={item.id}
+          value={item.displayValue}
+          subValue={item.displaySubValue}
+        />
+        <RadioGroupItem value={item.value} id={item.id} />
+      </RadioGroupItemContainer>
+      {index + 1 < numberOfRadioGroupItems && <RadioGroupDivider />}
+    </div>
+  ))}
+</RadioGroup>
+`}),`
+`,e.jsx(t,{of:s,sourceState:"none"}),`
+`,e.jsx(i.h2,{id:"variants",children:"Variants"}),`
+`,e.jsx(i.h3,{id:"simple",children:"Simple"}),`
+`,e.jsxs(i.p,{children:["Add ",e.jsx(i.code,{children:'variant="simple"'})," to both ",e.jsx(i.code,{children:"RadioGroup"})," and ",e.jsx(i.code,{children:"RadioGroupItemContainer"})]}),`
+`,e.jsx(a,{dark:!0,language:"tsx",code:`
+<RadioGroup variant="simple" defaultValue={radioGroupData[0].value}>
+  {radioGroupData.map((item, index) => (
+    <div key={index}>
+      <RadioGroupItemContainer variant="simple">
+        <RadioGroupItem value={item.value} id={item.id} />
+        <RadioGroupItemLabel
+          htmlFor={item.id}
+          value={item.displayValue}
+        />
+      </RadioGroupItemContainer>
+    </div>
+  ))}
+</RadioGroup>
+`}),`
+`,e.jsx(t,{of:l,sourceState:"none"}),`
+`,e.jsx(i.h3,{id:"simple-small",children:"Simple Small"}),`
+`,e.jsxs(i.p,{children:["Add ",e.jsx(i.code,{children:'size="small"'})," to ",e.jsx(i.code,{children:"RadioGroupItem"})]}),`
+`,e.jsx(a,{dark:!0,language:"tsx",code:`
+<RadioGroup variant="simple" defaultValue={radioGroupData[0].value}>
+  {radioGroupData.map((item, index) => (
+    <div key={index}>
+      <RadioGroupItemContainer variant="simple">
+        <RadioGroupItem size="small" value={item.value} id={item.id} />
+        <RadioGroupItemLabel
+          htmlFor={item.id}
+          value={item.displayValue}
+        />
+      </RadioGroupItemContainer>
+    </div>
+  ))}
+</RadioGroup>
+`}),`
+`,e.jsx(t,{of:c,sourceState:"none"})]})}function q(o={}){const{wrapper:i}={...n(),...o.components};return i?e.jsx(i,{...o,children:e.jsx(r,{...o})}):r(o)}export{q as default};
---
+++ assets/Textarea-d-YpqyIM.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as e}from"./index-DSkyVWTJ.js";import{M as m,T as a,d as n,C as p,e as s}from"./index-CJ6bG7ka.js";import"./AlertDialog-CwENANUf.js";import"./Checkbox-ZeP1BfCD.js";import"./Icon.types-DaL_GC-u.js";import"./Label-CkICs1_r.js";import"./PieChart-9tI5Soxs.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-juOyYLvc.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Text-COZqca-9.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";import{S as x,B as c}from"./Textarea.stories-Gde0L1CO.js";import"./index-CTjT7uj6.js";import"./iframe-BbTpSWzd.js";import"../sb-preview/runtime.js";import"./index-BqnMPuz1.js";import"./index-DVBfyTD9.js";import"./index-9r8iugjR.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-Ca1CH5Oj.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";function i(o){const r={h2:"h2",h4:"h4",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{of:x}),`
+`,t.jsx(a,{}),`
+`,t.jsx(r.h4,{id:"displays-a-form-textarea-or-a-component-that-looks-like-a-textarea",children:"Displays a form textarea or a component that looks like a textarea."}),`
+`,t.jsx(n,{dark:!0,language:"tsx",code:`
+import { Textarea } from '@0xintuition/1ui'
+
+<Textarea />
+`}),`
+`,t.jsx(p,{of:c,sourceState:"none"}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(s,{})]})}function I(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{I as default};
