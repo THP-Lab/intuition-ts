658c8f619816c112d886008ef7eb2f5816ab3ae5
Vitalsine85 <rob@creative-element.net>
2024-08-21 23:48:07 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@129aab75d2ea61ef9399eb717ca8b4539e8f313a 🚀

---
+++ assets/introduction-Ceg0v1kI.js
@@ -0,0 +1,7 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as r}from"./index-DSkyVWTJ.js";import{M as a,d as i}from"./index-blmywl8o.js";import"./index-CTjT7uj6.js";import"./iframe-mFn6ld0v.js";import"../sb-preview/runtime.js";import"./index-BqnMPuz1.js";import"./index-DVBfyTD9.js";import"./index-9r8iugjR.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";function e(o){const n={a:"a",h1:"h1",h2:"h2",p:"p",...r(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{title:"Documentation/Introduction"}),`
+`,t.jsx(n.h1,{id:"introduction",children:"Introduction"}),`
+`,t.jsxs(n.p,{children:["1ui is a web3-enabled ",t.jsx(n.a,{href:"https://react.dev/",rel:"nofollow",children:"React"})," component library by Intuition Systems."]}),`
+`,t.jsx(n.h2,{id:"usage",children:"Usage"}),`
+`,t.jsx(i,{dark:!0,language:"bash",code:"pnpm install @0xintuition/1ui"}),`
+`,t.jsx(i,{dark:!0,language:"bash",code:"npm install @0xintuition/1ui"}),`
+`,t.jsx(i,{dark:!0,language:"bash",code:"yarn add @0xintuition/1ui"})]})}function b(o={}){const{wrapper:n}={...r(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(e,{...o})}):e(o)}export{b as default};
