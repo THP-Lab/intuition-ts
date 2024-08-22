83ce77ab7b1cb75c9bf76baf609e7403caa451ff
jonathanprozzi <jonathanprozzi@gmail.com>
2024-08-22 03:18:09 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@d7563c832ea87b755bc2c0161c29905d3a821d44 🚀

---
+++ assets/ListHeaderCard.stories-Crsgdg45.js
@@ -0,0 +1,19 @@
+import{j as r}from"./jsx-runtime-Cw0GR0a5.js";import{ab as i,v as s}from"./AlertDialog-Ce_4ZjMI.js";import"./Checkbox-D8lO2F2v.js";import"./Icon.types-DHgQg1UV.js";import"./Label-CkICs1_r.js";import"./PieChart-9tI5Soxs.js";import"./ProgressBar-Bfgyotpp.js";import"./ProgressCard-BYKzYHAL.js";import"./SegmentedControl-DQCOk3Kf.js";import"./Separator-juOyYLvc.js";import"./Skeleton-pYLGnhFW.js";import"./Table-JXx_s4Wj.js";import"./Text-COZqca-9.js";import"./Textarea-74mgfiS7.js";import"./ValueDisplay-BLpqF4VW.js";const l={title:"Components/Lists/ListHeaderCard",component:i},e={args:{label:"Identities",value:35,children:r.jsx(s,{subject:{variant:"non-user",label:"0xintuition"},predicate:{variant:"non-user",label:"is really"},object:{variant:"non-user",label:"cool"}})},render:o=>r.jsx("div",{className:"w-[600px]",children:r.jsx(i,{...o})})};var a,t,n;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    label: 'Identities',
+    value: 35,
+    children: <Claim subject={{
+      variant: 'non-user',
+      label: '0xintuition'
+    }} predicate={{
+      variant: 'non-user',
+      label: 'is really'
+    }} object={{
+      variant: 'non-user',
+      label: 'cool'
+    }} />
+  },
+  render: args => <div className="w-[600px]">
+      <ListHeaderCard {...args} />
+    </div>
+}`,...(n=(t=e.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};const m=["BasicUsage"],h=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:e,__namedExportsOrder:m,default:l},Symbol.toStringTag,{value:"Module"}));export{e as B,h as S};
