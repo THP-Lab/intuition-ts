c04b01dc4e2b629ce0b6667e63060c2dd3c5899b
Vitalsine85 <rob@creative-element.net>
2024-08-27 19:39:40 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@6c4f87fbc2b4dc2b08d2130586315cd05091316a 🚀

---
+++ assets/Dialog-isj-VP0K.js
@@ -0,0 +1,29 @@
+import{j as o}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as e}from"./index-DSkyVWTJ.js";import{M as n,T as a,d as m,C as p}from"./index-DhtnDMZb.js";import{B as s}from"./AlertDialog-DLQ9V3DC.js";import"./Checkbox-BGNaxvcv.js";import"./Icon.types-cBi97MNl.js";import"./Label-BP8-Mx5Z.js";import"./PieChart-9tI5Soxs.js";import"./ProgressBar-B07pz6pZ.js";import"./ProgressCard-e5tcEEn0.js";import"./SegmentedControl-CoVeRGEn.js";import"./Separator-r-Tg2QWT.js";import"./Skeleton-k6WNkHQZ.js";import"./Table-4UgCNE7H.js";import"./Text-19x_d4-A.js";import"./Textarea-DtV2ds88.js";import"./ValueDisplay-ClYOGsr-.js";import{S as l,B as d}from"./Dialog.stories-CH4fkaWR.js";import"./index-CTjT7uj6.js";import"./iframe-DO3TBT1f.js";import"../sb-preview/runtime.js";import"./index-BqnMPuz1.js";import"./index-DVBfyTD9.js";import"./index-9r8iugjR.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-sXcn2mTS.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";function r(i){const t={h4:"h4",...e(),...i.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{of:l}),`
+`,o.jsx(a,{}),`
+`,o.jsx(t.h4,{id:"a-window-overlaid-on-either-the-primary-window-or-another-dialog-window-rendering-the-content-underneath-inert",children:"A window overlaid on either the primary window or another dialog window, rendering the content underneath inert."}),`
+`,o.jsx(s,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/dialog#api-reference","_blank"),children:"API Reference"}),`
+`,o.jsx(m,{dark:!0,language:"tsx",code:`
+import { 
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger, } from '@0xintuition/1ui'
+
+<Dialog>
+  <DialogTrigger>
+    <Button size="lg">Open dialog</Button>
+  </DialogTrigger>
+  <DialogContent>
+    <DialogHeader>
+      <DialogTitle>Are you absolutely sure?</DialogTitle>
+      <DialogDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </DialogDescription>
+    </DialogHeader>
+  </DialogContent>
+</Dialog>
+`}),`
+`,o.jsx(p,{of:d,sourceState:"none"})]})}function q(i={}){const{wrapper:t}={...e(),...i.components};return t?o.jsx(t,{...i,children:o.jsx(r,{...i})}):r(i)}export{q as default};
---
+++ assets/ListIdentityCard-CdrRwriD.js
@@ -0,0 +1,18 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as e}from"./index-DSkyVWTJ.js";import{M as n,T as s,d as m,C as p,e as a}from"./index-DhtnDMZb.js";import{S as c,B as d}from"./ListIdentityCard.stories-DtdPY-X1.js";import"./index-CTjT7uj6.js";import"./iframe-DO3TBT1f.js";import"../sb-preview/runtime.js";import"./index-BqnMPuz1.js";import"./index-DVBfyTD9.js";import"./index-9r8iugjR.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./AlertDialog-DLQ9V3DC.js";import"./Checkbox-BGNaxvcv.js";import"./Icon.types-cBi97MNl.js";import"./index-sXcn2mTS.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Text-19x_d4-A.js";import"./Label-BP8-Mx5Z.js";import"./PieChart-9tI5Soxs.js";import"./ProgressBar-B07pz6pZ.js";import"./ProgressCard-e5tcEEn0.js";import"./SegmentedControl-CoVeRGEn.js";import"./Separator-r-Tg2QWT.js";import"./Skeleton-k6WNkHQZ.js";import"./Table-4UgCNE7H.js";import"./Textarea-DtV2ds88.js";import"./ValueDisplay-ClYOGsr-.js";function i(o){const r={h2:"h2",h4:"h4",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:c}),`
+`,t.jsx(s,{}),`
+`,t.jsx(r.h4,{id:"a-card-component-to-display-identity-information",children:"A card component to display identity information"}),`
+`,t.jsx(m,{dark:!0,language:"tsx",code:`
+import { ListIdentityCard } from '@0xintuition/1ui'
+
+<ListIdentityCard
+  displayName="Best Crypto Portfolio Trackers"
+  imgSrc="https://avatars.githubusercontent.com/u/94311139?s=200&v=4"
+  identitiesCount={45}
+  savedAmount="0.047"
+  onSaveClick={() => alert('Best Crypto Portfolio Trackers saved!')}
+  isSaved={true}
+/>
+`}),`
+`,t.jsx(p,{of:d,sourceState:"none"}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(a,{})]})}function G(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{G as default};
