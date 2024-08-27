c636897035c001414cbfc08a190cc21389a01331
Vitalsine85 <rob@creative-element.net>
2024-08-27 18:51:04 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@9014ea84d7a75bd1e3373b666159bce02ddc2fac 🚀

---
+++ assets/SocialLinks-WST1sNgW.js
@@ -0,0 +1,25 @@
+import{j as i}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as r}from"./index-DSkyVWTJ.js";import{M as s,T as t,d as n,C as m}from"./index-DpANbTHz.js";import{S as p,B as c}from"./SocialLinks.stories-CMclE03M.js";import"./index-CTjT7uj6.js";import"./iframe-BgVUdEdV.js";import"../sb-preview/runtime.js";import"./index-BqnMPuz1.js";import"./index-DVBfyTD9.js";import"./index-9r8iugjR.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./AlertDialog-DHY_VWGd.js";import"./Checkbox-BGNaxvcv.js";import"./Icon.types-cBi97MNl.js";import"./index-sXcn2mTS.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Text-19x_d4-A.js";import"./Label-BP8-Mx5Z.js";import"./PieChart-9tI5Soxs.js";import"./ProgressBar-B07pz6pZ.js";import"./ProgressCard-e5tcEEn0.js";import"./SegmentedControl-CoVeRGEn.js";import"./Separator-r-Tg2QWT.js";import"./Skeleton-k6WNkHQZ.js";import"./Table-4UgCNE7H.js";import"./Textarea-DtV2ds88.js";import"./ValueDisplay-ClYOGsr-.js";function a(o){const e={h4:"h4",...r(),...o.components};return i.jsxs(i.Fragment,{children:[i.jsx(s,{of:p}),`
+`,i.jsx(t,{}),`
+`,i.jsx(e.h4,{id:"displays-a-users-social-media-connections",children:"Displays a user's social media connections."}),`
+`,i.jsx(n,{dark:!0,language:"tsx",code:`
+import { 
+  SocialLinks
+  SocialLinksBadge,
+  SocialLinksBadges,
+  SocialLinksButton, 
+} from '@0xintuition/1ui'
+
+<SocialLinks>
+  <SocialLinksBadges>
+    <SocialLinksBadge isVerified platform="discord" username="@superdave" />
+    <SocialLinksBadge isVerified platform="x" username="@superdave" />
+    <SocialLinksBadge platform="farcaster" username="@superdave" />
+    <SocialLinksBadge platform="lens" username="@superdave" />
+    <SocialLinksBadge isVerified platform="calendly" username="@superdave" />
+    <SocialLinksBadge platform="github" username="@superdave" />
+    <SocialLinksBadge platform="medium" username="@superdave" />
+  </SocialLinksBadges>
+  <SocialLinksButton onClick={() => doSomething()} />
+</SocialLinks>
+`}),`
+`,i.jsx(m,{of:c,sourceState:"none"})]})}function H(o={}){const{wrapper:e}={...r(),...o.components};return e?i.jsx(e,{...o,children:i.jsx(a,{...o})}):a(o)}export{H as default};
---
+++ assets/Switch.stories-BdQMHzNQ.js
@@ -0,0 +1,7 @@
+import{j as r}from"./jsx-runtime-Cw0GR0a5.js";import{b0 as s}from"./AlertDialog-DHY_VWGd.js";import"./Checkbox-BGNaxvcv.js";import"./Icon.types-cBi97MNl.js";import{L as m}from"./Label-BP8-Mx5Z.js";import"./PieChart-9tI5Soxs.js";import"./ProgressBar-B07pz6pZ.js";import"./ProgressCard-e5tcEEn0.js";import"./SegmentedControl-CoVeRGEn.js";import"./Separator-r-Tg2QWT.js";import"./Skeleton-k6WNkHQZ.js";import"./Table-4UgCNE7H.js";import"./Text-19x_d4-A.js";import"./Textarea-DtV2ds88.js";import"./ValueDisplay-ClYOGsr-.js";const p={title:"Components/Form Elements/Switch",component:s},e={args:{},render:i=>r.jsxs("div",{className:"flex items-center space-x-3",children:[r.jsx(s,{id:"airplane-mode",...i}),r.jsx(m,{htmlFor:"airplane-mode",children:"Airplane Mode"})]})};var o,t,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {},
+  render: args => <div className="flex items-center space-x-3">
+      <Switch id="airplane-mode" {...args} />
+      <Label htmlFor="airplane-mode">Airplane Mode</Label>
+    </div>
+}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const n=["BasicUsage"],B=Object.freeze(Object.defineProperty({__proto__:null,BasicUsage:e,__namedExportsOrder:n,default:p},Symbol.toStringTag,{value:"Module"}));export{e as B,B as S};
