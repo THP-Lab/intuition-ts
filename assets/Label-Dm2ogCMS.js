8b9f016346ea4323f9de56dbb49d9f8044407b02
Vitalsine85 <rob@creative-element.net>
2024-09-04 18:44:29 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@a5c9450c0cd15903ec09037498085b4134f6df66 🚀

---
+++ assets/Label-Dm2ogCMS.js
@@ -0,0 +1,10 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as e}from"./index-DSkyVWTJ.js";import{M as n,T as m,d as s,C as a}from"./index-oACoXUQK.js";import{B as p}from"./AlertDialog-kehiiu69.js";import"./Checkbox-BGNaxvcv.js";import"./Icon.types-cBi97MNl.js";import"./Label-BP8-Mx5Z.js";import"./PieChart-9tI5Soxs.js";import"./ProgressBar-B07pz6pZ.js";import"./ProgressCard-e5tcEEn0.js";import"./SegmentedControl-CoVeRGEn.js";import"./Separator-r-Tg2QWT.js";import"./Skeleton-k6WNkHQZ.js";import"./Table-4UgCNE7H.js";import"./Text-19x_d4-A.js";import"./Textarea-DtV2ds88.js";import"./ValueDisplay-ClYOGsr-.js";import{S as c,B as l}from"./Label.stories-DMtLn6hC.js";import"./index-CTjT7uj6.js";import"./iframe-D0zSBwId.js";import"../sb-preview/runtime.js";import"./index-BqnMPuz1.js";import"./index-DVBfyTD9.js";import"./index-9r8iugjR.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-Bb4qSo10.js";import"./index-sXcn2mTS.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";function i(o){const r={h4:"h4",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:c}),`
+`,t.jsx(m,{}),`
+`,t.jsx(r.h4,{id:"renders-an-accessible-label-associated-with-controls",children:"Renders an accessible label associated with controls."}),`
+`,t.jsx(p,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/label#api-reference","_blank"),children:"API Reference"}),`
+`,t.jsx(s,{dark:!0,language:"tsx",code:`
+import { Label } from '@0xintuition/1ui'
+
+<Label htmlFor="email">Your email address</Label>
+`}),`
+`,t.jsx(a,{of:l,sourceState:"none"})]})}function G(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{G as default};
