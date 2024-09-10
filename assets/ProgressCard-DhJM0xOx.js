a4a1f46f9030304937e9cf859b07f9630e419333
Vitalsine85 <rob@creative-element.net>
2024-09-10 16:36:41 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@6bfc72a5d1884483b93fe4d097a0f1d4673cbf1c 🚀

---
+++ assets/ProgressCard-DhJM0xOx.js
@@ -0,0 +1,11 @@
+import{j as r}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as n}from"./index-DSkyVWTJ.js";import{M as s,T as i,d as m,C as a,e as p}from"./index-FWQET-PI.js";import{S as c,B as d}from"./ProgressCard.stories-CfjuCVr2.js";import"./index-CTjT7uj6.js";import"./iframe-C6UrzEzG.js";import"../sb-preview/runtime.js";import"./index-BqnMPuz1.js";import"./index-DVBfyTD9.js";import"./index-9r8iugjR.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./ProgressCard-e5tcEEn0.js";import"./ProgressBar-B07pz6pZ.js";import"./index-sXcn2mTS.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./Text-19x_d4-A.js";import"./index-Bb4qSo10.js";function e(o){const t={h2:"h2",h4:"h4",...n(),...o.components};return r.jsxs(r.Fragment,{children:[r.jsx(s,{of:c}),`
+`,r.jsx(i,{}),`
+`,r.jsx(t.h4,{id:"generic-progress-card-banner-display",children:"Generic progress card banner display"}),`
+`,r.jsx(m,{dark:!0,language:"tsx",code:`
+import { ProgressCard } from '@0xintuition/1ui'
+
+<ProgressCard numberTotal={10} numberCompleted={5} />
+`}),`
+`,r.jsx(a,{of:d,sourceState:"none"}),`
+`,r.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,r.jsx(p,{})]})}function w(o={}){const{wrapper:t}={...n(),...o.components};return t?r.jsx(t,{...o,children:r.jsx(e,{...o})}):e(o)}export{w as default};
