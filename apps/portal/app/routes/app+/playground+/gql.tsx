d5adf545cfc4f663b546e80f04003db71d142c5e
jonathanprozzi <jonathanprozzi@gmail.com>
2024-10-03 20:45:46 -0400

    Switches port to 3000 to avoid collision with hasura, adds in a graphql playground with test query to hasura

---
+++ apps/portal/app/routes/app+/playground+/gql.tsx
@@ -0,0 +1,44 @@
+import process from 'process'
+
+import type { LoaderFunction } from '@remix-run/node'
+import { json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+
+export const loader: LoaderFunction = async () => {
+  const response = await fetch('http://localhost:8080/v1/graphql', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'x-hasura-admin-secret': process.env.HASURA_GRAPHQL_ADMIN_SECRET || '',
+    },
+    body: JSON.stringify({
+      query: `
+      query GetAtoms {
+        atoms {
+          id
+          label
+          creator {
+            id
+            label
+            image
+          }
+        }
+      }
+    `,
+    }),
+  })
+
+  const data = await response.json()
+  return json(data)
+}
+
+export default function Atoms() {
+  const data = useLoaderData<typeof loader>()
+
+  return (
+    <div>
+      <h1>Atoms</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  )
+}
--- apps/portal/vite.config.ts
+++ apps/portal/vite.config.ts
@@ -49,7 +49,7 @@ export default defineConfig({
       : null,
   ],
   server: {
-    port: 8080,
+    port: 3000,
   },
   build: {
     target: 'ES2022',
