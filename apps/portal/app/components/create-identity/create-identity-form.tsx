6fccc7513bf2aca47d6a02ba5dd06fb3241bfcdf
Vitalsine85 <rob@creative-element.net>
2024-10-07 15:20:13 -0400

    create identity form header

--- apps/portal/app/components/create-identity/create-identity-form.tsx
+++ apps/portal/app/components/create-identity/create-identity-form.tsx
@@ -174,7 +174,6 @@ export function IdentityForm({
 
   const { data: walletClient } = useWalletClient()
   const publicClient = usePublicClient()
-  const { address } = useAccount()
   const {
     writeContractAsync: writeCreateIdentity,
     awaitingWalletConfirmation,
@@ -453,10 +452,6 @@ export function IdentityForm({
     onClose()
   }
 
-  const walletBalance = useGetWalletBalance(
-    address ?? (wallet as `0x${string}`),
-  )
-
   return (
     <>
       <offChainFetcher.Form
@@ -638,69 +633,6 @@ export function IdentityForm({
                 errors={fields.external_reference.errors}
               />
             </div>
-            <div className="flex flex-col w-full gap-1.5">
-              <div className="flex flex-row items-center justify-between mb-1">
-                <div className="inline-flex gap-1">
-                  <Label htmlFor={fields.initial_deposit.id} hidden>
-                    Initial Deposit
-                  </Label>
-                  <div className="self-stretch flex-col justify-start items-start flex">
-                    <div className="flex w-full items-center justify-between gap-1">
-                      <Text
-                        variant="caption"
-                        className="text-secondary-foreground"
-                      >
-                        Initial Deposit
-                      </Text>
-
-                      <InfoTooltip
-                        title="Initial Deposit"
-                        content="You will not receive shares merely by creating this Atom/Identity - so, if you believe in it, and think that it will generate fees, then you will need to deposit on it, to gain ownership of it. You will not be charged an entry fee for depositing on your newly-created Atom/Identity."
-                      />
-                    </div>
-                  </div>
-                </div>
-                <Badge className="bg-transparent">
-                  <Icon name="wallet" className="h-4 w-4" />
-                  {(+walletBalance).toFixed(2)} ETH
-                </Badge>
-              </div>
-              <Label htmlFor={fields.initial_deposit.id} hidden>
-                Initial Deposit
-              </Label>
-              <Input
-                id="position"
-                autoComplete="off"
-                type="text"
-                value={initialDeposit}
-                onChange={(e) => {
-                  e.preventDefault()
-                  let inputValue = e.target.value
-                  if (inputValue.startsWith('.')) {
-                    inputValue = `0${inputValue}`
-                  }
-                  const sanitizedValue = inputValue.replace(/[^0-9.]/g, '')
-                  if (sanitizedValue.split('.').length > 2) {
-                    return
-                  }
-                  setInitialDeposit(sanitizedValue)
-                }}
-                min={'0'}
-                placeholder={'0'}
-                startAdornment="ETH"
-              />
-              <ErrorList
-                id={fields.initial_deposit.errorId}
-                errors={fields.initial_deposit.errors}
-              />
-              <Text
-                variant={TextVariant.caption}
-                className="text-center text-primary/70 mt-1"
-              >
-                Note: You will not be charged an entry fee for this initial
-                deposit.
-              </Text>
-            </div>
             <div className="mt-auto mx-auto">
               {isWrongNetwork ? (
                 <WrongNetworkButton />
@@ -716,7 +648,7 @@ export function IdentityForm({
                     }
                   }}
                   disabled={
-                    !address ||
+                    !wallet ||
                     loading ||
                     !formTouched ||
                     ['confirm', 'transaction-pending', 'awaiting'].includes(
@@ -748,7 +680,7 @@ export function IdentityForm({
                   variant="primary"
                   onClick={handleSubmit}
                   disabled={
-                    !address ||
+                    !wallet ||
                     loading ||
                     !formTouched ||
                     ['confirm', 'transaction-pending', 'awaiting'].includes(
