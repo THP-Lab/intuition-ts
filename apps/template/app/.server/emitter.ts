74903d91d11979dc3652747896b03cfaf49d87d0
jonathanprozzi <jonathanprozzi@gmail.com>
2024-10-09 13:11:22 -0400

    Removes unused .client and .server module files from template  (first pass)

--- apps/template/app/.server/emitter.ts
+++
@@ -1,9 +0,0 @@
-import { EventEmitter } from 'events'
-
-import { remember } from '@epic-web/remember'
-
-export const emitter = remember('emitter', () => {
-  const eventEmitter = new EventEmitter()
-  eventEmitter.setMaxListeners(20)
-  return eventEmitter
-})
