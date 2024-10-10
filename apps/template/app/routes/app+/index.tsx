07f4e70610b8042a680215e35b2e839310e2c5c4
jonathanprozzi <jonathanprozzi@gmail.com>
2024-10-10 13:46:15 -0400

    Adds the logout button -- removes changes to privy login for now (stepping back to rework). Upgrades privy packages

--- apps/template/app/routes/app+/index.tsx
+++ apps/template/app/routes/app+/index.tsx
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <div className="w-full flex flex-col gap-12">
       <HomeBanner />
+      <PrivyLogoutButton />
       <GetStarted />
       <PrivyLogoutButton />
     </div>
