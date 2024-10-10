e77082431025d8cf05047c5d62b649be1c5a11cf
jonathanprozzi <jonathanprozzi@gmail.com>
2024-10-10 10:36:53 -0400

    Continues to resolve rebase conflicts -- updates template to match current (pt 2)

--- apps/template/app/routes/app+/index.tsx
+++ apps/template/app/routes/app+/index.tsx
@@ -1,20 +1,13 @@
-<<<<<<< HEAD
 import PrivyLogoutButton from '@client/privy-logout-button'
 import GetStarted from '@components/get-started'
 import HomeBanner from '@components/home-banner'
-=======
-import HomeBanner from '@components/home/home-banner'
->>>>>>> 1814bcc60 (Add stripped down version of portal base)
 
 export default function App() {
   return (
     <div className="w-full flex flex-col gap-12">
       <HomeBanner />
-<<<<<<< HEAD
       <GetStarted />
       <PrivyLogoutButton />
-=======
->>>>>>> 1814bcc60 (Add stripped down version of portal base)
     </div>
   )
 }
