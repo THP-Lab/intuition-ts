398100c77309f11a76db22e03039b9ffc8febcf7
Vitalsine85 <rob@creative-element.net>
2024-10-31 18:13:39 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@b7eb3661d7892a1bc4a01583396ab7c2cde79623 🚀

---
+++ assets/HoverCard-CMaqPiYv.js
@@ -0,0 +1,42 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as i}from"./index-DSkyVWTJ.js";import{M as a,T as n,d as m,C as s}from"./index-CkM3WR1v.js";import{B as p}from"./AlertDialog-C5UQg7yU.js";import"./Checkbox-BhpbQMJC.js";import"./Icon.types-cBi97MNl.js";import"./Label-BP8-Mx5Z.js";import"./PieChart-9tI5Soxs.js";import"./ProgressBar-B07pz6pZ.js";import"./ProgressCard-Di9JTD4k.js";import"./SegmentedControl-CoVeRGEn.js";import"./Separator-r-Tg2QWT.js";import"./Skeleton-k6WNkHQZ.js";import"./Table-BlSnVnDo.js";import"./Text-BeXQet-m.js";import"./Textarea-DtV2ds88.js";import"./ValueDisplay-ClYOGsr-.js";import"./ScrollArea-B2SgIFBV.js";import{S as d,B as v}from"./HoverCard.stories-D_GhptP4.js";import"./index-CTjT7uj6.js";import"./iframe-YSewcKmI.js";import"../sb-preview/runtime.js";import"./index-wo0pwFmI.js";import"./index-DVBfyTD9.js";import"./index-9r8iugjR.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-G5r2yAnn.js";import"./index-Bb4qSo10.js";import"./index-sXcn2mTS.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";function o(e){const r={h4:"h4",...i(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{of:d}),`
+`,t.jsx(n,{}),`
+`,t.jsx(r.h4,{id:"for-sighted-users-to-preview-content-available-behind-a-link",children:"For sighted users to preview content available behind a link."}),`
+`,t.jsx(p,{variant:"accent",onClick:()=>window.open("https://www.radix-ui.com/primitives/docs/components/hover-card#api-reference","_blank"),children:"API Reference"}),`
+`,t.jsx(m,{dark:!0,language:"tsx",code:`
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '@0xintuition/1ui'
+
+<HoverCard>
+  <HoverCardTrigger asChild>
+    <Button variant="text">@super_dave</Button>
+  </HoverCardTrigger>
+  <HoverCardContent className="w-60">
+    <div className="flex gap-4">
+      <Avatar src="image.jpg" name="Super Dave" />
+      <div>
+        <Text variant="body">Super Dave</Text>
+        <Text variant="caption" className="text-primary/50">
+          @super_dave
+        </Text>
+      </div>
+    </div>
+    <Text variant="caption">
+      I am Super Dave, the most super Dave you&apos;ll ever meet.
+    </Text>
+    <div className="flex gap-10">
+      <div>
+        <Text variant="body">9122</Text>
+        <Text variant="caption" className="text-primary/50">
+          followers
+        </Text>
+      </div>
+      <div>
+        <Text variant="body">1239</Text>
+        <Text variant="caption" className="text-primary/50">
+          following
+        </Text>
+      </div>
+    </div>
+  </HoverCardContent>
+</HoverCard>
+`}),`
+`,t.jsx(s,{of:v,sourceState:"none"})]})}function J(e={}){const{wrapper:r}={...i(),...e.components};return r?t.jsx(r,{...e,children:t.jsx(o,{...e})}):o(e)}export{J as default};
---
+++ assets/TransactionStatusIndicator-DpkIhmft.js
@@ -0,0 +1,11 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as n}from"./index-DSkyVWTJ.js";import{M as s,T as a,d as m,C as p,e}from"./index-CkM3WR1v.js";import{S as c,B as u}from"./TransactionStatusIndicator.stories-CFEYAYuM.js";import"./index-CTjT7uj6.js";import"./iframe-YSewcKmI.js";import"../sb-preview/runtime.js";import"./index-wo0pwFmI.js";import"./index-DVBfyTD9.js";import"./index-9r8iugjR.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./AlertDialog-C5UQg7yU.js";import"./Checkbox-BhpbQMJC.js";import"./index-G5r2yAnn.js";import"./Icon.types-cBi97MNl.js";import"./index-sXcn2mTS.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Text-BeXQet-m.js";import"./Label-BP8-Mx5Z.js";import"./PieChart-9tI5Soxs.js";import"./ProgressBar-B07pz6pZ.js";import"./ProgressCard-Di9JTD4k.js";import"./SegmentedControl-CoVeRGEn.js";import"./Separator-r-Tg2QWT.js";import"./Skeleton-k6WNkHQZ.js";import"./Table-BlSnVnDo.js";import"./ScrollArea-B2SgIFBV.js";import"./Textarea-DtV2ds88.js";import"./ValueDisplay-ClYOGsr-.js";function i(o){const r={h2:"h2",h4:"h4",...n(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:c}),`
+`,t.jsx(a,{}),`
+`,t.jsx(r.h4,{id:"displays-an-icon-and-label-per-transaction-status",children:"Displays an icon and label per transaction status."}),`
+`,t.jsx(m,{dark:!0,language:"tsx",code:`
+import { TransactionStatusIndicator } from '@0xintuition/1ui'
+
+<TransactionStatusIndicator status="awaiting" />
+`}),`
+`,t.jsx(p,{of:u,sourceState:"none"}),`
+`,t.jsx(r.h2,{id:"properties",children:"Properties"}),`
+`,t.jsx(e,{})]})}function K(o={}){const{wrapper:r}={...n(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(i,{...o})}):i(o)}export{K as default};
