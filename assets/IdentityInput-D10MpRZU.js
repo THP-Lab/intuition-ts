fc8f3bd86ed233c5cb09c11bb389652ae0537ab1
jonathanprozzi <jonathanprozzi@gmail.com>
2024-11-08 19:48:50 +0000

    Deploying to gh-pages from @ 0xIntuition/intuition-ts@025d1427b92ac6450e3478a66f286b18fafb01b8 🚀

---
+++ assets/IdentityInput-D10MpRZU.js
@@ -0,0 +1,47 @@
+import{j as e}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as a}from"./index-DSkyVWTJ.js";import{M as s,T as m,d as i,C as r,e as c}from"./index-CJn5sriK.js";import{S as p,B as l,O as d}from"./IdentityInput.stories-CMwhF1Kj.js";import"./index-CTjT7uj6.js";import"./iframe-C-q2dkHV.js";import"../sb-preview/runtime.js";import"./index-wo0pwFmI.js";import"./index-DVBfyTD9.js";import"./index-9r8iugjR.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./AlertDialog-2bKyTLaZ.js";import"./Checkbox-BhpbQMJC.js";import"./index-G5r2yAnn.js";import"./Icon.types-cBi97MNl.js";import"./index-sXcn2mTS.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";import"./index-Bb4qSo10.js";import"./Text-BeXQet-m.js";import"./Label-BP8-Mx5Z.js";import"./PieChart-9tI5Soxs.js";import"./ProgressBar-B07pz6pZ.js";import"./ProgressCard-Di9JTD4k.js";import"./SegmentedControl-CoVeRGEn.js";import"./Separator-r-Tg2QWT.js";import"./Skeleton-k6WNkHQZ.js";import"./Table-BlSnVnDo.js";import"./ScrollArea-B2SgIFBV.js";import"./Textarea-DtV2ds88.js";import"./ValueDisplay-ClYOGsr-.js";function n(o){const t={h2:"h2",h3:"h3",h4:"h4",...a(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(s,{of:p}),`
+`,e.jsx(m,{}),`
+`,e.jsx(t.h4,{id:"allows-users-to-create-a-claim",children:"Allows users to create a claim."}),`
+`,e.jsx(i,{dark:!0,language:"tsx",code:`
+import { IdentityInput } from '@0xintuition/1ui'
+
+<IdentityInput
+  showLabels
+  subject={{
+    selectedValue: { name: 'Super Dave' },
+    onClick: () => doSomething,
+  }}
+  predicate={{
+    selectedValue: { name: 'is a', variant: 'non-user' },
+    onClick: () => doSomething,
+  }}
+  object={{
+    selectedValue: { name: 'daredevil', variant: 'non-user' },
+    onClick: () => doSomething,
+  }}
+/>
+`}),`
+`,e.jsx(r,{of:l,sourceState:"none"}),`
+`,e.jsx(t.h2,{id:"properties",children:"Properties"}),`
+`,e.jsx(c,{}),`
+`,e.jsx(t.h2,{id:"variants",children:"Variants"}),`
+`,e.jsx(t.h3,{id:"other",children:"Other"}),`
+`,e.jsx(i,{dark:!0,language:"tsx",code:`
+<IdentityInput
+  subject={{
+    placeholder: 'Select an identity',
+    selectedValue: {},
+    onClick: () => doSomething,
+  }}
+  predicate={{
+    placeholder: 'Select an identity',
+    selectedValue: {},
+    onClick: () => doSomething,
+  }}
+  object={{
+    placeholder: 'Select an identity',
+    selectedValue: {},
+    onClick: () => doSomething,
+  }}
+/>
+`}),`
+`,e.jsx(r,{of:d,sourceState:"none"})]})}function J(o={}){const{wrapper:t}={...a(),...o.components};return t?e.jsx(t,{...o,children:e.jsx(n,{...o})}):n(o)}export{J as default};
---
+++ assets/SegmentedControl-Blh-LkX0.js
@@ -0,0 +1,32 @@
+import{j as t}from"./jsx-runtime-Cw0GR0a5.js";import{useMDXComponents as r}from"./index-DSkyVWTJ.js";import{M as n,T as m,d as s,C as a}from"./index-CJn5sriK.js";import"./AlertDialog-2bKyTLaZ.js";import"./Checkbox-BhpbQMJC.js";import"./Icon.types-cBi97MNl.js";import"./Label-BP8-Mx5Z.js";import"./PieChart-9tI5Soxs.js";import"./ProgressBar-B07pz6pZ.js";import"./ProgressCard-Di9JTD4k.js";import"./SegmentedControl-CoVeRGEn.js";import"./Separator-r-Tg2QWT.js";import"./Skeleton-k6WNkHQZ.js";import"./Table-BlSnVnDo.js";import"./Text-BeXQet-m.js";import"./Textarea-DtV2ds88.js";import"./ValueDisplay-ClYOGsr-.js";import"./ScrollArea-B2SgIFBV.js";import{S as p,B as l}from"./SegmentedControl.stories-D0YA2fnU.js";import"./index-CTjT7uj6.js";import"./iframe-C-q2dkHV.js";import"../sb-preview/runtime.js";import"./index-wo0pwFmI.js";import"./index-DVBfyTD9.js";import"./index-9r8iugjR.js";import"./index-DXimoRZY.js";import"./index-Bx4XDAbk.js";import"./index-DrFu-skq.js";import"./index-G5r2yAnn.js";import"./index-Bb4qSo10.js";import"./index-sXcn2mTS.js";import"./themes-JyTiufSz.js";import"./palette-saq09hvB.js";function i(e){const o={h4:"h4",...r(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{of:p}),`
+`,t.jsx(m,{}),`
+`,t.jsx(o.h4,{id:"used-to-switch-between-views-within-a-page",children:"Used to switch between views within a page."}),`
+`,t.jsx(s,{dark:!0,language:"tsx",code:`
+import React from 'react'
+
+import { SegmentedControl } from '@0xintuition/1ui'
+
+const options = [
+{ value: 'overview', label: 'Overview' },
+{ value: 'claims', label: 'Claims' },
+{ value: 'positions', label: 'Positions' },
+{ value: 'activity', label: 'Activity' },
+]
+
+const [selectedTab, setSelectedTab] = React.useState(options[0].value)
+
+return (
+
+<SegmentedControl>
+  {options.map((option, index) => (
+    <SegmentedControlItem
+      key={index}
+      isActive={selectedTab === option.value}
+      onClick={() => setSelectedTab(option.value)}
+    >
+      {option.label}
+    </SegmentedControlItem>
+  ))}
+</SegmentedControl>
+) `}),`
+`,t.jsx(a,{of:l,sourceState:"none"})]})}function H(e={}){const{wrapper:o}={...r(),...e.components};return o?t.jsx(o,{...e,children:t.jsx(i,{...e})}):i(e)}export{H as default};
