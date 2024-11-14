7d42adabe6fa9aa20d803c5f5d3638620bec191f
Auroter <seangherardi@gmail.com>
2024-11-13 18:03:36 -0700

    label atoms table to match other view

--- apps/data-populator/app/routes/app+/index.tsx
+++ apps/data-populator/app/routes/app+/index.tsx
@@ -73,6 +73,7 @@ import { getTooltip, TooltipKey } from '@lib/utils/tooltips'
 import { json, type ActionFunctionArgs } from '@remix-run/node'
 import { useActionData, useNavigation } from '@remix-run/react'
 import {
+  Atom,
   BookCheck,
   CirclePlus,
   Download,
@@ -1812,6 +1813,11 @@ export default function CSVEditor() {
         {/* CSV data table */}
         {csvData.length > 0 && (
           <div className="overflow-x-auto rounded-lg border border-primary/30">
+            <div className="flex items-center space-x-2 px-4 py-2">
+              <Atom className="w-6 h-6" strokeWidth={1.5} />
+              <Text variant={TextVariant.headline}>Atoms</Text>
+            </div>
+
             <div className="w-full border-b border-primary/15 bg-gradient-to-r from-primary/10 to-primary/5">
               <div className="w-full flex justify-between items-center px-4 py-2 h-12">
                 <div className="text-sm tracking-wide uppercase">
