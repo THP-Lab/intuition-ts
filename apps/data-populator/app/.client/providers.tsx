34d5d1734bab2622c1b411a1e338a9ca5a862122
Auroter <seangherardi@gmail.com>
2024-11-20 16:55:18 -0700

    revert privy to local config to fix things

--- apps/data-populator/app/.client/providers.tsx
+++ apps/data-populator/app/.client/providers.tsx
@@ -2,12 +2,14 @@ import { createContext, useContext, useState } from 'react'
 
 import { TooltipProvider } from '@0xintuition/1ui'
 
+import { alchemyRpcUrlMap } from '@lib/utils/chains'
 import { wagmiConfig } from '@lib/utils/wagmi'
 import type { PrivyClientConfig } from '@privy-io/react-auth'
-import { PrivyProvider } from '@privy-io/react-auth'
+import { addRpcUrlOverrideToChain, PrivyProvider } from '@privy-io/react-auth'
 import { SmartWalletsProvider } from '@privy-io/react-auth/smart-wallets'
 import { WagmiProvider } from '@privy-io/wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { base, baseSepolia } from 'viem/chains'
 
 const queryClient = new QueryClient()
 
@@ -27,15 +29,17 @@ export function useTooltips() {
   return useContext(TooltipContext)
 }
 
-// const baseSepoliaOverride = addRpcUrlOverrideToChain(
-//   baseSepolia,
-//   alchemyRpcUrlMap(baseSepolia.id),
-// )
+// Comment these out if not using smart wallets
+const baseSepoliaOverride = addRpcUrlOverrideToChain(
+  baseSepolia,
+  alchemyRpcUrlMap(baseSepolia.id),
+)
 
-// const baseMainnetOverride = addRpcUrlOverrideToChain(
-//   base,
-//   alchemyRpcUrlMap(base.id),
-// )
+// And this.
+const baseMainnetOverride = addRpcUrlOverrideToChain(
+  base,
+  alchemyRpcUrlMap(base.id),
+)
 
 const privyConfig: PrivyClientConfig = {
   embeddedWallets: {
@@ -43,15 +47,19 @@ const privyConfig: PrivyClientConfig = {
     requireUserPasswordOnCreate: false,
     noPromptOnSignature: false,
   },
-  loginMethods: ['wallet'],
+  // loginMethods: ['wallet'], // <- Portal Config
+  loginMethods: ['wallet', 'email', 'sms', 'github'], // <- Development Config
   appearance: {
     theme: 'dark',
     showWalletLoginFirst: true,
   },
   // TODO: uncomment this when we add support for smart wallet providers
-  // defaultChain:
-  // import.meta.env.VITE_DEPLOY_ENV === 'development' ? baseSepolia : base,
-  // supportedChains: [baseSepoliaOverride, baseMainnetOverride],
+  // Note: We rolled back to this because EOAs are not behaving as expected in dev or prod
+  // ------------------------------------------------------------------------------------\
+  defaultChain:
+    import.meta.env.VITE_DEPLOY_ENV === 'development' ? baseSepolia : base,
+  supportedChains: [baseSepoliaOverride, baseMainnetOverride],
+  // ------------------------------------------------------------------------------------/
 }
 
 export default function Providers({
